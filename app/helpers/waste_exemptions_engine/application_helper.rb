d54993310df88f79ba55dbc644ad73307b31a823
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-04 23:13:23 +0000

    Resolve initial rubocop issues

--- app/helpers/waste_exemptions_engine/application_helper.rb
+++ app/helpers/waste_exemptions_engine/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module WasteExemptionsEngine
   module ApplicationHelper
   end
--- lib/waste_exemptions_engine.rb
+++ lib/waste_exemptions_engine.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "waste_exemptions_engine/engine"
 
 module WasteExemptionsEngine
