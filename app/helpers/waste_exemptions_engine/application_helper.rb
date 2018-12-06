f2e4c742e6d8656cdc13c3376f501e0c841bd33c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-06 17:21:01 +0000

    Add first working static page os-terms
    In combination with adding the engine to waste-exemptions-front-office these changes result in the os-terms page
    
    - being visible
    - using the GOV.UK template and styles
    - all images visible
    - all content loaded
    
    Essentially this is confirmation that app can load the engine, with the app controlling layout and styling and the engine controlling the content.

--- app/helpers/waste_exemptions_engine/application_helper.rb
+++ app/helpers/waste_exemptions_engine/application_helper.rb
@@ -2,5 +2,26 @@
 
 module WasteExemptionsEngine
   module ApplicationHelper
+    def title
+      title_elements = [title_text, "Register as a waste carrier", "GOV.UK"]
+      # Remove empty elements, for example if no specific title is set
+      title_elements.delete_if(&:empty?)
+      title_elements.join(" - ")
+    end
+
+    private
+
+    def title_text
+      # Check if the title is set in the view (we do this for High Voltage pages)
+      return content_for :title if content_for?(:title)
+
+      # Otherwise, look up translation key based on controller path, action name and .title
+      # Solution from https://coderwall.com/p/a1pj7w/rails-page-titles-with-the-right-amount-of-magic
+      title = t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: "")
+      return title if title.present?
+
+      # Default to title for "new" action if the current action doesn't return anything
+      t("#{controller_path.tr('/', '.')}.new.title", default: "")
+    end
   end
 end
--- lib/waste_exemptions_engine/engine.rb
+++ lib/waste_exemptions_engine/engine.rb
@@ -11,5 +11,12 @@ module WasteExemptionsEngine
       g.fixture_replacement :factory_bot
       g.factory_bot dir: "spec/factories"
     end
+
+    # Load I18n translation files from engine before loading ones from the host app
+    # This means values in the host app can override those in the engine
+    config.before_initialize do
+      engine_locales = Dir["#{config.root}/config/locales/**/*.yml"]
+      config.i18n.load_path = engine_locales + config.i18n.load_path
+    end
   end
 end
