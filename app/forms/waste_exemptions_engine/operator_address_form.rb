340029a8b94fe85c8cf436e6a853e756e10e1bd9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-24 12:59:30 +0000

    Remove the duplication in the operator address forms
    With the previous work to remove duplication and make things consistent across the address lookup and manual base forms, it left the operator address lookup and manual forms being almost exactly alike.
    
    Therefore to reduce duplication, this adds a module to hold the duplicated code, which the 2 form objects now just include.

---
+++ app/forms/waste_exemptions_engine/operator_address_form.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  module OperatorAddressForm
+    include CanNavigateFlexibly
+
+    attr_accessor :business_type
+
+    def initialize(enrollment)
+      super
+      # We only use this for the correct microcopy
+      self.business_type = @enrollment.business_type
+    end
+
+    private
+
+    def existing_address
+      @enrollment.operator_address
+    end
+
+    def address_type
+      Address.address_types[:operator]
+    end
+  end
+end
--- app/forms/waste_exemptions_engine/operator_address_lookup_form.rb
+++ app/forms/waste_exemptions_engine/operator_address_lookup_form.rb
@@ -2,24 +2,7 @@
 
 module WasteExemptionsEngine
   class OperatorAddressLookupForm < AddressLookupForm
-    include CanNavigateFlexibly
+    include OperatorAddressForm
 
-    attr_accessor :business_type
-
-    def initialize(enrollment)
-      super
-      # We only use this for the correct microcopy
-      self.business_type = @enrollment.business_type
-    end
-
-    private
-
-    def existing_address
-      @enrollment.operator_address
-    end
-
-    def address_type
-      Address.address_types[:operator]
-    end
   end
 end
