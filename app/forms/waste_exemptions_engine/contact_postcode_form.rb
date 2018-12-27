94158b9a6aab85a4afb5daf6b06a67b8c8317284
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-27 12:18:02 +0000

    Refactor postcode form objects (#15)
    Having implemented another postcode page for the contact, we identified there was some duplication we could remove by pushing logic into the base PostcodeForm that both the contact and operator form objects inherited.

--- app/forms/waste_exemptions_engine/contact_postcode_form.rb
+++ app/forms/waste_exemptions_engine/contact_postcode_form.rb
@@ -4,28 +4,10 @@ module WasteExemptionsEngine
   class ContactPostcodeForm < PostcodeForm
     include CanNavigateFlexibly
 
-    attr_accessor :business_type, :contact_postcode
+    private
 
-    def initialize(enrollment)
-      super
-
-      self.contact_postcode = @enrollment.interim.contact_postcode
+    def existing_postcode
+      @enrollment.interim.contact_postcode
     end
-
-    def submit(params)
-      # Assign the params for validation and pass them to the BaseForm method
-      # for updating
-      self.contact_postcode = params[:contact_postcode]
-      format_postcode(contact_postcode)
-      attributes = {}
-
-      # While we won't proceed if the postcode isn't valid, we should always
-      # save it in case it's needed for manual entry
-      @enrollment.interim.update_attributes(contact_postcode: contact_postcode)
-
-      super(attributes, params[:token])
-    end
-
-    validates :contact_postcode, "waste_exemptions_engine/postcode": true
   end
 end
--- app/forms/waste_exemptions_engine/operator_postcode_form.rb
+++ app/forms/waste_exemptions_engine/operator_postcode_form.rb
@@ -4,30 +4,19 @@ module WasteExemptionsEngine
   class OperatorPostcodeForm < PostcodeForm
     include CanNavigateFlexibly
 
-    attr_accessor :business_type, :operator_postcode
+    attr_accessor :business_type
 
     def initialize(enrollment)
       super
 
-      self.operator_postcode = @enrollment.interim.operator_postcode
       # We only use this for the correct microcopy
       self.business_type = @enrollment.business_type
     end
 
-    def submit(params)
-      # Assign the params for validation and pass them to the BaseForm method
-      # for updating
-      self.operator_postcode = params[:operator_postcode]
-      format_postcode(operator_postcode)
-      attributes = {}
+    private
 
-      # While we won't proceed if the postcode isn't valid, we should always
-      # save it in case it's needed for manual entry
-      @enrollment.interim.update_attributes(operator_postcode: operator_postcode)
-
-      super(attributes, params[:token])
+    def existing_postcode
+      @enrollment.interim.operator_postcode
     end
-
-    validates :operator_postcode, "waste_exemptions_engine/postcode": true
   end
 end
