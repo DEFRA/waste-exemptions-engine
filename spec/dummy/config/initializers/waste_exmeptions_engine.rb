9d03bd333fa3cb8b7928d5babecd24b3556578e6
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-11 15:24:58 +0000

    Add WEX engine config to the test app

---
+++ spec/dummy/config/initializers/waste_exmeptions_engine.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+WasteExemptionsEngine.configure do |config|
+  # General config
+  config.application_name = "waste-exemptions-front-office"
+  config.git_repository_url = "https://github.com/DEFRA/waste-exemptions-front-office"
+
+  # Companies house API config
+  config.companies_house_host = ENV["COMPANIES_HOUSE_URL"] || "https://api.companieshouse.gov.uk/company/"
+  config.companies_house_api_key = ENV["COMPANIES_HOUSE_API_KEY"]
+
+  # Addressbase facade config
+  config.addressbase_url = ENV["ADDRESSBASE_URL"] || "http://localhost:9002"
+
+  # Email config
+  config.email_service_email = ENV["EMAIL_SERVICE_EMAIL"] || "wex-local@example.com"
+
+  # PDF config
+  config.use_xvfb_for_wickedpdf = ENV["USE_XVFB_FOR_WICKEDPDF"] || "true"
+end
