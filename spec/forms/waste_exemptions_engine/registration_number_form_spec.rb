00c3f7a63ec2dfe27389b7de846a2ea258e221bf
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-11 15:19:45 +0000

    Refactor existing form specs

--- spec/forms/waste_exemptions_engine/registration_number_form_spec.rb
+++ spec/forms/waste_exemptions_engine/registration_number_form_spec.rb
@@ -5,28 +5,31 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe RegistrationNumberForm, type: :model do
     before do
-      allow_any_instance_of(DefraRubyValidators::CompaniesHouseService).to receive(:status).and_return(:active)
+      allow_any_instance_of(DefraRubyValidators::CompaniesHouseService)
+        .to receive(:status)
+        .and_return(:active)
+    end
+
+    it_behaves_like "a validated form", :registration_number_form do
+      let(:valid_params) { { token: form.token, company_no: "09360070" } }
+      let(:invalid_params) { { token: form.token, company_no: "foo" } }
     end
 
     describe "#submit" do
       context "when the form is valid" do
-        let(:registration_number_form) { build(:registration_number_form) }
-        let(:valid_params) { { token: registration_number_form.token, company_no: "09360070" } }
-
-        it "should submit" do
-          expect(registration_number_form.submit(valid_params)).to eq(true)
-        end
+        subject(:form) { build(:registration_number_form) }
+        let(:valid_params) { { token: form.token, company_no: "09360070" } }
 
         context "when the company_no is less than 8 characters" do
           before(:each) { valid_params[:company_no] = "946107" }
 
-          it "should increase the length" do
-            registration_number_form.submit(valid_params)
-            expect(registration_number_form.company_no).to eq("00946107")
+          it "should pad company_no with 0s" do
+            form.submit(valid_params)
+            expect(form.company_no).to eq("00946107")
           end
 
           it "should submit" do
-            expect(registration_number_form.submit(valid_params)).to eq(true)
+            expect(form.submit(valid_params)).to eq(true)
           end
         end
 
@@ -34,12 +37,12 @@ module WasteExemptionsEngine
           before(:each) { valid_params[:company_no] = "sc534714" }
 
           it "should convert company_no to uppercase" do
-            registration_number_form.submit(valid_params)
-            expect(registration_number_form.company_no).to eq("SC534714")
+            form.submit(valid_params)
+            expect(form.company_no).to eq("SC534714")
           end
 
           it "should submit" do
-            expect(registration_number_form.submit(valid_params)).to eq(true)
+            expect(form.submit(valid_params)).to eq(true)
           end
         end
 
@@ -47,22 +50,13 @@ module WasteExemptionsEngine
           before(:each) { valid_params[:company_no] = "  SC534714  " }
 
           it "should remove the whitespace" do
-            registration_number_form.submit(valid_params)
-            expect(registration_number_form.company_no).to eq("SC534714")
+            form.submit(valid_params)
+            expect(form.company_no).to eq("SC534714")
           end
 
           it "should submit" do
-            expect(registration_number_form.submit(valid_params)).to eq(true)
+            expect(form.submit(valid_params)).to eq(true)
           end
-        end
-      end
-
-      context "when the form is invalid" do
-        let(:registration_number_form) { build(:registration_number_form) }
-        let(:invalid_params) { { token: registration_number_form.token, company_no: "foo" } }
-
-        it "should not submit" do
-          expect(registration_number_form.submit(invalid_params)).to eq(false)
         end
       end
     end
