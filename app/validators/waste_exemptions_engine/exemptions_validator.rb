bf933927c13ed386bdf48754032942b107a429b8
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-13 17:27:33 +0000

    Add ExemptionsValidator tests

--- app/validators/waste_exemptions_engine/exemptions_validator.rb
+++ app/validators/waste_exemptions_engine/exemptions_validator.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
 
 module WasteExemptionsEngine
-  class ExemptionsValidator < ActiveModel::EachValidator
+  class ExemptionsValidator < BaseValidator
     def validate_each(record, attribute, value)
       return true if value.present? && value.length.positive?
 
       record.errors[attribute] << error_message(record, attribute, "inclusion")
       false
-    end
-
-    private
-
-    def error_message(record, attribute, error)
-      class_name = record.class.to_s.underscore
-      I18n.t("activemodel.errors.models.#{class_name}.attributes.#{attribute}.#{error}")
     end
   end
 end
---
+++ spec/validators/waste_exemptions_engine/exemptions_validator_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Test
+  ExemptionsValidatable = Struct.new(:exemptions) do
+    include ActiveModel::Validations
+
+    validates :exemptions, "waste_exemptions_engine/exemptions": true
+  end
+end
+
+module WasteExemptionsEngine
+  RSpec.describe ExemptionsValidator, type: :model do
+    valid_exemptions = %w[3 17]
+
+    it_behaves_like "a validator", Test::ExemptionsValidatable, :exemptions, valid_exemptions
+
+    describe "#validate_each" do
+      context "when the exclusion selection is not valid" do
+        context "because the exclusions are not present" do
+          validatable = Test::ExemptionsValidatable.new
+          error_message = Helpers::Translator.error_message(validatable, :exemptions, :inclusion)
+
+          it_behaves_like "an invalid record", validatable, :exemptions, error_message
+        end
+
+        context "because the exclusions are empty" do
+          validatable = Test::ExemptionsValidatable.new([])
+          error_message = Helpers::Translator.error_message(validatable, :exemptions, :inclusion)
+
+          it_behaves_like "an invalid record", validatable, :exemptions, error_message
+        end
+      end
+    end
+  end
+end
