e563e21a96286e96a56197349f4006b85fbe4a12
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-13 17:27:09 +0000

    Add EmailValidator tests

--- app/validators/waste_exemptions_engine/email_validator.rb
+++ app/validators/waste_exemptions_engine/email_validator.rb
@@ -3,7 +3,9 @@
 require "validates_email_format_of"
 
 module WasteExemptionsEngine
-  class EmailValidator < ActiveModel::EachValidator
+  class EmailValidator < BaseValidator
+    include CanValidatePresence
+
     def validate_each(record, attribute, value)
       return false unless value_is_present?(record, attribute, value)
 
@@ -12,24 +14,12 @@ module WasteExemptionsEngine
 
     private
 
-    def value_is_present?(record, attribute, value)
-      return true if value.present?
-
-      record.errors[attribute] << error_message(record, attribute, "blank")
-      false
-    end
-
     def valid_format?(record, attribute, value)
       # validate_email_format returns nil if the validation passes
       return true unless ValidatesEmailFormatOf.validate_email_format(value)
 
       record.errors[attribute] << error_message(record, attribute, "invalid_format")
       false
-    end
-
-    def error_message(record, attribute, error)
-      class_name = record.class.to_s.underscore
-      I18n.t("activemodel.errors.models.#{class_name}.attributes.#{attribute}.#{error}")
     end
   end
 end
---
+++ spec/validators/waste_exemptions_engine/email_validator_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Test
+  EmailValidatable = Struct.new(:email) do
+    include ActiveModel::Validations
+
+    validates :email, "waste_exemptions_engine/email": true
+  end
+end
+
+module WasteExemptionsEngine
+  RSpec.describe EmailValidator, type: :model do
+    valid_email = "test@example.com"
+    invalid_email = "foo@bar"
+
+    it_behaves_like "a validator", Test::EmailValidatable, :email, valid_email
+    it_behaves_like "a presence validator", Test::EmailValidatable, :email
+
+    describe "#validate_each" do
+      context "when the email is not valid" do
+        context "because the email is not correctly formated" do
+          validatable = Test::EmailValidatable.new(invalid_email)
+          error_message = Helpers::Translator.error_message(validatable, :email, :invalid_format)
+
+          it_behaves_like "an invalid record", validatable, :email, error_message
+        end
+      end
+    end
+  end
+end
