e0f3ad4f2da33c8527cd728dd1b81eaa9fbd8cfb
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-14 10:55:40 +0000

    Refactor validator example group to support multiple parameters

--- spec/support/shared_examples/validators/validator.rb
+++ spec/support/shared_examples/validators/validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "a validator" do |validatable_class, property, valid_value|
+RSpec.shared_examples "a validator" do |validatable_class, property, valid_input|
   it "is a type of BaseValidator" do
     expect(described_class.ancestors)
       .to include(WasteExemptionsEngine::BaseValidator)
@@ -8,7 +8,8 @@ RSpec.shared_examples "a validator" do |validatable_class, property, valid_value
 
   describe "#validate_each" do
     context "when the #{property} is valid" do
-      it_behaves_like "a valid record", validatable_class.new(valid_value)
+      valid_input = [valid_input] unless valid_input.is_a? Array
+      it_behaves_like "a valid record", validatable_class.new(*valid_input)
     end
   end
 end
