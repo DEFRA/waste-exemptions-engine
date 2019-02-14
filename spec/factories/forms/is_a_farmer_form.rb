4217641c4919e26694ae820f7dc1a559e715e832
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-02-14 09:19:16 +0000

    Remove specs
    @eminnett is carrying out a body of work to implement tests, but in a way that reduces duplication and improves consistency in how we do them.
    
    Because of this I am removing the tests I added so they don't interfere with this work. It will mean CodeClimate will shout at me, but I believe it'll be cleaner in the long run.

--- spec/factories/forms/is_a_farmer_form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :is_a_farmer_form, class: WasteExemptionsEngine::IsAFarmerForm do
-    initialize_with { new(create(:transient_registration, workflow_state: "is_a_farmer_form")) }
-  end
-end
--- spec/factories/forms/on_a_farm_form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :on_a_farm_form, class: WasteExemptionsEngine::OnAFarmForm do
-    initialize_with { new(create(:transient_registration, workflow_state: "on_a_farm_form")) }
-  end
-end
