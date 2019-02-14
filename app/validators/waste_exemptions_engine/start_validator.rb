8c19d4bd1e82912f529cfd0be2b158896e372b11
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-14 12:02:19 +0000

    Add StartValidator tests

--- app/validators/waste_exemptions_engine/start_validator.rb
+++ app/validators/waste_exemptions_engine/start_validator.rb
@@ -1,22 +1,15 @@
 # frozen_string_literal: true
 
 module WasteExemptionsEngine
-  class StartValidator < ActiveModel::EachValidator
+  class StartValidator < BaseValidator
+    include CanValidateSelection
+
     def validate_each(record, attribute, value)
       valid_options = %w[new
                          reregister
                          change]
-      return true if value.present? && valid_options.include?(value)
 
-      record.errors[attribute] << error_message(record, attribute, "inclusion")
-      false
-    end
-
-    private
-
-    def error_message(record, attribute, error)
-      class_name = record.class.to_s.underscore
-      I18n.t("activemodel.errors.models.#{class_name}.attributes.#{attribute}.#{error}")
+      value_is_included?(record, attribute, value, valid_options)
     end
   end
 end
