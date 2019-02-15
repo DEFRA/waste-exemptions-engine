eee98b5386cfb20d6f5b5c298a304ddc3a246f22
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-15 09:22:01 +0000

    Remove 'calls :validate_each' validator expectation
    This expectation was fine when every validator was using #validate_each, but the Person and MainPerson validators use #validate instead. I decided that attempting to handle both would cause too much complexity for a test that is of arguable value.

--- spec/support/shared_examples/validators/valid_record.rb
+++ spec/support/shared_examples/validators/valid_record.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples "a valid record" do |validatable|
-  # The method in this context is #validate_each
-  it "gets called" do
-    expect_any_instance_of(described_class)
-      .to receive(:validate_each)
-      .once
-
-    validatable.valid?
-  end
-
   it "confirms the object is valid" do
     expect(validatable).to be_valid
   end
