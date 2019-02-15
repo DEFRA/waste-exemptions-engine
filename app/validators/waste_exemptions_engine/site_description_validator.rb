e32a3abc9dedd5932f2a89aa926c14c85b022eff
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-15 09:25:21 +0000

    Fix typo

--- app/validators/waste_exemptions_engine/site_description_validator.rb
+++ app/validators/waste_exemptions_engine/site_description_validator.rb
@@ -8,8 +8,8 @@ module WasteExemptionsEngine
     def validate_each(record, attribute, value)
       return false unless value_is_present?(record, attribute, value)
 
-      macx_length = 500
-      value_is_not_too_long?(record, attribute, value, macx_length)
+      max_length = 500
+      value_is_not_too_long?(record, attribute, value, max_length)
     end
   end
 end
