a95a29b12aebfa029d25d3ed2c6dd6c3aa74e252
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-15 11:50:46 +0000

    Refactor MainPersonValidator to avoid duplicatation

--- spec/validators/waste_exemptions_engine/main_person_validator_spec.rb
+++ spec/validators/waste_exemptions_engine/main_person_validator_spec.rb
@@ -30,188 +30,74 @@ module WasteExemptionsEngine
       expect(described_class.ancestors).to include(WasteExemptionsEngine::PersonValidator)
     end
 
+    expectations = [
+      { business_type: :partnership,                   num_people: { 0 => false, 1 => false, 2 => true } },
+      { business_type: :sole_trader,                   num_people: { 0 => false, 1 => true,  2 => false } },
+      { business_type: :limited_company,               num_people: { 0 => false, 1 => true,  2 => true } },
+      { business_type: :limited_liability_partnership, num_people: { 0 => false, 1 => true,  2 => true } },
+      { business_type: :local_authority,               num_people: { 0 => false, 1 => true,  2 => true } },
+      { business_type: :charity,                       num_people: { 0 => false, 1 => true,  2 => true } }
+    ]
+
     describe "#validate" do
-      context "when the business type is partnership" do
-        let(:transient_registration) { create(:transient_registration, :partnership) }
-        let(:error_message) do
-          Helpers::Translator.state_error_message(validatable, :not_enough_main_people, :other, count: 2)
-        end
+      expectations.each do |expectation|
+        business_type = expectation[:business_type]
+        allowed_num_people = expectation[:num_people]
 
-        subject(:validatable) { Test::MainPersonValidatable.new(transient_registration) }
-
-        context "and the number of people is two" do
-          before(:each) do
-            transient_registration.transient_people = [build(:transient_person), build(:transient_person)]
-          end
-
-          it "confirms the object is valid" do
-            expect(validatable).to be_valid
-          end
-
-          it "the errors are empty" do
-            validatable.valid?
-            expect(validatable.errors).to be_empty
-          end
-        end
-
-        context "and the number of people is one" do
-          before(:each) { transient_registration.transient_people = [build(:transient_person)] }
-
-          it "confirms the object is invalid" do
-            expect(validatable).to_not be_valid
-          end
-
-          it "adds a single validation error to the record" do
-            validatable.valid?
-            expect(validatable.errors[:base].count).to eq(1)
-          end
-
-          it "adds an appropriate validation error" do
-            validatable.valid?
-            expect(error_message).to_not include("translation missing:")
-            expect(validatable.errors[:base]).to eq([error_message])
-          end
-        end
-
-        context "and there aren't any people" do
-          before(:each) { transient_registration.transient_people = [] }
-
-          it "confirms the object is invalid" do
-            expect(validatable).to_not be_valid
-          end
-
-          it "adds a single validation error to the record" do
-            validatable.valid?
-            expect(validatable.errors[:base].count).to eq(1)
-          end
-
-          it "adds an appropriate validation error" do
-            validatable.valid?
-            expect(error_message).to_not include("translation missing:")
-            expect(validatable.errors[:base]).to eq([error_message])
-          end
-        end
-      end
-
-      context "when the business type is sole_trader" do
-        let(:transient_registration) { create(:transient_registration, :sole_trader) }
-        subject(:validatable) { Test::MainPersonValidatable.new(transient_registration) }
-
-        context "and the number of people is one" do
-          before(:each) { transient_registration.transient_people = [build(:transient_person)] }
-
-          it "confirms the object is valid" do
-            expect(validatable).to be_valid
-          end
-
-          it "the errors are empty" do
-            validatable.valid?
-            expect(validatable.errors).to be_empty
-          end
-        end
-
-        context "and the number of people is two" do
-          let(:error_message) do
-            Helpers::Translator.state_error_message(validatable, :too_many_main_people, :one)
-          end
-
-          before(:each) do
-            transient_registration.transient_people = [build(:transient_person), build(:transient_person)]
-          end
-
-          it "confirms the object is invalid" do
-            expect(validatable).to_not be_valid
-          end
-
-          it "adds a single validation error to the record" do
-            validatable.valid?
-            expect(validatable.errors[:base].count).to eq(1)
-          end
-
-          it "adds an appropriate validation error" do
-            validatable.valid?
-            expect(error_message).to_not include("translation missing:")
-            expect(validatable.errors[:base]).to eq([error_message])
-          end
-        end
-
-        context "and there aren't any people" do
-          let(:error_message) do
-            Helpers::Translator.state_error_message(validatable, :not_enough_main_people, :one)
-          end
-
-          before(:each) { transient_registration.transient_people = [] }
-
-          it "confirms the object is invalid" do
-            expect(validatable).to_not be_valid
-          end
-
-          it "adds a single validation error to the record" do
-            validatable.valid?
-            expect(validatable.errors[:base].count).to eq(1)
-          end
-
-          it "adds an appropriate validation error" do
-            validatable.valid?
-            expect(error_message).to_not include("translation missing:")
-            expect(validatable.errors[:base]).to eq([error_message])
-          end
-        end
-      end
-
-      %i[limited_company limited_liability_partnership local_authority charity].each do |business_type|
         context "when the business type is #{business_type}" do
           let(:transient_registration) { create(:transient_registration, business_type) }
           subject(:validatable) { Test::MainPersonValidatable.new(transient_registration) }
 
-          context "and the number of people is two" do
-            before(:each) do
-              transient_registration.transient_people = [build(:transient_person), build(:transient_person)]
-            end
+          min_people = allowed_num_people.rassoc(true).first # This will get the first key that has a value of true.
 
-            it "confirms the object is valid" do
-              expect(validatable).to be_valid
-            end
+          allowed_num_people.each do |num_people, is_allowed|
 
-            it "the errors are empty" do
-              validatable.valid?
-              expect(validatable.errors).to be_empty
-            end
-          end
+            context "and the number of people is #{num_people}" do
+              before(:each) do
+                people = []
+                num_people.times { people << build(:transient_person) }
+                transient_registration.transient_people = people
+              end
 
-          context "and the number of people is one" do
-            before(:each) { transient_registration.transient_people = [build(:transient_person)] }
+              if is_allowed
+                it "confirms the object is valid" do
+                  expect(validatable).to be_valid
+                end
 
-            it "confirms the object is valid" do
-              expect(validatable).to be_valid
-            end
+                it "the errors are empty" do
+                  validatable.valid?
+                  expect(validatable.errors).to be_empty
+                end
+              else
+                let(:error_message) do
+                  if num_people > min_people && !is_allowed
+                    if min_people == 1
+                      Helpers::Translator.state_error_message(validatable, :too_many_main_people, :one)
+                    else
+                      Helpers::Translator.state_error_message(validatable, :too_many_main_people, :other, count: 2)
+                    end
+                  elsif min_people == 1
+                    Helpers::Translator.state_error_message(validatable, :not_enough_main_people, :one)
+                  else
+                    Helpers::Translator.state_error_message(validatable, :not_enough_main_people, :other, count: 2)
+                  end
+                end
 
-            it "the errors are empty" do
-              validatable.valid?
-              expect(validatable.errors).to be_empty
-            end
-          end
+                it "confirms the object is invalid" do
+                  expect(validatable).to_not be_valid
+                end
 
-          context "and there aren't any people" do
-            let(:error_message) do
-              Helpers::Translator.state_error_message(validatable, :not_enough_main_people, :one)
-            end
+                it "adds a single validation error to the record" do
+                  validatable.valid?
+                  expect(validatable.errors[:base].count).to eq(1)
+                end
 
-            before(:each) { transient_registration.transient_people = [] }
-
-            it "confirms the object is invalid" do
-              expect(validatable).to_not be_valid
-            end
-
-            it "adds a single validation error to the record" do
-              validatable.valid?
-              expect(validatable.errors[:base].count).to eq(1)
-            end
-
-            it "adds an appropriate validation error" do
-              validatable.valid?
-              expect(error_message).to_not include("translation missing:")
-              expect(validatable.errors[:base]).to eq([error_message])
+                it "adds an appropriate validation error" do
+                  validatable.valid?
+                  expect(error_message).to_not include("translation missing:")
+                  expect(validatable.errors[:base]).to eq([error_message])
+                end
+              end
             end
           end
         end
