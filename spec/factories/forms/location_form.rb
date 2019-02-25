a176b2ba0bd99e5e4720cf164e631127518a0614
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-25 11:25:23 +0000

    Add LocationForm tests

---
+++ spec/factories/forms/location_form.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :location_form, class: WasteExemptionsEngine::LocationForm do
+    initialize_with do
+      new(create(:transient_registration, workflow_state: "location_form"))
+    end
+  end
+end
