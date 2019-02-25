bc52ec24260b4378d4fa1ce7ddea0653c03ee853
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-25 11:24:14 +0000

    Add the IsAFarmerForm and OnAFarmForm tests

---
+++ spec/factories/forms/on_a_farm_form.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :on_a_farm_form, class: WasteExemptionsEngine::OnAFarmForm do
+    initialize_with do
+      new(create(:transient_registration, workflow_state: "on_a_farm_form"))
+    end
+  end
+end
