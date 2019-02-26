de5ec90ba4371eaa933226737f8cf15d68f9744d
irisfaraway <iris.faraway@gmail.com>
2019-02-26 16:38:31 +0000

    Override methods for submitting dead-end forms
    We have some 'forms' which aren't really forms as the user can't submit them. They are only used when the user journey reaches a dead end (at least within the service).
    
    This commit overrides the `submit` methods in the form classes and the `create` methods in the form controllers so that we don't attempt to go through the usual process of updating a transient_registration with form data. This is consistent with how we handle these forms in waste carriers, and how the RegistrationCompleteForm works within this engine.

--- app/controllers/waste_exemptions_engine/register_in_northern_ireland_forms_controller.rb
+++ app/controllers/waste_exemptions_engine/register_in_northern_ireland_forms_controller.rb
@@ -6,8 +6,7 @@ module WasteExemptionsEngine
       super(RegisterInNorthernIrelandForm, "register_in_northern_ireland_form")
     end
 
-    def create
-      super(RegisterInNorthernIrelandForm, "register_in_northern_ireland_form")
-    end
+    # Override this method as user shouldn't be able to "submit" this page
+    def create; end
   end
 end
