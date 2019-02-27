75e35ce825b1fef617bab6af688369cff6d26d17
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-02-27 10:50:03 +0000

    Add a test to the TransientPerson spec

--- spec/models/waste_exemptions_engine/transient_person_spec.rb
+++ spec/models/waste_exemptions_engine/transient_person_spec.rb
@@ -4,15 +4,22 @@ require "rails_helper"
 
 module WasteExemptionsEngine
   RSpec.describe TransientPerson, type: :model do
-    describe "public interface" do
-      subject(:transient_person) { build(:transient_person) }
+    subject(:transient_person) { build(:transient_person) }
 
+    describe "public interface" do
       associations = [:transient_registration]
 
       (Helpers::ModelProperties::PERSON + associations).each do |property|
         it "responds to property" do
           expect(transient_person).to respond_to(property)
         end
+      end
+    end
+
+    describe "#person_attributes" do
+      it "returns attributes specific to defining a person" do
+        attributes = transient_person.person_attributes
+        expect(attributes.keys).to match_array(Helpers::ModelProperties::PERSON.map(&:to_s))
       end
     end
   end
