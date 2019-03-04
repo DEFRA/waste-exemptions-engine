f829c3a7e07db141d71c49bf0393c723282d5281
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-03-04 08:54:27 +0000

    Add Registration Complete workflow tests

---
+++ spec/models/waste_exemptions_engine/transient_registration_workflow_state/registration_complete_form_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe TransientRegistration, type: :model do
+    describe "#workflow_state" do
+      current_state = :registration_complete_form
+      subject(:transient_registration) { create(:transient_registration, workflow_state: current_state) }
+
+      context "when a TransientRegistration's state is #{current_state}" do
+        it "can not transition to any states" do
+          permitted_states = transient_registration.aasm.states(permitted: true).map(&:name)
+          expect(permitted_states).to be_empty
+        end
+      end
+    end
+  end
+end
