1d5c4ed153be89428c8cd1b86e60cb0a919017c7
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-03-04 08:45:33 +0000

    Add traits to the two address factories

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  address_types = WasteExemptionsEngine::TransientAddress.address_types
+
   factory :address, class: WasteExemptionsEngine::Address do
+    trait :operator_address do
+      address_type { address_types[:operator] }
+    end
+
+    trait :contact_address do
+      address_type { address_types[:contact] }
+    end
+
+    trait :site_address do
+      address_type { address_types[:site] }
+    end
   end
 end
--- spec/factories/transient_address.rb
+++ spec/factories/transient_address.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  address_types = WasteExemptionsEngine::TransientAddress.address_types
+
   factory :transient_address, class: WasteExemptionsEngine::TransientAddress do
+    trait :operator_address do
+      address_type { address_types[:operator] }
+    end
+
+    trait :contact_address do
+      address_type { address_types[:contact] }
+    end
+
+    trait :site_address do
+      address_type { address_types[:site] }
+    end
   end
 end
