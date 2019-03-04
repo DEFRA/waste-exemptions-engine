0bb55b194b2ef04ca65ce244d61cc6eb7bc83a81
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-03-04 08:48:59 +0000

    Add workflow tests for terminal workflow states

---
+++ spec/models/waste_exemptions_engine/transient_registration_workflow_state/register_in_northern_ireland_form_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe TransientRegistration, type: :model do
+    describe "#workflow_state" do
+      it_behaves_like "a final state", previous_state: :location_form, current_state: :register_in_northern_ireland_form
+    end
+  end
+end
