8f284a2a320e52d55afe55ddc7504a7298a27781
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-03-07 07:52:35 +0000

    Add :complete TransientRegistration factory trait

--- spec/factories/transient_registration.rb
+++ spec/factories/transient_registration.rb
@@ -25,5 +25,33 @@ FactoryBot.define do
     trait :sole_trader do
       business_type { "soleTrader" }
     end
+
+    trait :complete do
+      transient_addresses do
+        address_types = WasteExemptionsEngine::TransientAddress.address_types
+        [
+          create(:transient_address, address_type: address_types[:operator]),
+          create(:transient_address, address_type: address_types[:contact]),
+          create(:transient_address, address_type: address_types[:site])
+        ]
+      end
+
+      location { "england" }
+      applicant_first_name { "Joe" }
+      applicant_last_name { "Bloggs" }
+      applicant_phone { "01234567890" }
+      applicant_email { "test@example.com" }
+      business_type { "limitedCompany" }
+      company_no { "sc534714" }
+      operator_name { "Acme Waste Management" }
+      contact_first_name { "Joe" }
+      contact_last_name { "Bloggs" }
+      contact_position { "Chief Waste Carrier" }
+      contact_phone { "01234567890" }
+      contact_email { "test@example.com" }
+      on_a_farm { true }
+      is_a_farmer { true }
+      exemptions { WasteExemptionsEngine::Exemption.all }
+    end
   end
 end
