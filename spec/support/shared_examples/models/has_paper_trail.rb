eaf14085875cb84d043e9a2a97a6eccec075c7e6
irisfaraway <iris.faraway@gmail.com>
2019-03-11 13:31:22 +0000

    Improve PaperTrail tests and efficiency
    This commit adds the PaperTrail RSpec helper. This switches PaperTrail off by default when running tests (which should improve our speed) but also lets us use some extra matches when tests are enabled (only in the PaperTrail shared example).

--- spec/support/shared_examples/models/has_paper_trail.rb
+++ spec/support/shared_examples/models/has_paper_trail.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "it has PaperTrail" do |model_factory, field|
+RSpec.shared_examples "it has PaperTrail", versioning: true do |model_factory, field|
   subject(:instance) { create(model_factory) }
+
+  it "has PaperTrail" do
+    expect(PaperTrail).to be_enabled
+  end
+
+  it "is versioned" do
+    expect(instance).to be_versioned
+  end
 
   it "creates a new version when it is updated" do
     expect { instance.update_attribute(field, "foo") }.to change { instance.versions.count }.by(1)
+  end
+
+  it "stores the correct values when it is updated" do
+    instance.update_attribute(field, "foo")
+    instance.update_attribute(field, "bar")
+    expect(instance).to have_a_version_with(field => "foo")
   end
 end
