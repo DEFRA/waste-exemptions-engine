275b594eecb9853f02f836a4512535f710e63626
Edward Minnett <edward.minnett@defra.gsi.gov.uk>
2019-04-01 11:31:34 +0100

    Refactor boolean config values to behave as booleans

--- config/initializers/wickedpdf.rb
+++ config/initializers/wickedpdf.rb
@@ -2,7 +2,7 @@
 
 require "wicked_pdf"
 
-if WasteExemptionsEngine.configuration.use_xvfb_for_wickedpdf == "true"
+if WasteExemptionsEngine.configuration.use_xvfb_for_wickedpdf
   WickedPdf.config = {
     exe_path: WasteExemptionsEngine::Engine.root.join("script", "wkhtmltopdf.sh").to_s
   }
