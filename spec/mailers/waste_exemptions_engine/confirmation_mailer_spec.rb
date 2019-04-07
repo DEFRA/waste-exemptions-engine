66d65a783a17e39a05feab69ca014c655608403c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-04-07 21:22:03 +0100

    Remove all mailer tests
    Trying to confirm if its something in the mailer tests that is intefering.

--- spec/mailers/waste_exemptions_engine/confirmation_mailer_spec.rb
+++ spec/mailers/waste_exemptions_engine/confirmation_mailer_spec.rb
@@ -4,33 +4,33 @@ require "rails_helper"
 
 module WasteExemptionsEngine
   RSpec.describe ConfirmationMailer, type: :mailer do
-    before(:all) do
-      @service_email = "test@wex.gov.uk"
-      WasteExemptionsEngine.configuration.email_service_email = @service_email
+    # before(:all) do
+    #   @service_email = "test@wex.gov.uk"
+    #   WasteExemptionsEngine.configuration.email_service_email = @service_email
 
-      @registration = create(:registration, :emailed)
-      @recipient = "test@example.com"
-      @mail = ConfirmationMailer.send_confirmation_email(@registration, @recipient)
-    end
+    #   @registration = create(:registration, :emailed)
+    #   @recipient = "test@example.com"
+    #   @mail = ConfirmationMailer.send_confirmation_email(@registration, @recipient)
+    # end
 
-    describe "#send_confirmation_email" do
-      it "uses the correct 'to' address" do
-        expect(@mail.to).to eq([@recipient])
-      end
+    # describe "#send_confirmation_email" do
+    #   it "uses the correct 'to' address" do
+    #     expect(@mail.to).to eq([@recipient])
+    #   end
 
-      it "uses the correct 'from' address" do
-        expect(@mail.from).to eq([@service_email])
-      end
+    #   it "uses the correct 'from' address" do
+    #     expect(@mail.from).to eq([@service_email])
+    #   end
 
-      it "uses the correct subject" do
-        subject = "Waste exemptions registration #{@registration.reference} completed"
-        expect(@mail.subject).to eq(subject)
-      end
+    #   it "uses the correct subject" do
+    #     subject = "Waste exemptions registration #{@registration.reference} completed"
+    #     expect(@mail.subject).to eq(subject)
+    #   end
 
-      it "includes the correct template in the body" do
-        expect(@mail.body.encoded).to include("This is not a permit")
-      end
-    end
+    #   it "includes the correct template in the body" do
+    #     expect(@mail.body.encoded).to include("This is not a permit")
+    #   end
+    # end
 
     # context "attachments" do
     #   it "has 3 attachments (pdf and logo)" do
