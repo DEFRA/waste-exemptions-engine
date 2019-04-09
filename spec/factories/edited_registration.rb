b231c1798f51972b41bf42e3146fe69dedade710
irisfaraway <iris.faraway@gmail.com>
2019-04-09 14:08:40 +0100

    EditedRegistraton factory uses a Registration when initialising
    When we create a new EditedRegistration, it will need to copy all its data from an existing Registration, similar to how the waste carriers engine currently creates new TransientRegistrations for renewing.
    
    So that we have data to copy, we need a new trait for the Registration which has all the expected data.

--- spec/factories/edited_registration.rb
+++ spec/factories/edited_registration.rb
@@ -2,5 +2,10 @@
 
 FactoryBot.define do
   factory :edited_registration, class: WasteExemptionsEngine::EditedRegistration do
+    # Create a new registration when initializing so we can copy its data
+    initialize_with do
+      # Reload the registration so we can get the reference
+      new(reference: create(:registration, :complete).reference)
+    end
   end
 end
--- spec/factories/registration.rb
+++ spec/factories/registration.rb
@@ -2,6 +2,13 @@
 
 FactoryBot.define do
   factory :registration, class: WasteExemptionsEngine::Registration do
+    # Normally the reference would be created in the transient_registration and then transferred. However, since we're
+    # creating this Registration out of thin air, we need to generate it here. Format should be WEX900001 to avoid
+    # clashes with other Registrations, where the reference is based on the id of the original TransientRegistration.
+    sequence :reference do |n|
+      format("WEX9%05d", n)
+    end
+
     trait :limited_company do
       business_type { "limitedCompany" }
     end
@@ -24,6 +31,33 @@ FactoryBot.define do
 
     trait :sole_trader do
       business_type { "soleTrader" }
+    end
+
+    trait :complete do
+      location { "england" }
+      applicant_first_name { "Joe" }
+      applicant_last_name { "Bloggs" }
+      applicant_phone { "01234567890" }
+      applicant_email { "test@example.com" }
+      business_type { "limitedCompany" }
+      company_no { "09360070" }
+      operator_name { "Acme Waste Management" }
+      contact_first_name { "Joe" }
+      contact_last_name { "Bloggs" }
+      contact_position { "Chief Waste Carrier" }
+      contact_phone { "01234567890" }
+      contact_email { "test@example.com" }
+      on_a_farm { true }
+      is_a_farmer { true }
+      exemptions { WasteExemptionsEngine::Exemption.all }
+
+      after(:create) do |registration|
+        registration.addresses = [
+          create(:address, :operator_address, :manual),
+          create(:address, :contact_address, :manual),
+          create(:address, :site_address, :manual)
+        ]
+      end
     end
   end
 end
