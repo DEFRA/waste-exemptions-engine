1b5366f040b6485e1f7ba062b2f6bac9226dcb5f
irisfaraway <iris.faraway@gmail.com>
2019-04-11 14:23:19 +0100

    Refactor address lookup transitions into shared example
    The editing workflow will bring in another 3 files like this, so it's time for a refactor to reduce the duplication.
    
    Address lookup forms all follow a similar pattern. They always go back to a postcode form. They always have the option to skip to a manual address form.
    
    The only differences are in the next form (when it's not a manual address form) and what the form itself is called (site, operator, contact).

--- spec/models/waste_exemptions_engine/transient_registration_workflow_state/operator_address_lookup_form_spec.rb
+++ spec/models/waste_exemptions_engine/transient_registration_workflow_state/operator_address_lookup_form_spec.rb
@@ -5,64 +5,10 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe TransientRegistration, type: :model do
     describe "#workflow_state" do
-      previous_state = :operator_postcode_form
-      current_state = :operator_address_lookup_form
-      subject(:transient_registration) { create(:transient_registration, workflow_state: current_state) }
-
-      context "when a TransientRegistration's state is #{current_state}" do
-        context "when transient_registration.skip_to_manual_address? is false" do
-          next_state = :contact_name_form
-          alt_state = :operator_address_manual_form
-
-          before(:each) { transient_registration.address_finder_error = false }
-
-          it "can only transition to either #{previous_state}, #{next_state}, or #{alt_state}" do
-            permitted_states = Helpers::WorkflowStates.permitted_states(transient_registration)
-            expect(permitted_states).to match_array([previous_state, next_state, alt_state])
-          end
-
-          it "changes to #{next_state} after the 'next' event" do
-            expect(transient_registration.send(:skip_to_manual_address?)).to eq(false)
-            expect(transient_registration).to transition_from(current_state).to(next_state).on_event(:next)
-          end
-
-          it "changes to #{alt_state} after the 'skip_to_manual_address' event" do
-            expect(transient_registration.send(:skip_to_manual_address?)).to eq(false)
-            expect(transient_registration)
-              .to transition_from(current_state)
-              .to(alt_state)
-              .on_event(:skip_to_manual_address)
-          end
-        end
-
-        context "when transient_registration.skip_to_manual_address? is true" do
-          next_state = :operator_address_manual_form
-
-          before(:each) { transient_registration.address_finder_error = true }
-
-          it "can only transition to either #{previous_state} or #{next_state}" do
-            permitted_states = Helpers::WorkflowStates.permitted_states(transient_registration)
-            expect(permitted_states).to match_array([previous_state, next_state])
-          end
-
-          it "changes to #{next_state} after the 'next' event" do
-            expect(transient_registration.send(:skip_to_manual_address?)).to eq(true)
-            expect(transient_registration).to transition_from(current_state).to(next_state).on_event(:next)
-          end
-
-          it "changes to #{next_state} after the 'skip_to_manual_address' event" do
-            expect(transient_registration.send(:skip_to_manual_address?)).to eq(true)
-            expect(transient_registration)
-              .to transition_from(current_state)
-              .to(next_state)
-              .on_event(:skip_to_manual_address)
-          end
-        end
-
-        it "changes to #{previous_state} after the 'back' event" do
-          expect(transient_registration).to transition_from(current_state).to(previous_state).on_event(:back)
-        end
-      end
+      it_behaves_like "an address lookup transition",
+                      next_state_without_skipping_to_manual: :contact_name_form,
+                      address_type: "operator",
+                      factory: :transient_registration
     end
   end
 end
