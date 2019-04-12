3e8bdeae73d825f789bc60d8ff27cafbbbbdc8fb
cintamani <cintamani.puddu@gmail.com>
2019-04-12 15:06:39 +0100

    Add missing `# frozen_string_literal: true` to the new initializer file

--- config/initializers/action_mailer_interceptors.rb
+++ config/initializers/action_mailer_interceptors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 if WasteExemptionsEngine.configuration.use_last_email_cache
   ActionMailer::Base.register_interceptor(WasteExemptionsEngine::LastEmailCacheService)
 end
