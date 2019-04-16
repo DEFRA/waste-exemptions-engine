83b0721b0acfb15972aa3a8a957efdbe744df1bc
cintamani <cintamani.puddu@gmail.com>
2019-04-16 15:22:07 +0100

    Remove spaces, tabs and next line

--- spec/support/pdf_matchers.rb
+++ spec/support/pdf_matchers.rb
@@ -6,12 +6,14 @@ require "pdf-reader"
 RSpec::Matchers.define :match_pdf_content do |fixtures_path_to_expected_pdf|
   match do |actual|
     expected = PDF::Reader.new(fixtures_path_to_expected_pdf)
-    @expected = expected.pages.map(&:text).join.delete("\n")
+    @expected = expected.pages.map(&:text).join
+    @expected = @expected.delete("\n").delete(" ").delete("\t")
 
     temp_pdf = Tempfile.new("pdf")
     temp_pdf.binmode
     temp_pdf << actual
-    @actual = PDF::Reader.new(temp_pdf).pages.map(&:text).join.delete("\n")
+    @actual = PDF::Reader.new(temp_pdf).pages.map(&:text).join
+    @actual = @actual.delete("\n").delete(" ").delete("\t")
     temp_pdf.close
 
     @actual == @expected
