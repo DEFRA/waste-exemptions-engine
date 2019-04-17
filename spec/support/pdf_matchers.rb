6a8ddda82eb53641f2ac18c7505b088d935f7a4a
cintamani <cintamani.puddu@gmail.com>
2019-04-17 16:57:22 +0100

    Add comments explaining the code around setting the temporary file in binary mode

--- spec/support/pdf_matchers.rb
+++ spec/support/pdf_matchers.rb
@@ -10,7 +10,14 @@ RSpec::Matchers.define :match_pdf_content do |fixtures_path_to_expected_pdf|
     @expected = @expected.delete("\n").delete(" ").delete("\t")
 
     temp_pdf = Tempfile.new("pdf")
+
+    ## Most linux systems distinguish between binary and text content files.
+    ## The PDF content is not compatible with file managed in text mode.
+    ## Tempfile#binmode will tell the system to manage the file in binmode rather
+    ## than text mode. This issue does not occur on OSX but it does in most
+    ## production servers.
     temp_pdf.binmode
+
     temp_pdf << actual
     @actual = PDF::Reader.new(temp_pdf).pages.map(&:text).join
     @actual = @actual.delete("\n").delete(" ").delete("\t")
