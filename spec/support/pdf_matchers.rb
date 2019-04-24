124cbb5e1464ffde794aad5924c4b6aa45159f43
cintamani <cintamani.puddu@gmail.com>
2019-04-24 14:51:09 +0100

    Refactoring of PdfMatchers helper file - Remove duplicated code

--- spec/support/pdf_matchers.rb
+++ spec/support/pdf_matchers.rb
@@ -3,26 +3,39 @@
 require "rspec/expectations"
 require "pdf-reader"
 
+module Helpers
+  module PdfMatchersHelper
+    def self.extract_pdf_content(pdf_stream)
+      temp_pdf = Tempfile.new("pdf")
+
+      ## Most linux systems distinguish between binary and text content files.
+      ## The PDF content is not compatible with file managed in text mode.
+      ## Tempfile#binmode will tell the system to manage the file in binmode rather
+      ## than text mode. This issue does not occur on OSX but it does in most
+      ## production servers.
+      temp_pdf.binmode
+
+      temp_pdf << pdf_stream
+      pdf_text = PDF::Reader.new(temp_pdf).pages.map(&:text).join
+      pdf_text = clean_pdf_text(pdf_text)
+      temp_pdf.close
+
+      pdf_text
+    end
+
+    def self.clean_pdf_text(text)
+      text.delete("\n").delete(" ").delete("\t")
+    end
+  end
+end
+
 RSpec::Matchers.define :match_pdf_content do |fixtures_path_to_expected_pdf|
   match do |actual|
     expected = PDF::Reader.new(fixtures_path_to_expected_pdf)
     @expected = expected.pages.map(&:text).join
-    @expected = @expected.delete("\n").delete(" ").delete("\t")
+    @expected = Helpers::PdfMatchersHelper.clean_pdf_text(@expected)
 
-    temp_pdf = Tempfile.new("pdf")
-
-    ## Most linux systems distinguish between binary and text content files.
-    ## The PDF content is not compatible with file managed in text mode.
-    ## Tempfile#binmode will tell the system to manage the file in binmode rather
-    ## than text mode. This issue does not occur on OSX but it does in most
-    ## production servers.
-    temp_pdf.binmode
-
-    temp_pdf << actual
-    @actual = PDF::Reader.new(temp_pdf).pages.map(&:text).join
-    @actual = @actual.delete("\n").delete(" ").delete("\t")
-    temp_pdf.close
-
+    @actual = Helpers::PdfMatchersHelper.extract_pdf_content(actual)
     @actual == @expected
   end
 
@@ -33,22 +46,9 @@ end
 
 RSpec::Matchers.define :include_pdf_content do |expected|
   match do |actual|
-    @expected = expected.delete("\n").delete(" ").delete("\t")
+    @expected = Helpers::PdfMatchersHelper.clean_pdf_text(expected)
 
-    temp_pdf = Tempfile.new("pdf")
-
-    ## Most linux systems distinguish between binary and text content files.
-    ## The PDF content is not compatible with file managed in text mode.
-    ## Tempfile#binmode will tell the system to manage the file in binmode rather
-    ## than text mode. This issue does not occur on OSX but it does in most
-    ## production servers.
-    temp_pdf.binmode
-
-    temp_pdf << actual
-    @actual = PDF::Reader.new(temp_pdf).pages.map(&:text).join
-    @actual = @actual.delete("\n").delete(" ").delete("\t")
-    temp_pdf.close
-
+    @actual = Helpers::PdfMatchersHelper.extract_pdf_content(actual)
     @actual.include?(@expected)
   end
 
