05a34678c9fb8fefd766ed3f8996197f3e96c6fd
irisfaraway <iris.faraway@gmail.com>
2019-04-24 17:25:47 +0100

    Add more test data to registration factory
    We'll be copying this around a lot in the edit test, so the more data to play with, the better.

--- spec/factories/registration.rb
+++ spec/factories/registration.rb
@@ -2,6 +2,15 @@
 
 FactoryBot.define do
   factory :registration, class: WasteExemptionsEngine::Registration do
+    # Normally the reference would be created in the transient_registration and
+    # then transferred. However, since we're creating this Registration out of
+    # thin air, we need to generate it here. Format should be WEX900001 to
+    # avoid clashes with other Registrations, where the reference is based on
+    # the id of the original TransientRegistration.
+    sequence :reference do |n|
+      format("WEX9%05d", n)
+    end
+
     trait :limited_company do
       business_type { "limitedCompany" }
     end
@@ -37,6 +46,31 @@ FactoryBot.define do
     trait :confirmable do
       emailable
       company_no { "123123456" }
+    end
+
+    trait :complete do
+      location { "england" }
+      applicant_first_name { "Joe" }
+      applicant_last_name { "Bloggs" }
+      applicant_phone { "01234567890" }
+      applicant_email { "test@example.com" }
+      business_type { "limitedCompany" }
+      company_no { "09360070" }
+      operator_name { "Acme Waste Management" }
+      contact_first_name { "Joe" }
+      contact_last_name { "Bloggs" }
+      contact_position { "Chief Waste Carrier" }
+      contact_phone { "01234567890" }
+      contact_email { "test@example.com" }
+      on_a_farm { true }
+      is_a_farmer { true }
+      exemptions { build_list(:exemption, 10) }
+
+      addresses do
+        [build(:address, :operator_address),
+         build(:address, :contact_address),
+         build(:address, :site_address)]
+      end
     end
   end
 end
