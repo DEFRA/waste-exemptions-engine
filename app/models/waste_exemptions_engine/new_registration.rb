107a6ec9ba3328de4cf1c5b1fa03b0c2c5d8e712
irisfaraway <iris.faraway@gmail.com>
2019-04-24 15:43:58 +0100

    Add NewRegistration
    Our first subtype of TransientRegistration. At the moment, this is basically identical.

---
+++ app/models/waste_exemptions_engine/new_registration.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  class NewRegistration < TransientRegistration
+  end
+end
---
+++ spec/models/waste_exemptions_engine/new_registration_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe NewRegistration, type: :model do
+    subject(:new_registration) { create(:new_registration) }
+
+    it "subclasses TransientRegistration" do
+      expect(described_class).to be < TransientRegistration
+    end
+
+    describe "public interface" do
+      Helpers::ModelProperties::TRANSIENT_REGISTRATION.each do |property|
+        it "responds to property" do
+          expect(new_registration).to respond_to(property)
+        end
+      end
+    end
+  end
+end
