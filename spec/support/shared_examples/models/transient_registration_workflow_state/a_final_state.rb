63d579a4bc6efe4b086accea354176bad5e5ca42
irisfaraway <iris.faraway@gmail.com>
2019-04-24 15:52:31 +0100

    Allow workflow_state examples to use different factories
    This should allow us to test workflows for different models.

--- spec/support/shared_examples/models/transient_registration_workflow_state/a_final_state.rb
+++ spec/support/shared_examples/models/transient_registration_workflow_state/a_final_state.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "a final state" do |previous_state:, current_state:|
-  context "when a TransientRegistration's state is #{current_state}" do
-    subject(:transient_registration) { create(:transient_registration, workflow_state: current_state) }
+RSpec.shared_examples "a final state" do |previous_state:, current_state:, factory: :transient_registration|
+  context "when a subject's state is #{current_state}" do
+    subject(:subject) { create(factory, workflow_state: current_state) }
 
     it "can only transition to #{previous_state}" do
-      permitted_states = Helpers::WorkflowStates.permitted_states(transient_registration)
+      permitted_states = Helpers::WorkflowStates.permitted_states(subject)
       expect(permitted_states).to eq([previous_state])
     end
 
     it "is unable to transition when the 'next' event is issued" do
-      expect { transient_registration.next }.to raise_error(AASM::InvalidTransition)
+      expect { subject.next }.to raise_error(AASM::InvalidTransition)
     end
 
     it "changes to #{previous_state} after the 'back' event" do
-      expect(transient_registration).to transition_from(current_state).to(previous_state).on_event(:back)
+      expect(subject).to transition_from(current_state).to(previous_state).on_event(:back)
     end
   end
 end
