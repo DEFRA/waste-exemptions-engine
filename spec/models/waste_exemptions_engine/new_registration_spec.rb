2dc264307751c607fe6d69df2f3ab9670f8964e5
irisfaraway <iris.faraway@gmail.com>
2019-04-25 12:49:36 +0100

    Create shared example to test TransientRegistration behaviour
    Since we won't have any non-subclassed TransientRegistrations, this will allow us to test the core behaviour on its subclasses.

--- spec/models/waste_exemptions_engine/new_registration_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_spec.rb
@@ -6,6 +6,8 @@ module WasteExemptionsEngine
   RSpec.describe NewRegistration, type: :model do
     subject(:new_registration) { create(:new_registration) }
 
+    it_behaves_like "a transient_registration", :new_registration
+
     it "subclasses TransientRegistration" do
       expect(described_class).to be < TransientRegistration
     end
---
+++ spec/support/shared_examples/models/transient_registration.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples "a transient_registration" do |model_factory|
+  subject(:transient_registration) { create(model_factory) }
+
+  describe "public interface" do
+    Helpers::ModelProperties::TRANSIENT_REGISTRATION.each do |property|
+      it "responds to property" do
+        expect(transient_registration).to respond_to(property)
+      end
+    end
+  end
+
+  describe "#token" do
+    context "when a transient registration is created" do
+      it "has a token" do
+        expect(transient_registration.token).not_to be_empty
+      end
+    end
+  end
+
+  describe "#registration_attributes" do
+    it "returns attributes specific to defining a registration" do
+      attributes = transient_registration.registration_attributes
+      registration_attributes = Helpers::ModelProperties::REGISTRATION.map(&:to_s) - ["submitted_at"]
+      expect(attributes.keys).to match_array(registration_attributes)
+    end
+  end
+
+  it_behaves_like "an owner of registration attributes", model_factory, :transient_address
+end
