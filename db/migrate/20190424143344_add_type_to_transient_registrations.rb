946e1f81cef9b7e93fd1463503a3321a044e2025
irisfaraway <iris.faraway@gmail.com>
2019-04-25 12:19:55 +0100

    Update existing TransientRegs during type migration
    If there are any existing TransientRegistrations in the DB when the subtypes are added, they will become NewRegistrations by default.

--- db/migrate/20190424143344_add_type_to_transient_registrations.rb
+++ db/migrate/20190424143344_add_type_to_transient_registrations.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class AddTypeToTransientRegistrations < ActiveRecord::Migration
-  def change
+  def up
     add_column :transient_registrations, :type, :string
+    # Set any existing TransientRegistrations to NewRegistrations so everything has a type
+    execute <<-SQL
+       UPDATE transient_registrations
+       SET type = 'WasteExemptionsEngine::NewRegistration';
+    SQL
+  end
+
+  def down
+    remove_column :transient_registrations, :type
   end
 end
