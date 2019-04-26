3b07ea16d1fd92f7994505cc90c57b036d43982c
irisfaraway <iris.faraway@gmail.com>
2019-04-26 09:15:49 +0100

    Add shared example for un-back-able final states
    We have an example for 'final' states which users can go back out of, but not ones which are truly the end (in this case, 'journey complete' pages). This commit adds that missing shared example and refactors the existing specs to use it.

--- spec/models/waste_exemptions_engine/edit_registration_workflow_state/edit_complete_form_spec.rb
+++ spec/models/waste_exemptions_engine/edit_registration_workflow_state/edit_complete_form_spec.rb
@@ -5,15 +5,9 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe EditRegistration, type: :model do
     describe "#workflow_state" do
-      current_state = :edit_complete_form
-      subject(:edit_registration) { create(:edit_registration, workflow_state: current_state) }
-
-      context "when a WasteExemptionsEngine::EditRegistration's state is #{current_state}" do
-        it "can not transition to any states" do
-          permitted_states = Helpers::WorkflowStates.permitted_states(edit_registration)
-          expect(permitted_states).to be_empty
-        end
-      end
+      it_behaves_like "a fixed final state",
+                      current_state: :edit_complete_form,
+                      factory: :edit_registration
     end
   end
 end
--- spec/models/waste_exemptions_engine/new_registration_workflow_state/registration_complete_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/registration_complete_form_spec.rb
@@ -5,15 +5,9 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe NewRegistration, type: :model do
     describe "#workflow_state" do
-      current_state = :registration_complete_form
-      subject(:new_registration) { create(:new_registration, workflow_state: current_state) }
-
-      context "when a NewRegistration's state is #{current_state}" do
-        it "can not transition to any states" do
-          permitted_states = Helpers::WorkflowStates.permitted_states(new_registration)
-          expect(permitted_states).to be_empty
-        end
-      end
+      it_behaves_like "a fixed final state",
+                      current_state: :registration_complete_form,
+                      factory: :new_registration
     end
   end
 end
