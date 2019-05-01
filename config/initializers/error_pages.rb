8661f5d5e842fcd9801692aed796bd282a6a1a36
cintamani <cintamani.puddu@gmail.com>
2019-05-01 16:17:07 +0100

    Remove dot

--- config/initializers/error_pages.rb
+++ config/initializers/error_pages.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 
 unless Rails.env.development? || Rails.env.test?
   Rails.application.config.exceptions_app = WasteExemptionsEngine::Engine.routes
