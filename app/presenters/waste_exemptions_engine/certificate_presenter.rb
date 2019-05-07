d9a19ecdc53615b89764ce1c36604a42791c7939
cintamani <cintamani.puddu@gmail.com>
2019-05-07 12:37:26 +0100

    Refactor and add test coverage on the certificate presenter

--- app/presenters/waste_exemptions_engine/certificate_presenter.rb
+++ app/presenters/waste_exemptions_engine/certificate_presenter.rb
@@ -8,26 +8,26 @@ module WasteExemptionsEngine
 
     def partners_names
       people.select(&:partner?).map do |person|
-        format_name(first: person.first_name, last: person.last_name)
+        format_name(person.first_name, person.last_name)
       end.join("</br>").html_safe
     end
 
     def applicant_name
-      format_name(first: applicant_first_name, last: applicant_last_name)
+      format_name(applicant_first_name, applicant_last_name)
     end
 
-    def business_type
+    def human_business_type
       I18n.t("waste_exemptions_engine.pdfs.certificate.busness_types.#{super}")
     end
 
     def contact_name
-      format_name(first: contact_first_name, last: contact_last_name)
+      format_name(contact_first_name, contact_last_name)
     end
 
     private
 
-    def format_name(names)
-      format("%<first>s %<last>s", names)
+    def format_name(first_name, last_name)
+      "#{first_name} #{last_name}"
     end
   end
 end
---
+++ spec/presenters/waste_exemptions_engine/certificate_presenter_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe CertificatePresenter do
+    subject { described_class.new(registration) }
+
+    describe "#partnership?" do
+      let(:registration) { double(:registration, business_type: business_type) }
+
+      context "when the business type is partnership" do
+        let(:business_type) { "Partnership" }
+
+        it "returns true" do
+          expect(subject).to be_partnership
+        end
+      end
+
+      context "when the business type is not partnership" do
+        let(:business_type) { "something_else" }
+
+        it "returns false" do
+          expect(subject).to_not be_partnership
+        end
+      end
+    end
+
+    describe "#partners_names" do
+      let(:partners) do
+        [
+          double(:person, first_name: "Joe", last_name: "Snow", partner?: true),
+          double(:person, first_name: "Anita", last_name: "Flieg", partner?: true)
+        ]
+      end
+      let(:registration) { double(:registration, people: partners) }
+
+      it "returns an html formatted string of partners names" do
+        expect(subject.partners_names).to eq("Joe Snow</br>Anita Flieg")
+      end
+    end
+  end
+end
