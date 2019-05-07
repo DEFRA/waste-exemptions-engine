0447566db037ea7a86701438be4efec142502cbf
cintamani <cintamani.puddu@gmail.com>
2019-05-07 15:15:42 +0100

    Add faker setting to its own support file

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,8 +2,6 @@
 
 # Require this here to get factories to play nice with engine
 require "factory_bot_rails"
-# https://github.com/stympy/faker
-require "faker"
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
