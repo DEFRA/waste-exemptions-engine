b9bf9af3cb45a92d674300cddbbe78aff53a95cb
cintamani <cintamani.puddu@gmail.com>
2019-05-15 16:12:41 +0100

    Change headings and table layout on edit site location (#174)
    Addresses point 10 of QA document from https://eaflood.atlassian.net/browse/RUBY-62
    
    In order to address some consistency issues with the way we display site addresses,
    this change introduces an if statement to deal with different heading and table rows whether an address is located by grid reference or is inserted manually.

--- app/models/waste_exemptions_engine/address.rb
+++ app/models/waste_exemptions_engine/address.rb
@@ -2,6 +2,8 @@
 
 module WasteExemptionsEngine
   class Address < ActiveRecord::Base
+    include CanBeLocatedByGridReference
+
     has_paper_trail ignore: %i[blpu_state_code
                                logical_status_code
                                country_iso
@@ -13,9 +15,5 @@ module WasteExemptionsEngine
 
     enum address_type: { unknown: 0, operator: 1, contact: 2, site: 3 }
     enum mode: { unknown_mode: 0, lookup: 1, manual: 2, auto: 3 }
-
-    def located_by_grid_reference?
-      site? && auto?
-    end
   end
 end
