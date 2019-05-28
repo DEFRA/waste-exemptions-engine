87e874b5fe3a4f2d46c92d80a3211dd802aa9c4d
irisfaraway <iris.faraway@gmail.com>
2019-05-28 14:46:20 +0100

    Create a hash for business types
    Resolves https://github.com/DEFRA/waste-exemptions-engine/issues/195
    
    This commit creates a new HashWithIndifferentAccess for storing the string values for business types. It also starts using that hash in the checks in the module.

--- app/models/concerns/waste_exemptions_engine/can_have_registration_attributes.rb
+++ app/models/concerns/waste_exemptions_engine/can_have_registration_attributes.rb
@@ -4,13 +4,23 @@ module WasteExemptionsEngine
   module CanHaveRegistrationAttributes
     extend ActiveSupport::Concern
 
+    BUSINESS_TYPES = HashWithIndifferentAccess.new(
+      sole_trader: "soleTrader",
+      limited_company: "limitedCompany",
+      partnership: "partnership",
+      limited_liability_partnership: "limitedLiabilityPartnership",
+      local_authority: "localAuthority",
+      charity: "charity"
+    )
+
     # Some business types should not have a company_no
     def company_no_required?
-      %w[limitedCompany limitedLiabilityPartnership].include?(business_type)
+      [BUSINESS_TYPES[:limited_company],
+       BUSINESS_TYPES[:limited_liability_partnership]].include?(business_type)
     end
 
     def partnership?
-      %w[partnership].include?(business_type)
+      [BUSINESS_TYPES[:partnership]].include?(business_type)
     end
 
     def operator_address
