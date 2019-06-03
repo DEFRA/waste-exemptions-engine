204ef86d6df7489d0e54c12291b5d0b9e7835d86
cintamani <cintamani.puddu@gmail.com>
2019-06-03 10:52:00 +0100

    Allow a service main method to reeive no params

--- app/services/waste_exemptions_engine/base_service.rb
+++ app/services/waste_exemptions_engine/base_service.rb
@@ -2,8 +2,12 @@
 
 module WasteExemptionsEngine
   class BaseService
-    def self.run(attrs)
-      new.run(attrs)
+    def self.run(attrs=nil)
+      if attrs
+        new.run(attrs)
+      else
+        new.run
+      end
     end
   end
 end
