d9f4cac9914163497bcedd123123a70682e7f211
irisfaraway <iris.faraway@gmail.com>
2019-07-26 17:48:58 +0100

    Create form classes for new states
    There's very little to test here at the moment as these forms submit no data, although we do have precedent for testing whether flexible navigation is allowed. The request tests will hit these and provide coverage.

---
+++ spec/factories/forms/renewal_complete_form.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :renewal_complete_form, class: WasteExemptionsEngine::RenewalCompleteForm do
+    initialize_with do
+      new(create(:renewing_registration, workflow_state: "renewal_complete_form"))
+    end
+  end
+end
