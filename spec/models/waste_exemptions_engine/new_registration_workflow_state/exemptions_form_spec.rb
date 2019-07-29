0e4b8757e192eccd87cdb03f9e09229c3d5f98b6
cintamani <cintamani.puddu@gmail.com>
2019-07-29 11:52:56 +0100

    Change position of the Exemptions form (#239)
    * Change position of the Exemptions form
    
    From: https://eaflood.atlassian.net/browse/RUBY-457
    
    Change the state machine and fix tests so that the exemptions screen will now appear just after the location screens.
    
    Other fixes were necessary on the test suite for all this to run. The first thing that went wrong with them, is that removing the exemptions form between the site address and check your answers screen was generating an issue with the helper we use to fetch the correct `previous state` of a form. The code that was there was doing very little on fetching the correct previous status, was not accounting for multiple options, like is the case when dealing with the site_address, and was using "next" AASM events rather than "back" ones, so it was actually working just by chances rather than correct logic.
    Once that was fixed to return the correct previous state, it was now breaking the logic for "flexibly navigatable" tests in the "get form" share examples. This is because we rely on the get_previous_state to return a result for forms that should not have any previous state, like is the case for not flexibly navigatable forms, hence the fixes there.

--- spec/models/waste_exemptions_engine/new_registration_workflow_state/exemptions_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/exemptions_form_spec.rb
@@ -5,68 +5,11 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe NewRegistration, type: :model do
     describe "#workflow_state" do
-      next_state = :check_your_answers_form
-      current_state = :exemptions_form
-      let(:site_address) { build(:transient_address, :site_address) }
-      subject(:new_registration) do
-        create(:new_registration, workflow_state: current_state, addresses: [site_address])
-      end
-
-      context "when a NewRegistration's state is #{current_state}" do
-        it "changes to #{next_state} after the 'next' event" do
-          expect(new_registration).to transition_from(current_state).to(next_state).on_event(:next)
-        end
-
-        context "when neither new_registration.site_address_was_manually_entered? nor " \
-        "new_registration.site_address_was_entered? are true" do
-          previous_state = :site_grid_reference_form
-
-          it "can only transition to either #{previous_state} or #{next_state}" do
-            permitted_states = Helpers::WorkflowStates.permitted_states(new_registration)
-            expect(permitted_states).to match_array([previous_state, next_state])
-          end
-
-          it "changes to #{previous_state} after the 'back' event" do
-            expect(new_registration.send(:site_address_was_manually_entered?)).to eq(false)
-            expect(new_registration.send(:site_address_was_entered?)).to eq(false)
-            expect(new_registration).to transition_from(current_state).to(previous_state).on_event(:back)
-          end
-        end
-
-        context "when new_registration.site_address_was_manually_entered? is true" do
-          previous_state = :site_address_manual_form
-
-          before(:each) { new_registration.site_address.manual! }
-
-          it "can only transition to either #{previous_state} or #{next_state}" do
-            permitted_states = Helpers::WorkflowStates.permitted_states(new_registration)
-            expect(permitted_states).to match_array([previous_state, next_state])
-          end
-
-          it "changes to #{previous_state} after the 'back' event" do
-            expect(new_registration.send(:site_address_was_manually_entered?)).to eq(true)
-            expect(new_registration.send(:site_address_was_entered?)).to eq(false)
-            expect(new_registration).to transition_from(current_state).to(previous_state).on_event(:back)
-          end
-        end
-
-        context "when new_registration.site_address_was_entered? is true" do
-          previous_state = :site_address_lookup_form
-
-          before(:each) { new_registration.site_address.lookup! }
-
-          it "can only transition to either #{previous_state} or #{next_state}" do
-            permitted_states = Helpers::WorkflowStates.permitted_states(new_registration)
-            expect(permitted_states).to match_array([previous_state, next_state])
-          end
-
-          it "changes to #{previous_state} after the 'back' event" do
-            expect(new_registration.send(:site_address_was_manually_entered?)).to eq(false)
-            expect(new_registration.send(:site_address_was_entered?)).to eq(true)
-            expect(new_registration).to transition_from(current_state).to(previous_state).on_event(:back)
-          end
-        end
-      end
+      it_behaves_like "a simple bidirectional transition",
+                      previous_state: :location_form,
+                      current_state: :exemptions_form,
+                      next_state: :applicant_name_form,
+                      factory: :new_registration
     end
   end
 end
