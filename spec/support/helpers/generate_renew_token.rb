58f8cff997d69c144e0609f9a15b1e2ca49aa241
cintamani <cintamani.puddu@gmail.com>
2019-07-31 15:08:25 +0100

    Rename file, add self. to module methods so that they can be called directly

--- spec/support/helpers/generate_renew_token.rb
+++ spec/support/helpers/generate_renew_token.rb
@@ -4,25 +4,25 @@ require "jwt"
 
 module Helpers
   module GenerateRenewToken
-    def generate_valid_renew_token(registration)
+    def self.generate_valid_renew_token(registration)
       payload = { data: { registration_id: registration.id }, exp: Time.now.to_i + 100 }
 
       generate_test_renew_token(registration, payload)
     end
 
-    def generate_expired_renew_token(registration)
+    def self.generate_expired_renew_token(registration)
       payload = { data: { registration_id: registration.id }, exp: Time.now.to_i - 1000 }
 
       generate_test_renew_token(registration, payload)
     end
 
-    def generate_invalid_payload_renew_token(registration)
+    def self.generate_invalid_payload_renew_token(registration)
       payload = {}
 
       generate_test_renew_token(registration, payload)
     end
 
-    def generate_test_renew_token(registration, payload)
+    def self.generate_test_renew_token(registration, payload)
       registration.renew_token = JWT.encode(payload, Rails.application.secrets.secret_key_base, "HS384")
 
       registration.save
@@ -30,7 +30,7 @@ module Helpers
       registration.renew_token
     end
 
-    def generate_renew_token_without_updating_registration(registration)
+    def self.generate_renew_token_without_updating_registration(registration)
       payload = { data: { registration_id: registration.id }, exp: Time.now.to_i - 100 }
 
       JWT.encode(payload, Rails.application.secrets.secret_key_base, "HS384")
