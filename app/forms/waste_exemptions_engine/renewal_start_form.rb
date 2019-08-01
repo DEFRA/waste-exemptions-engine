98bf2f579ab77263cf0f6d1d3d3d83058f281750
irisfaraway <iris.faraway@gmail.com>
2019-08-01 18:21:09 +0100

    Persist and validate the user's selection on the form

--- app/forms/waste_exemptions_engine/renewal_start_form.rb
+++ app/forms/waste_exemptions_engine/renewal_start_form.rb
@@ -4,8 +4,19 @@ module WasteExemptionsEngine
   class RenewalStartForm < BaseForm
     attr_accessor :temp_renew_without_changes
 
-    def submit(params)
-      super({}, params[:token])
+    def initialize(registration)
+      super
+      self.temp_renew_without_changes = @transient_registration.temp_renew_without_changes
     end
+
+    def submit(params)
+      # Assign the params for validation and pass them to the BaseForm method for updating
+      self.temp_renew_without_changes = params[:temp_renew_without_changes]
+      attributes = { temp_renew_without_changes: temp_renew_without_changes }
+
+      super(attributes, params[:token])
+    end
+
+    validates :temp_renew_without_changes, "defra_ruby/validators/true_false": true
   end
 end
