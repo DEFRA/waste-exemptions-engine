3a3e4730c02c9e61a402c4a4a5fd7187ab7dbd38
irisfaraway <iris.faraway@gmail.com>
2019-08-01 18:09:46 +0100

    Add radio button options the page

--- app/forms/waste_exemptions_engine/renewal_start_form.rb
+++ app/forms/waste_exemptions_engine/renewal_start_form.rb
@@ -2,6 +2,8 @@
 
 module WasteExemptionsEngine
   class RenewalStartForm < BaseForm
+    attr_accessor :temp_renew_without_changes
+
     def submit(params)
       super({}, params[:token])
     end
