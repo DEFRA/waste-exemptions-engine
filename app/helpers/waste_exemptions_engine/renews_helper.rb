3d7e6d68649b91a81c4bf18081219c3036e8479c
cintamani <cintamani.puddu@gmail.com>
2019-08-06 13:02:54 +0100

    Prevent user to renew again
    From: https://eaflood.atlassian.net/browse/RUBY-511
    
    Prevent the user from renew a registration that has already been renewed and present them an informative page.

---
+++ app/helpers/waste_exemptions_engine/renews_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  module RenewsHelper
+    def make_a_new_registration_link
+      link_to t(".make_a_new_registration"), "/"
+    end
+  end
+end
