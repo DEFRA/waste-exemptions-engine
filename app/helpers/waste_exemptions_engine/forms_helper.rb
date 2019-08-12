c3428ced79685c3b4070549e723b04966604bf6c
irisfaraway <iris.faraway@gmail.com>
2019-08-12 11:15:50 +0100

    Determine correct journey value for data layer

--- app/helpers/waste_exemptions_engine/forms_helper.rb
+++ app/helpers/waste_exemptions_engine/forms_helper.rb
@@ -2,8 +2,23 @@
 
 module WasteExemptionsEngine
   module FormsHelper
-    def data_layer
-      {}
+    def data_layer(transient_registration)
+      {
+        journey: data_layer_value_for_journey(transient_registration)
+      }
+    end
+
+    private
+
+    def data_layer_value_for_journey(transient_registration)
+      case transient_registration.class.name
+      when "WasteExemptionsEngine::EditRegistration"
+        :edit
+      when "WasteExemptionsEngine::NewRegistration"
+        :new
+      when "WasteExemptionsEngine::RenewingRegistration"
+        :renew
+      end
     end
   end
 end
--- spec/helpers/waste_exemptions_engine/forms_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/forms_helper_spec.rb
@@ -5,8 +5,34 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe FormsHelper, type: :helper do
     describe "data_layer" do
-      it "returns the correct value" do
-        expect(helper.data_layer).to eq({})
+      context "when the transient_registration is an EditRegistration" do
+        let(:transient_registration) { build(:edit_registration) }
+
+        it "returns the correct value" do
+          expected_hash = { journey: :edit }
+
+          expect(helper.data_layer(transient_registration)).to eq(expected_hash)
+        end
+      end
+
+      context "when the transient_registration is a NewRegistration" do
+        let(:transient_registration) { build(:new_registration) }
+
+        it "returns the correct value" do
+          expected_hash = { journey: :new }
+
+          expect(helper.data_layer(transient_registration)).to eq(expected_hash)
+        end
+      end
+
+      context "when the transient_registration is a RenewingRegistration" do
+        let(:transient_registration) { build(:renewing_registration) }
+
+        it "returns the correct value" do
+          expected_hash = { journey: :renew }
+
+          expect(helper.data_layer(transient_registration)).to eq(expected_hash)
+        end
       end
     end
   end
