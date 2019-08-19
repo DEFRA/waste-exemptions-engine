b6987f380fe4e84b1289286cd833c30f5e122cde
irisfaraway <iris.faraway@gmail.com>
2019-08-19 09:50:33 +0100

    Move matcher to existing file

--- spec/support/form_matchers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rspec/expectations"
-
-RSpec::Matchers.define :have_html_escaped_string do |expected|
-  match do |actual|
-    actual.include?(CGI.escape_html(expected))
-  end
-end
