474b247ef4fb1f4a1a475d276693388aa5b9352e
cintamani <cintamani.puddu@gmail.com>
2019-08-29 10:55:32 +0100

    Fix tests

--- spec/helpers/waste_exemptions_engine/application_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/application_helper_spec.rb
@@ -2,10 +2,12 @@
 
 require "rails_helper"
 
-RSpec.describe WasteExemptionsEngine::ApplicationHelper, type: :helper do
-  describe "#format_names" do
-    it "concatenates first and last name into a full name" do
-      expect(helper.format_names("Fiona", "Laurel")).to eq("Fiona Laurel")
+module WasteExemptionsEngine
+  RSpec.describe ApplicationHelper, type: :helper do
+    describe "#format_names" do
+      it "concatenates first and last name into a full name" do
+        expect(helper.format_names("Fiona", "Laurel")).to eq("Fiona Laurel")
+      end
     end
   end
 end
