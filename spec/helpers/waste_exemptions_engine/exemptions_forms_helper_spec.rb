463f8705184e2c4926118532d35ee6cf51950764
cintamani <cintamani.puddu@gmail.com>
2019-08-30 12:50:26 +0100

    Prevent random failure of scenario (#329)
    I have observed that we randomly fail the scenario subject of this PR because the DB is in a dirty status.

--- spec/helpers/waste_exemptions_engine/exemptions_forms_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/exemptions_forms_helper_spec.rb
@@ -6,6 +6,10 @@ module WasteExemptionsEngine
   RSpec.describe ExemptionsFormsHelper, type: :helper do
     describe "#all_exemptions" do
       it "returns a list of all exemptions ordered by ID" do
+        # FIXME: There are still tests leaving the DB dirty with exemptions.
+        # This line will prevent random failure of this scenario
+        WasteExemptionsEngine::Exemption.delete_all
+
         exemptions = create_list(:exemption, 3)
         expect(helper.all_exemptions).to eq(exemptions)
       end
