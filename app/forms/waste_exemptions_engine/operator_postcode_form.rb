f7155b58c73a415242f70f61d6389d1492ac3b99
cintamani <cintamani.puddu@gmail.com>
2019-09-02 13:59:53 +0100

    Operator postcode form refactoring
    This removes inheritance between the operator postcode and the base postcode form. It also renames the parameters on the operator postocode to match the attribute name on the transient registration

--- app/forms/waste_exemptions_engine/operator_postcode_form.rb
+++ app/forms/waste_exemptions_engine/operator_postcode_form.rb
@@ -1,20 +1,32 @@
 # frozen_string_literal: true
 
 module WasteExemptionsEngine
-  class OperatorPostcodeForm < PostcodeForm
-    attr_accessor :business_type
+  class OperatorPostcodeForm < BaseForm
+    attr_accessor :business_type, :temp_operator_postcode
+
+    validates :temp_operator_postcode, "waste_exemptions_engine/postcode": true
 
     def initialize(registration)
       super
 
       # We only use this for the correct microcopy
       self.business_type = @transient_registration.business_type
+      self.temp_operator_postcode = @transient_registration.temp_operator_postcode
     end
 
-    private
+    def submit(params)
+      # Assign the params for validation and pass them to the BaseForm method
+      # for updating
+      self.temp_operator_postcode = format_postcode(params[:temp_operator_postcode])
 
-    def existing_postcode
-      @transient_registration.temp_operator_postcode
+      # We pass through an empty hash for the attributes, as there is nothing to
+      # update on the registration itself
+      super({ temp_operator_postcode: temp_operator_postcode })
+    end
+
+    # TODO: Move in AddressHelper?
+    def format_postcode(postcode)
+      postcode.upcase.strip
     end
   end
 end
