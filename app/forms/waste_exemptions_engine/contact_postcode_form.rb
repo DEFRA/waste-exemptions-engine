4931c96b0d875c7447bc19e3f859f6d21dff508e
cintamani <cintamani.puddu@gmail.com>
2019-09-04 12:43:13 +0100

    Contact postcode form refactoring (#333)
    * Contact postcode form refactoring
    
    This removes inheritance between the contact postcode and the base postcode form. It also renames the parameters on the contact postocode to match the attribute name on the transient registration

--- app/forms/waste_exemptions_engine/contact_postcode_form.rb
+++ app/forms/waste_exemptions_engine/contact_postcode_form.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
 module WasteExemptionsEngine
-  class ContactPostcodeForm < PostcodeForm
+  class ContactPostcodeForm < BasePostcodeForm
+    attr_accessor :business_type, :temp_contact_postcode
 
-    private
+    validates :temp_contact_postcode, "waste_exemptions_engine/postcode": true
 
-    def existing_postcode
-      @transient_registration.temp_contact_postcode
+    def initialize(registration)
+      super
+
+      # We only use this for the correct microcopy
+      self.business_type = @transient_registration.business_type
+      self.temp_contact_postcode = @transient_registration.temp_contact_postcode
+    end
+
+    def submit(params)
+      self.temp_contact_postcode = format_postcode(params[:temp_contact_postcode])
+
+      # We persist the postcode regardless of validations.
+      transient_registration.update_attributes(temp_contact_postcode: temp_contact_postcode)
+
+      super({})
     end
   end
 end
