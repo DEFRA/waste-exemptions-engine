3129faa7b69bbdfc4428df081883734ce3210767
cintamani <cintamani.puddu@gmail.com>
2019-09-23 22:05:55 +0100

    Add scopes to the address models for area lookup
    From: https://eaflood.atlassian.net/browse/RUBY-487
    
    Add scopes to find addresses missing areas and with valid easting and northing parameters

--- spec/models/waste_exemptions_engine/address_spec.rb
+++ spec/models/waste_exemptions_engine/address_spec.rb
@@ -16,6 +16,33 @@ module WasteExemptionsEngine
       end
     end
 
+    context "scopes" do
+      describe ".with_easting_and_northing" do
+        it "returns all address with x and y information" do
+          create(:address, x: nil, y: 123.4)
+          create(:address, x: 123.4, y: nil)
+
+          valid_address = create(:address, x: 123.4, y: 123.4)
+
+          expect(described_class.with_easting_and_northing.size).to eq(1)
+          expect(described_class.with_easting_and_northing.first).to eq(valid_address)
+        end
+      end
+
+      describe ".missing_ea_area" do
+        it "returns all address with a missing EA area" do
+          create(:address, area: "West Midlands")
+
+          nil_area = create(:address, area: nil)
+          empty_area = create(:address, area: "")
+
+          expect(described_class.missing_ea_area.size).to eq(2)
+          expect(described_class.missing_ea_area).to include(empty_area)
+          expect(described_class.missing_ea_area).to include(nil_area)
+        end
+      end
+    end
+
     describe "#located_by_grid_reference?" do
       subject { described_class.new(mode: mode, address_type: :site) }
 
