eb6730b6d5c714f527c602326962c4166f3cdb39
cintamani <cintamani.puddu@gmail.com>
2019-09-24 11:49:26 +0100

    Apply suggestions from code review

--- app/models/waste_exemptions_engine/address.rb
+++ app/models/waste_exemptions_engine/address.rb
@@ -11,6 +11,6 @@ module WasteExemptionsEngine
     enum address_type: { unknown: 0, operator: 1, contact: 2, site: 3 }
     enum mode: { unknown_mode: 0, lookup: 1, manual: 2, auto: 3 }
 
-    scope :missing_easting_and_northing, -> { where("x IS NULL OR y IS NULL") }
+    scope :missing_easting_or_northing, -> { where("x IS NULL OR y IS NULL") }
   end
 end
--- spec/models/waste_exemptions_engine/address_spec.rb
+++ spec/models/waste_exemptions_engine/address_spec.rb
@@ -17,7 +17,7 @@ module WasteExemptionsEngine
     end
 
     context "scopes" do
-      describe ".missing_easting_and_northing" do
+      describe ".missing_easting_or_northing" do
         it "returns all address with x and y information" do
           missing_info_records = []
           missing_info_records << create(:address, x: nil, y: 123.4)
@@ -26,7 +26,7 @@ module WasteExemptionsEngine
 
           create(:address, x: 123.4, y: 123.4)
 
-          expect(described_class.missing_easting_and_northing).to match_array(missing_info_records)
+          expect(described_class.missing_easting_or_northing).to match_array(missing_info_records)
         end
       end
     end
