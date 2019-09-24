53a145db52bd4f9ee59d9b3b9e015c5a394705aa
cintamani <cintamani.puddu@gmail.com>
2019-09-24 11:51:25 +0100

    Add scopes to the address models for area lookup (#358)
    * Add scopes to the address models for area lookup
    
    From: https://eaflood.atlassian.net/browse/RUBY-487
    
    Add scopes to find addresses missing areas and with valid easting and northing parameters

--- app/models/waste_exemptions_engine/address.rb
+++ app/models/waste_exemptions_engine/address.rb
@@ -10,5 +10,8 @@ module WasteExemptionsEngine
 
     enum address_type: { unknown: 0, operator: 1, contact: 2, site: 3 }
     enum mode: { unknown_mode: 0, lookup: 1, manual: 2, auto: 3 }
+
+    scope :with_easting_and_northing, -> { where.not(x: nil, y: nil) }
+    scope :missing_area, -> { where(area: [nil, ""]) }
   end
 end
