8cc6234b4611fe76f0da0ee74a00fffb8d6258b0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-09-26 14:34:17 +0100

    Update and rename scope for sites missing x & y

--- app/models/waste_exemptions_engine/address.rb
+++ app/models/waste_exemptions_engine/address.rb
@@ -11,7 +11,7 @@ module WasteExemptionsEngine
     enum address_type: { unknown: 0, operator: 1, contact: 2, site: 3 }
     enum mode: { unknown_mode: 0, lookup: 1, manual: 2, auto: 3 }
 
-    scope :missing_easting_or_northing, -> { where("x IS NULL OR y IS NULL") }
+    scope :sites_missing_easting_or_northing, -> { where("address_type = 3 AND (x IS NULL OR y IS NULL)") }
     scope :with_easting_and_northing, -> { where.not(x: nil, y: nil) }
     scope :missing_area, -> { where(area: [nil, ""]) }
   end
