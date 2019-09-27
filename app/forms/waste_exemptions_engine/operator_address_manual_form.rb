9ddbf7aa1cc60d586e267d394577621d7d00f7ab
cintamani <cintamani.puddu@gmail.com>
2019-09-27 11:57:13 +0200

    Operator Address manual form refactoring (#367)
    Following on: https://github.com/DEFRA/waste-exemptions-engine/pull/341/files
    This apply the same refactoing to the operator address manual form.
    The goal is to make use of rails-ness on these forms, so we want them to name the attributes as per TransientAddress attributes. This will allow the use of strong parameters and less code to custom-submit these forms rather than letting rails do the job for us :D

--- app/forms/waste_exemptions_engine/operator_address_manual_form.rb
+++ app/forms/waste_exemptions_engine/operator_address_manual_form.rb
@@ -1,7 +1,63 @@
 # frozen_string_literal: true
 
 module WasteExemptionsEngine
-  class OperatorAddressManualForm < AddressManualForm
-    include OperatorAddressForm
+  class OperatorAddressManualForm < BaseForm
+    attr_accessor :address_finder_error
+    attr_accessor :operator_address
+    attr_accessor :premises, :street_address, :locality, :postcode, :city, :business_type
+
+    validates :operator_address, "waste_exemptions_engine/manual_address": true
+
+    def initialize(registration)
+      super
+
+      self.postcode = transient_registration.temp_operator_postcode
+
+      self.operator_address = @transient_registration.operator_address
+
+      # Check if the user reached this page through an Address finder error.
+      # Then wipe the temp attribute as we only need it for routing
+      self.address_finder_error = @transient_registration.address_finder_error
+      transient_registration.update_attributes(address_finder_error: nil)
+
+      # Prefill the existing address unless the postcode has changed from the existing address's postcode
+      prefill_operator_address_params if saved_address_still_valid?
+    end
+
+    def submit(params)
+      permitted_attributes = params.require(:operator_address)
+      permitted_attributes = permitted_attributes.permit(:locality, :postcode, :city, :premises, :street_address, :mode)
+
+      # Needed for validation
+      assign_params(permitted_attributes)
+
+      super(operator_address_attributes: permitted_attributes)
+    end
+
+    private
+
+    def assign_params(permitted_attributes)
+      self.operator_address = transient_registration.build_operator_address(permitted_attributes)
+
+      prefill_operator_address_params
+    end
+
+    def saved_address_still_valid?
+      return false unless operator_address
+      return true if postcode.blank?
+      return true if postcode == operator_address.postcode
+
+      false
+    end
+
+    def prefill_operator_address_params
+      return unless operator_address
+
+      self.premises = operator_address.premises&.strip
+      self.street_address = operator_address.street_address&.strip
+      self.locality = operator_address.locality&.strip
+      self.city = operator_address.city&.strip
+      self.postcode = operator_address.postcode&.strip
+    end
   end
 end
