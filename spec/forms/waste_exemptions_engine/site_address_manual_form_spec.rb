5b68387869c3a5de19b884d8783179c76a662abf
cintamani <cintamani.puddu@gmail.com>
2019-09-27 11:36:29 +0200

    Site Address manual form refactoring (#368)
    Following on: https://github.com/DEFRA/waste-exemptions-engine/pull/341/files
    This apply the same refactoing to the site address manual form.
    The goal is to make use of rails-ness on these forms, so we want them to name the attributes as per TransientAddress attributes. This will allow the use of strong parameters and less code to custom-submit these forms rather than letting rails do the job for us :D

--- spec/forms/waste_exemptions_engine/site_address_manual_form_spec.rb
+++ spec/forms/waste_exemptions_engine/site_address_manual_form_spec.rb
@@ -4,13 +4,130 @@ require "rails_helper"
 
 module WasteExemptionsEngine
   RSpec.describe SiteAddressManualForm, type: :model do
-    it_behaves_like "a manual address form", :site_address_manual_form
+    let(:form_factory) { :site_address_manual_form }
 
-    it "includes SiteAddressForm" do
-      included_modules = described_class.ancestors.select { |ancestor| ancestor.instance_of?(Module) }
+    it "validates the address data using the ManualAddressValidator class" do
+      validators = build(form_factory)._validators
+      validator_classes = validators.values.flatten.map(&:class)
+      expect(validator_classes).to include(WasteExemptionsEngine::ManualAddressValidator)
+    end
 
-      expect(included_modules)
-        .to include(WasteExemptionsEngine::SiteAddressForm)
+    before(:each) { VCR.insert_cassette("postcode_valid") }
+    after(:each) { VCR.eject_cassette }
+
+    it_behaves_like "a validated form", :site_address_manual_form do
+      let(:valid_params) do
+        [
+          {
+            token: form.token,
+            site_address: {
+              premises: "Horizon House",
+              street_address: "Deanery Rd",
+              locality: "Bristol",
+              city: "Bristol",
+              postcode: "BS1 5AH"
+            }
+          },
+          {
+            token: form.token,
+            site_address: {
+              premises: "Horizon House",
+              street_address: "Deanery Rd",
+              locality: "",
+              city: "Bristol",
+              postcode: ""
+            }
+          }
+        ]
+      end
+      let(:invalid_params) do
+        [
+          {
+            token: form.token,
+            site_address: {
+              premises: "",
+              street_address: "",
+              locality: "",
+              city: "",
+              postcode: ""
+            }
+          },
+          {
+            token: form.token,
+            site_address: {
+              premises: Helpers::TextGenerator.random_string(201), # The max length is 200.
+              street_address: Helpers::TextGenerator.random_string(161), # The max length is 160.
+              locality: Helpers::TextGenerator.random_string(71), # The max length is 70.
+              city: Helpers::TextGenerator.random_string(31), # The max length is 30.
+              postcode: Helpers::TextGenerator.random_string(9) # The max length is 8.
+            }
+          }
+        ]
+      end
+    end
+
+    # Make sure the transient registration gets updated when submitted.
+    describe "#submit" do
+      context "when the form is valid" do
+        subject(:form) { build(form_factory) }
+        let(:address_data) do
+          {
+            site_address: {
+              premises: "Example House",
+              street_address: "2 On The Road",
+              locality: "Near Horizon House",
+              city: "Bristol",
+              postcode: "BS1 5AH"
+            }
+          }
+        end
+        let(:white_space_address_data) do
+          {
+            site_address: {
+              premises: "  Example House ",
+              street_address: " 2 On The Road  ",
+              locality: " Near Horizon House   ",
+              city: "  Bristol  ",
+              postcode: "   BS1 5AH  "
+            }
+          }
+        end
+        let(:valid_params) { address_data.merge(token: form.token) }
+        let(:white_space_params) { white_space_address_data.merge(token: form.token) }
+        let(:transient_registration) { form.transient_registration }
+
+        it "updates the transient registration with the submitted address data" do
+          # Ensure the test data is properly configured:
+          expect(transient_registration.transient_addresses).to be_empty
+
+          form.submit(ActionController::Parameters.new(valid_params))
+
+          expect(transient_registration.transient_addresses.count).to eq(1)
+          submitted_address = transient_registration.transient_addresses.first
+          address_data[:site_address].each do |key, value|
+            expect(submitted_address.send(key)).to eq(value)
+          end
+        end
+
+        context "when the address data includes extraneous white space" do
+          it "strips the extraneous white space from the submitted address data" do
+            # Ensure the test data is properly configured:
+            address_data[:site_address].each do |key, value|
+              expect(white_space_params[:site_address][key]).not_to eq(value)
+              expect(white_space_params[:site_address][key].strip).to eq(value)
+            end
+            expect(transient_registration.transient_addresses).to be_empty
+
+            form.submit(ActionController::Parameters.new(white_space_params))
+
+            expect(transient_registration.reload.transient_addresses.count).to eq(1)
+            submitted_address = transient_registration.transient_addresses.first
+            address_data[:site_address].each do |key, value|
+              expect(submitted_address.send(key)).to eq(value)
+            end
+          end
+        end
+      end
     end
   end
 end
