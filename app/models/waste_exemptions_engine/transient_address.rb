f9d3bbab76c95cbb59b8843b904ddb1ee8df2bf4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-10-08 15:58:56 +0100

    Refactor hooks to have 1 site check & 1 save call
    We've refactored the calls to multiple after_create methods into a call to one. Now we only perform the `site?` check and call to `save!` to persist the changes once.
    
    The downside is we have complexity issues with rubocop. I've chosen to add an exception here as I think what is currently there makes sense, and the reasons provided are valid.
    
    However I'm open to suggestions. If there is a way we can reduce the calls to the db and the duplication whilst still solving the complexity issue properly I would love to hear it!

--- app/models/waste_exemptions_engine/transient_address.rb
+++ app/models/waste_exemptions_engine/transient_address.rb
@@ -13,7 +13,7 @@ module WasteExemptionsEngine
     enum address_type: { unknown: 0, operator: 1, contact: 2, site: 3 }
     enum mode: { unknown_mode: 0, lookup: 1, manual: 2, auto: 3 }
 
-    after_create :update_x_and_y, :update_grid_reference, :update_area
+    after_create :update_site_details
 
     def address_attributes
       attributes.except("id", "transient_registration_id", "created_at", "updated_at")
@@ -45,50 +45,42 @@ module WasteExemptionsEngine
 
     private
 
-    def update_x_and_y
+    # Disabling the following cops for these reasons.
+    #
+    # The part of the code that tips the balance to being too complex is the
+    # `if foo.blank? checks after each method call. Take them away and we're
+    # fine.
+    #
+    # The first issue is if we move those checks into the methods, then the
+    # complexity warnings move to them instead. We would argue that actually
+    # neither this method, nor the methods we call here are actually that
+    # complex and it would make things worse not better to break things up
+    # further.
+    #
+    # Secondly we have a nice separation between what the checks represent. Here
+    # our checks are determining whther we need to run the updates or not. The
+    # update methods themselves don't care if the fields they are updating are
+    # populated or not, they just care whether they have the data they need to
+    # run.
+    #
+    # By splitting them up in this way we retain the flexibility to call these
+    # methods to update the fields irrespectibe of their current state. And
+    # we reduce what they need to know to just 'have i got the data I need'.
+    #
+    # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/PerceivedComplexity
+    def update_site_details
       return unless site?
-      return unless x.blank? || y.blank?
 
-      update_x_and_y_from_grid_reference if auto?
-      update_x_and_y_from_postcode if manual?
+      update_x_and_y_from_grid_reference if x.blank? || y.blank?
+      update_x_and_y_from_postcode if x.blank? || y.blank?
+      update_grid_reference_from_x_and_y if grid_reference.blank?
+      update_area_from_x_and_y if area.blank?
 
       save!
     end
-
-    def update_grid_reference
-      return unless site?
-      return unless grid_reference.blank?
-
-      update_grid_reference_from_x_and_y
-
-      save!
-    end
-
-    def update_area
-      return unless site?
-
-      update_area_from_x_and_y
-
-      save!
-    end
-
-    def update_area_from_x_and_y
-      return if x.blank? || y.blank?
-
-      # The AreaLookup service handles errors and notifies Errbit in the event
-      # of an error. This is why unlike other methods we don't have a rescue here
-      self.area = AreaLookupService.run(easting: x, northing: y)
-    end
-
-    def update_grid_reference_from_x_and_y
-      return if x.blank? || y.blank?
-
-      location = OsMapRef::Location.for("#{x}, #{y}")
-      self.grid_reference = location.map_reference
-    rescue OsMapRef::Error => e
-      self.grid_reference = ""
-      handle_error(e, "X & Y to grid reference failed:\n #{e}", x: x, y: y)
-    end
+    # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocop:enable Metrics/PerceivedComplexity
 
     def update_x_and_y_from_grid_reference
       return if grid_reference.blank?
@@ -112,6 +104,24 @@ module WasteExemptionsEngine
 
       self.x = results.first["x"].to_f
       self.y = results.first["y"].to_f
+    end
+
+    def update_grid_reference_from_x_and_y
+      return if x.blank? || y.blank?
+
+      location = OsMapRef::Location.for("#{x}, #{y}")
+      self.grid_reference = location.map_reference
+    rescue OsMapRef::Error => e
+      self.grid_reference = ""
+      handle_error(e, "X & Y to grid reference failed:\n #{e}", x: x, y: y)
+    end
+
+    def update_area_from_x_and_y
+      return if x.blank? || y.blank?
+
+      # The AreaLookup service handles errors and notifies Errbit in the event
+      # of an error. This is why unlike other methods we don't have a rescue here
+      self.area = AreaLookupService.run(easting: x, northing: y)
     end
 
     def error_from_postcode_lookup_for_x_and_y_update(results)
