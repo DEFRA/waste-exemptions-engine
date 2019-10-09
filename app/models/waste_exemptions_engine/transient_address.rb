cd93fc177b19fa9a72b738c02ef0aa892b56e0ac
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-10-09 10:23:09 +0100

    Add method that determines if x & y need updating
    This also has the benefit of removing the last rubocop complexity issue.

--- app/models/waste_exemptions_engine/transient_address.rb
+++ app/models/waste_exemptions_engine/transient_address.rb
@@ -41,38 +41,18 @@ module WasteExemptionsEngine
 
     private
 
-    # Disabling the following cops for these reasons.
-    #
-    # The part of the code that tips the balance to being too complex is the
-    # `if foo.blank? checks after each method call. Take them away and we're
-    # fine.
-    #
-    # The first issue is if we move those checks into the methods, then the
-    # complexity warnings move to them instead. We would argue that actually
-    # neither this method, nor the methods we call here are actually that
-    # complex and it would make things worse not better to break things up
-    # further.
-    #
-    # Secondly we have a nice separation between what the checks represent. Here
-    # our checks are determining whther we need to run the updates or not. The
-    # update methods themselves don't care if the fields they are updating are
-    # populated or not, they just care whether they have the data they need to
-    # run.
-    #
-    # By splitting them up in this way we retain the flexibility to call these
-    # methods to update the fields irrespectibe of their current state. And
-    # we reduce what they need to know to just 'have i got the data I need'.
-    #
-    # rubocop:disable Metrics/CyclomaticComplexity
     def update_site_details
-      update_x_and_y_from_grid_reference if x.blank? || y.blank?
-      update_x_and_y_from_postcode if x.blank? || y.blank?
+      update_x_and_y_from_grid_reference if update_x_and_y?
+      update_x_and_y_from_postcode if update_x_and_y?
       update_grid_reference_from_x_and_y if grid_reference.blank?
       update_area_from_x_and_y if area.blank?
 
       save!
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
+
+    def update_x_and_y?
+      x.blank? || y.blank?
+    end
 
     def update_x_and_y_from_grid_reference
       return if grid_reference.blank?
