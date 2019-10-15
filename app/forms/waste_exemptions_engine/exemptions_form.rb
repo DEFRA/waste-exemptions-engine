45ae3b97149800484e1210db78b66d1deb954739
cintamani <cintamani.puddu@gmail.com>
2019-10-15 12:24:26 +0100

    Apply suggestions from code review

--- app/forms/waste_exemptions_engine/exemptions_form.rb
+++ app/forms/waste_exemptions_engine/exemptions_form.rb
@@ -7,8 +7,8 @@ module WasteExemptionsEngine
     validates :exemptions, "waste_exemptions_engine/exemptions": true
 
     def submit(params)
-      # TODO: This line here is in order to by-pass the Rails 4 bug for which 
-      # empty parameters are parsed as nil. 
+      # TODO: This line here is in order to by-pass the Rails 4 bug for which
+      # empty parameters are parsed as nil.
       # This is fixed in Rails 5, so whenever we upgrade we can also get rid of this
       params[:exemption_ids] ||= []
 
