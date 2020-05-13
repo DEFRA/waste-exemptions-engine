e84e1ceb0d235152e5dc436c07add3ccf1c03e19
cintamani <cintamani.puddu@gmail.com>
2020-05-13 11:45:55 +0100

    Refactor request tests to compact them
    We decided a while ago that we whould compact all scenarios in the same context to one, in order to make only one request per context and so speed up our test suite. This apply this principle to the test suite.

--- spec/requests/waste_exemptions_engine/errors_spec.rb
+++ spec/requests/waste_exemptions_engine/errors_spec.rb
@@ -6,24 +6,18 @@ module WasteExemptionsEngine
   RSpec.describe "Errors", type: :request do
     describe "#show" do
       %w[401 403 404 422].each do |code|
-        it "renders the error_#{code} template" do
+        it "renders the error_#{code} template, responds with a status of #{code}, returns W3C valid HTML content", vcr: true do
           get error_path(code)
+
           expect(response).to render_template("error_#{code}")
-        end
-
-        it "responds with a status of #{code}" do
-          get error_path(code)
           expect(response.code).to eq(code)
-        end
-
-        it "returns W3C valid HTML content", vcr: true do
-          get error_path(code)
           expect(response.body).to have_valid_html
         end
       end
 
       it "renders the generic error template when no matching error template exists" do
         get error_path("601")
+
         expect(response.code).to eq("500")
         expect(response).to render_template(:error_generic)
       end
--- spec/support/shared_examples/requests/skip_to_manual_address.rb
+++ spec/support/shared_examples/requests/skip_to_manual_address.rb
@@ -7,13 +7,10 @@ RSpec.shared_examples "skip to manual address" do |form_factory, address_type:|
   status_code = WasteExemptionsEngine::ApplicationController::SUCCESSFUL_REDIRECTION_CODE
 
   describe "GET #{form_factory} skip to manual address" do
-    it "renders the appropriate template" do
+    it "renders the appropriate template and returns a #{status_code} status code" do
       get manual_address_request_path
-      expect(response.location).to include(manual_address_form_path)
-    end
 
-    it "responds to the GET request with a #{status_code} status code" do
-      get manual_address_request_path
+      expect(response.location).to include(manual_address_form_path)
       expect(response.code).to eq(status_code.to_s)
     end
   end
