31a6aca5dd914745f674a71f9acd43c27f47f1e1
cintamani <cintamani.puddu@gmail.com>
2020-06-19 15:40:06 +0100

    Comment out profiling code and add comment on how to use

--- spec/support/ruby-prof.rb
+++ spec/support/ruby-prof.rb
@@ -1,20 +1,20 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require "ruby-prof"
+# To create profiling reports, uncomment this code.
 
-RSpec.configure do |config|
-  # Clean the database before running tests. Setup as per
-  # https://github.com/DatabaseCleaner/database_cleaner#rspec-example
-  config.before(:suite) do
-    RubyProf.start
-  end
+# require "ruby-prof"
 
-  config.after(:suite) do |example|
-    result = RubyProf.stop
+# RSpec.configure do |config|
+#   # Clean the database before running tests. Setup as per
+#   # https://github.com/DatabaseCleaner/database_cleaner#rspec-example
+#   config.before(:suite) do
+#     RubyProf.start
+#   end
 
-    printer = RubyProf::MultiPrinter.new(result)
-    printer.print(:path => ".", :profile => "profile")
-    # printer.print(File.open)
-    # printer.print(STDOUT)
-  end
-end
+#   config.after(:suite) do |example|
+#     result = RubyProf.stop
+
+#     printer = RubyProf::MultiPrinter.new(result)
+#     printer.print(:path => ".", :profile => "profile")
+#   end
+# end
