a61ac3fed08ac9824b690f1e5b503cb12a12e69a
irisfaraway <iris.faraway@gmail.com>
2020-08-28 12:41:09 +0100

    Set up ConfirmationEmailService
    https://eaflood.atlassian.net/browse/RUBY-1183
    
    This is the service that will replace the ConfirmationMailer. It sends a request to Notify to send out the new confirmation email.
    
    Previously we had a fallback in case the certificate PDF wasn't generated properly. It's not possible to handle this within the same Notify template so instead we request a different email template depending on whether the PDF is generated successfully or not.

---
+++ spec/services/waste_exemptions_engine/confirmation_email_service_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe ConfirmationEmailService do
+    describe "run" do
+      # Keep reference the same so it doesn't mess up the cassette
+      let(:registration) { create(:registration, :complete, reference: "FOO") }
+      let(:recipient) { registration.contact_email }
+      let(:service) do
+        ConfirmationEmailService.run(registration: registration,
+                                     recipient: recipient)
+      end
+
+      context "when the PDF is generated" do
+        it "sends an email" do
+          VCR.use_cassette("confirmation_email") do
+            expect_any_instance_of(Notifications::Client).to receive(:send_email).and_call_original
+
+            response = service
+
+            expect(response).to be_a(Notifications::Client::ResponseNotification)
+            expect(response.template["id"]).to eq("98d5dcee-ea29-415f-952e-b8e287555e10")
+            expect(response.content["subject"]).to eq("Waste exemptions registration #{registration.reference} completed")
+          end
+        end
+      end
+
+      context "when the PDF fails to generate" do
+        before { expect(ConfirmationPdfGeneratorService).to receive(:run).and_raise(StandardError) }
+
+        it "sends an email" do
+          VCR.use_cassette("confirmation_email_no_certificate") do
+            expect_any_instance_of(Notifications::Client).to receive(:send_email).and_call_original
+
+            response = service
+
+            expect(response).to be_a(Notifications::Client::ResponseNotification)
+            expect(response.template["id"]).to eq("8fcf5d04-944f-4cd1-b261-962fedd3859f")
+            expect(response.content["subject"]).to eq("Waste exemptions registration #{registration.reference} completed")
+          end
+        end
+      end
+    end
+  end
+end
