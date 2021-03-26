b54d0a57dea7d3cbf1d010cc11d6e13d8de44285
irisfaraway <iris.faraway@gmail.com>
2021-03-26 15:33:58 +0000

    Only drop tables if they exist

--- db/migrate/20210308160306_remove_ad_letter_exports.rb
+++ db/migrate/20210308160306_remove_ad_letter_exports.rb
@@ -1,6 +1,6 @@
 class RemoveAdLetterExports < ActiveRecord::Migration[6.0]
   def change
-    drop_table :ad_confirmation_letters_exports
-    drop_table :ad_renewal_letters_exports
+    drop_table :ad_confirmation_letters_exports, if_exists: true
+    drop_table :ad_renewal_letters_exports, if_exists: true
   end
 end
