38bc5731a8097f225dbda1734cf27927e18e2edc
irisfaraway <iris.faraway@gmail.com>
2021-05-18 18:06:19 +0100

    test

--- app/services/waste_exemptions_engine/address_lookup_service.rb
+++ app/services/waste_exemptions_engine/address_lookup_service.rb
@@ -3,7 +3,13 @@
 module WasteExemptionsEngine
   class AddressLookupService < BaseService
     def run(postcode)
-      DefraRuby::Address::EaAddressFacadeV11Service.run(postcode)
+      response = DefraRuby::Address::EaAddressFacadeV11Service.run(postcode)
+
+      Rails.logger.info response.successful?
+      Rails.logger.info response.results
+      Rails.logger.info response.error
+
+      response
     end
   end
 end
