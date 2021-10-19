b6540ccae2cbf9e04eccdbfaf9cd60db508df189
Iris Faraway <iris.faraway@gmail.com>
2021-10-19 11:59:33 +0100

    Apply Design System to check your answers page (#610)
    https://eaflood.atlassian.net/browse/RUBY-1635
    
    This applies the new GOV.UK Design System to the check your answers page.

---
+++ spec/presenters/waste_exemptions_engine/data_overview_presenter_spec.rb
@@ -0,0 +1,168 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe DataOverviewPresenter, type: :presenter do
+    let(:new_registration) do
+      create(:new_registration,
+             :complete)
+    end
+
+    subject do
+      described_class.new(new_registration)
+    end
+
+    describe "#company_rows" do
+      let(:expected_data) do
+        [
+          {
+            title: "Business type",
+            value: "Limited company"
+          },
+          {
+            title: "Operator name",
+            value: new_registration.operator_name
+          },
+          {
+            title: "Companies House number",
+            value: new_registration.company_no
+          },
+          {
+            title: "Place of business",
+            value: "England"
+          },
+          {
+            title: "Address",
+            value: [
+              new_registration.operator_address.organisation,
+              new_registration.operator_address.premises,
+              new_registration.operator_address.street_address,
+              new_registration.operator_address.locality,
+              new_registration.operator_address.city,
+              new_registration.operator_address.postcode
+            ].join("<br>").html_safe
+          },
+          {
+            title: "Contact name",
+            value: "#{new_registration.contact_first_name} #{new_registration.contact_last_name}"
+          },
+          {
+            title: "Contact position",
+            value: new_registration.contact_position
+          },
+          {
+            title: "Contact address",
+            value: [
+              new_registration.contact_address.organisation,
+              new_registration.contact_address.premises,
+              new_registration.contact_address.street_address,
+              new_registration.contact_address.locality,
+              new_registration.contact_address.city,
+              new_registration.contact_address.postcode
+            ].join("<br>").html_safe
+          },
+          {
+            title: "Contact details",
+            value: "#{new_registration.contact_phone}<br>#{new_registration.contact_email}".html_safe
+          }
+        ]
+      end
+
+      it "returns the properly-formatted data" do
+        expect(subject.company_rows).to eq(expected_data)
+      end
+
+      context "when the registration is a partnership" do
+        let(:new_registration) do
+          create(:new_registration,
+                 :complete,
+                 :partnership,
+                 :has_people)
+        end
+
+        before do
+          first_partner = new_registration.transient_people.first
+          second_partner = new_registration.transient_people.last
+          partner_text = "#{first_partner.first_name} #{first_partner.last_name}<br>"\
+                         "#{second_partner.first_name} #{second_partner.last_name}".html_safe
+
+          expected_data[0][:value] = "Partnership"
+          # Replace the Companies House info with partners instead
+          expected_data[2] = { title: "Partners", value: partner_text }
+        end
+
+        it "returns the properly-formatted data" do
+          expect(subject.company_rows).to eq(expected_data)
+        end
+      end
+    end
+
+    describe "#registration_rows" do
+      let(:expected_data) do
+        [
+          {
+            title: "Exemptions",
+            value: new_registration.exemptions.map(&:code).join(", ")
+          },
+          {
+            title: "Will this waste operation take place on a farm?",
+            value: "Yes"
+          },
+          {
+            title: "Are the waste exemptions used by a farmer or farming business?",
+            value: "Yes"
+          },
+          {
+            title: "Form completed by",
+            value: "#{new_registration.applicant_first_name} #{new_registration.applicant_last_name}"
+          },
+          {
+            title: "Telephone number",
+            value: new_registration.applicant_phone
+          },
+          {
+            title: "Email address",
+            value: new_registration.applicant_email
+          },
+          {
+            title: "Grid reference",
+            value: new_registration.site_address.grid_reference
+          },
+          {
+            title: "Site description",
+            value: new_registration.site_address.description
+          }
+        ]
+      end
+
+      it "returns the properly-formatted data" do
+        expect(subject.registration_rows).to eq(expected_data)
+      end
+
+      context "when the site address is a postal address" do
+        before do
+          new_registration.site_address = create(:transient_address, :site_address, :using_postal_address)
+
+          # Replace the grid reference with postal address instead
+          expected_data[6] = {
+            title: "Site address",
+            value: [
+              new_registration.site_address.organisation,
+              new_registration.site_address.premises,
+              new_registration.site_address.street_address,
+              new_registration.site_address.locality,
+              new_registration.site_address.city,
+              new_registration.site_address.postcode
+            ].join("<br>").html_safe
+          }
+          expected_data.delete_at(7)
+        end
+
+        it "returns the properly-formatted data" do
+          expect(subject.registration_rows).to eq(expected_data)
+        end
+      end
+    end
+  end
+end
