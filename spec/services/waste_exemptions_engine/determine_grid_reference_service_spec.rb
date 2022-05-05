f33d22672ce96e173edb25df3d123e174f09b028
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-05-05 17:26:25 +0100

    Add a leading zero to short easting/northing values before passing to OsMapRef

--- spec/services/waste_exemptions_engine/determine_grid_reference_service_spec.rb
+++ spec/services/waste_exemptions_engine/determine_grid_reference_service_spec.rb
@@ -10,13 +10,29 @@ module WasteExemptionsEngine
       let(:northing) { 172_855.0 }
       let(:location) { double(:location) }
 
-      it "fetch a grid reference from Os maps" do
-        expect(described_class.run(easting: easting, northing: northing)).to eq("ST 58337 72855")
+      context "given 6-digit easting and northing values" do
+        it "fetches a grid reference from OS Maps" do
+          expect(described_class.run(easting: easting, northing: northing)).to eq("ST 58337 72855")
+        end
+
+        it "does not notify Airbrake and Rails" do
+          expect(Airbrake).to_not receive(:notify)
+          expect(Rails.logger).to_not receive(:error)
+        end
       end
 
-      it "does not notify Airbrake and Rails" do
-        expect(Airbrake).to_not receive(:notify)
-        expect(Rails.logger).to_not receive(:error)
+      context "given a 5-digit easting value" do
+        let(:easting) { 58_337.0 }
+        it "fetches the correct grid reference" do
+          expect(described_class.run(easting: easting, northing: northing)).to eq("SQ 58337 72855")
+        end
+      end
+
+      context "given a 5-digit northing value" do
+        let(:northing) { 70_817.0 }
+        it "fetches the correct grid reference" do
+          expect(described_class.run(easting: easting, northing: northing)).to eq("SY 58337 70817")
+        end
       end
 
       context "when an error happens" do
