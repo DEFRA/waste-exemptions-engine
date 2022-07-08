10cab5ca0f588f15e65fb3d7daa3c0f00692e9ec
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-08 15:25:01 +0100

    Incorporate new check_contact_name form into new registration workflow

--- spec/models/waste_exemptions_engine/new_registration_workflow_state/operator_address_lookup_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/operator_address_lookup_form_spec.rb
@@ -6,7 +6,7 @@ module WasteExemptionsEngine
   RSpec.describe NewRegistration, type: :model do
     describe "#workflow_state" do
       it_behaves_like "an address lookup transition",
-                      next_state_if_not_skipping_to_manual: :contact_name_form,
+                      next_state_if_not_skipping_to_manual: :check_contact_name_form,
                       address_type: "operator",
                       factory: :new_registration
     end
