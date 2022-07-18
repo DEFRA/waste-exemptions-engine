94a787329378227e42738dbb50856831b7fe773d
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-18 16:18:11 +0100

    Add a custom validator to allow submission of blank main people form in some circumstances

---
+++ app/validators/waste_exemptions_engine/main_person_form_validator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  class MainPersonFormValidator < PersonNameValidator
+    # class MainPersonFormValidator < ActiveModel::Validator
+    include CanAddValidationErrors
+
+    def validate(record)
+      # Allow blank form submission if sufficient people already added
+      return true if !fields_have_content?(record) && enough_main_people?(record)
+
+      super(record)
+    end
+
+    private
+
+    def fields_have_content?(record)
+      record.first_name.present? || record.last_name.present?
+    end
+
+    def enough_main_people?(record)
+      record.business_type != "partnership" || record.transient_people.length > 1
+    end
+  end
+end
