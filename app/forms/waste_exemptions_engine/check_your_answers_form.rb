bad28c68484e47e67c256e1abeed17edf8157f95
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-20 17:03:18 +0100

    Allow blank applicant and contact emails in the check your answers page in the back office

--- app/forms/waste_exemptions_engine/check_your_answers_form.rb
+++ app/forms/waste_exemptions_engine/check_your_answers_form.rb
@@ -9,7 +9,9 @@ module WasteExemptionsEngine
     validates :location, "defra_ruby/validators/location": true
     validates :applicant_first_name, :applicant_last_name, "waste_exemptions_engine/person_name": true
     validates :applicant_phone, "defra_ruby/validators/phone_number": true
-    validates :applicant_email, "defra_ruby/validators/email": true
+
+    validates_with WasteExemptionsEngine::OptionalEmailValidator, attributes: [:applicant_email], if: :back_office?
+    validates_with DefraRuby::Validators::EmailValidator, attributes: [:applicant_email], unless: :back_office?
 
     validates :business_type, "defra_ruby/validators/business_type": true
     validates :company_no, "defra_ruby/validators/companies_house_number": true, if: :company_no_required?
@@ -19,7 +21,10 @@ module WasteExemptionsEngine
     validates :contact_first_name, :contact_last_name, "waste_exemptions_engine/person_name": true
     validates :contact_position, "defra_ruby/validators/position": true
     validates :contact_phone, "defra_ruby/validators/phone_number": true
-    validates :contact_email, "defra_ruby/validators/email": true
+
+    validates_with WasteExemptionsEngine::OptionalEmailValidator, attributes: [:contact_email], if: :back_office?
+    validates_with DefraRuby::Validators::EmailValidator, attributes: [:contact_email], unless: :back_office?
+
     validates :contact_address, "waste_exemptions_engine/address": true
 
     validates :on_a_farm, inclusion: { in: [true, false] }
@@ -54,6 +59,10 @@ module WasteExemptionsEngine
       return true unless transient_registration&.site_address
 
       transient_registration.site_address.auto?
+    end
+
+    def back_office?
+      WasteExemptionsEngine.configuration.host_is_back_office?
     end
   end
 end
