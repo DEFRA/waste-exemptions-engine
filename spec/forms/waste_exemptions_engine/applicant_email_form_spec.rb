9644a6eb6864a22345a0a60d53cd157c78acf8fc
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-20 16:59:35 +0100

    Add form unit tests for optional applicant and contact email addresses

--- spec/forms/waste_exemptions_engine/applicant_email_form_spec.rb
+++ spec/forms/waste_exemptions_engine/applicant_email_form_spec.rb
@@ -4,64 +4,6 @@ require "rails_helper"
 
 module WasteExemptionsEngine
   RSpec.describe ApplicantEmailForm, type: :model do
-    subject(:form) { build(:applicant_email_form) }
-
-    describe "validations" do
-      subject(:validators) { form._validators }
-
-      describe "email attribute validation" do
-        it "validates the applicant email using the EmailValidator class" do
-          expect(validators.keys).to include(:applicant_email)
-          expect(validators[:applicant_email].first.class)
-            .to eq(DefraRuby::Validators::EmailValidator)
-        end
-
-        it "validates the confirmed email using the EmailValidator class" do
-          expect(validators.keys).to include(:confirmed_email)
-          expect(validators[:confirmed_email].map(&:class))
-            .to include(DefraRuby::Validators::EmailValidator)
-        end
-      end
-
-      describe "matching email validation" do
-        it "validates the applicant email and confirmed email using the MatchingEmailValidator class " do
-          expect(validators.keys).to include(:confirmed_email)
-
-          matching_email_validators = validators[:confirmed_email].select do |v|
-            v.instance_of?(WasteExemptionsEngine::MatchingEmailValidator)
-          end
-          expect(matching_email_validators.count).to eq(1)
-          expect(matching_email_validators.first.options).to eq(compare_to: :applicant_email)
-        end
-      end
-    end
-
-    it_behaves_like "a validated form", :applicant_email_form do
-      let(:valid_params) do
-        { applicant_email: "test@example.com", confirmed_email: "test@example.com" }
-      end
-      let(:invalid_params) do
-        [
-          { applicant_email: "test@example.com", confirmed_email: "different@example.com" },
-          { applicant_email: "", confirmed_email: "test@example.com" },
-          { applicant_email: "test@example.com", confirmed_email: "" },
-          { applicant_email: "", confirmed_email: "" }
-        ]
-      end
-    end
-
-    describe "#submit" do
-      context "when the form is valid" do
-        it "updates the transient registration with the applicant email address" do
-          email = "test@example.com"
-          valid_params = { applicant_email: email, confirmed_email: email }
-          transient_registration = form.transient_registration
-
-          expect(transient_registration.applicant_email).to be_blank
-          form.submit(ActionController::Parameters.new(valid_params).permit!)
-          expect(transient_registration.applicant_email).to eq(email)
-        end
-      end
-    end
+    it_behaves_like "an optional email form", :applicant_email_form, :applicant_email
   end
 end
---
+++ spec/support/shared_examples/forms/optional_email_form.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples "an optional email form", vcr: true do |form_factory, email_attribute|
+  subject(:form) { build(form_factory) }
+
+  describe "validations" do
+    subject(:validators) { form._validators }
+
+    describe "email attribute validation" do
+      it "validates the #{email_attribute} using the OptionalEmailFormValidator class" do
+        expect(validators[email_attribute].first.class).to eq(WasteExemptionsEngine::OptionalEmailFormValidator)
+      end
+    end
+  end
+
+  it_behaves_like "a validated form", form_factory do
+    let(:valid_params) do
+      { "#{email_attribute}": "test@example.com", confirmed_email: "test@example.com" }
+    end
+    let(:invalid_params) do
+      [
+        { "#{email_attribute}": "test@example.com", confirmed_email: "different@example.com" },
+        { "#{email_attribute}": "", confirmed_email: "test@example.com" },
+        { "#{email_attribute}": "test@example.com", confirmed_email: "" },
+        { "#{email_attribute}": "", confirmed_email: "" }
+      ]
+    end
+  end
+
+  describe "#submit" do
+    let(:params) do
+      {
+        "#{email_attribute}": email_address,
+        confirmed_email: email_address,
+        no_email_address: defined?(no_email_address) ? no_email_address : nil
+      }
+    end
+    let(:email_address) { Faker::Internet.email }
+    let(:is_back_office) { false }
+    before { allow(WasteExemptionsEngine.configuration).to receive(:host_is_back_office?).and_return(is_back_office) }
+
+    shared_examples "should submit" do
+      it "submits the form successfully" do
+        expect(form.submit(ActionController::Parameters.new(params))).to eq(true)
+      end
+    end
+
+    shared_examples "should not submit" do
+      it "does not submit the form successfully" do
+        expect(form.submit(ActionController::Parameters.new(params))).to eq(false)
+      end
+    end
+
+    context "when the form is valid" do
+      context "when running in the front office" do
+        let(:is_back_office) { false }
+
+        context "with an email address" do
+          let(:email_address) { Faker::Internet.email }
+
+          it_behaves_like "should submit"
+
+          it "updates the transient registration with the contact email address" do
+            expect { form.submit(ActionController::Parameters.new(params).permit!) }
+              .to change { form.transient_registration.send(email_attribute) }
+              .from(nil).to(email_address)
+          end
+        end
+      end
+
+      context "when running in the back office" do
+        let(:is_back_office) { true }
+
+        context "with an email address" do
+          let(:email_address) { Faker::Internet.email }
+
+          it_behaves_like "should submit"
+        end
+
+        context "without an email address" do
+          let(:email_address) { nil }
+          let(:no_email_address) { "1" }
+
+          it_behaves_like "should submit"
+        end
+      end
+    end
+
+    context "when the form is not valid" do
+      context "when running in the front office" do
+        let(:is_back_office) { false }
+
+        context "with no parameters" do
+          let(:params) { {} }
+
+          it_behaves_like "should not submit"
+        end
+      end
+
+      context "when running in the back office" do
+        let(:is_back_office) { true }
+
+        context "without an email address and with the no-email-address option not selected" do
+          let(:email_address) { nil }
+          let(:no_email_address) { "0" }
+
+          it_behaves_like "should not submit"
+        end
+
+        context "with an email address and with the no-email-address option selected" do
+          let(:email_address) { Faker::Internet.email }
+          let(:no_email_address) { "1" }
+
+          it_behaves_like "should not submit"
+        end
+      end
+    end
+  end
+end
