d6efce16e075587e0fad33e225b155cc59b2a747
David Georgiou <david.georgiou@gmail.com>
2022-08-10 11:46:31 +0100

    fix failing spec

--- app/models/waste_exemptions_engine/renewing_registration.rb
+++ app/models/waste_exemptions_engine/renewing_registration.rb
@@ -31,9 +31,9 @@ module WasteExemptionsEngine
     # renwals cannot change business type
     # this checks for an attempted change, and undoes it
     def changing_business_type?
-      if referring_registration.business_type != business_type
-        registration_attributes["business_type"] = referring_registration.business_type
-      end
+      return false unless referring_registration.business_type != business_type
+
+      registration_attributes["business_type"] = referring_registration.business_type
     end
   end
 end
