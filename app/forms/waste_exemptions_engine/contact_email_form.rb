0e6a92dcfcb459df47ef07b272b3f6a95af67323
Rebecca Ransome <beckyrose200@aol.com>
2022-08-15 12:07:27 +0100

    Removing blank logic from the code

--- app/forms/waste_exemptions_engine/contact_email_form.rb
+++ app/forms/waste_exemptions_engine/contact_email_form.rb
@@ -4,21 +4,20 @@ module WasteExemptionsEngine
   class ContactEmailForm < BaseForm
     delegate :contact_email, to: :transient_registration
 
-    attr_writer :confirmed_email
-    attr_accessor :no_email_address
+    attr_accessor :confirmed_email, :no_email_address
 
     validates_with OptionalEmailFormValidator, attributes: [:contact_email]
 
     def submit(params)
+      # Blank email address values should be processed as nil
+      params[:contact_email] = nil if params[:contact_email].blank?
+      params[:confirmed_email] = nil if params[:confirmed_email].blank?
+
       # Assign the params for validation and pass them to the BaseForm method for updating
       self.confirmed_email = params[:confirmed_email]
       self.no_email_address = params[:no_email_address]
 
       super(params.permit(:contact_email))
-    end
-
-    def confirmed_email
-      @confirmed_email || contact_email
     end
   end
 end
