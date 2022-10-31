5bc38b5e094834e761a49e6ac43472e12b5db774
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-10-31 11:54:45 +0000

    layout tweak for sonarcloud

--- app/models/concerns/waste_exemptions_engine/can_activate_exemption.rb
+++ app/models/concerns/waste_exemptions_engine/can_activate_exemption.rb
@@ -23,13 +23,12 @@ module WasteExemptionsEngine
       def activate_exemption
         self.registered_on = Date.today
 
-        self.expires_on =
-          if transient_registration.is_a? WasteExemptionsEngine::RenewingRegistration
-            transient_registration.registration.expires_on +
-              WasteExemptionsEngine.configuration.years_before_expiry.years
-          else
-            Date.today + (WasteExemptionsEngine.configuration.years_before_expiry.years - 1.day)
-          end
+        self.expires_on = if transient_registration.is_a? WasteExemptionsEngine::RenewingRegistration
+                            transient_registration.registration.expires_on +
+                              WasteExemptionsEngine.configuration.years_before_expiry.years
+                          else
+                            Date.today + (WasteExemptionsEngine.configuration.years_before_expiry.years - 1.day)
+                          end
 
         save!
       end
