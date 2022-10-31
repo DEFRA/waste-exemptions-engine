ed91bf8e6e583de9e02a227766fc41df12ef666a
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-10-31 14:03:31 +0000

    rubocop/sonarcloud tweak

--- app/models/concerns/waste_exemptions_engine/can_activate_exemption.rb
+++ app/models/concerns/waste_exemptions_engine/can_activate_exemption.rb
@@ -19,19 +19,20 @@ module WasteExemptionsEngine
 
       end
 
-      # Transition effects
+      # rubocop:disable Style/ConditionalAssignment
       def activate_exemption
         self.registered_on = Date.today
 
-        self.expires_on = if transient_registration.is_a? WasteExemptionsEngine::RenewingRegistration
-                            transient_registration.registration.expires_on +
-                              WasteExemptionsEngine.configuration.years_before_expiry.years
-                          else
-                            Date.today + (WasteExemptionsEngine.configuration.years_before_expiry.years - 1.day)
-                          end
+        if transient_registration.is_a? WasteExemptionsEngine::RenewingRegistration
+          self.expires_on = transient_registration.registration.expires_on +
+                            WasteExemptionsEngine.configuration.years_before_expiry.years
+        else
+          self.expires_on = Date.today + (WasteExemptionsEngine.configuration.years_before_expiry.years - 1.day)
+        end
 
         save!
       end
+      # rubocop:enable Style/ConditionalAssignment
     end
   end
 end
