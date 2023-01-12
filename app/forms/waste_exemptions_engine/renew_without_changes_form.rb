edd3a45cb702bac1807efad8232887e0a48c70da
Tobias Svensson <tob@tobiassvensson.co.uk>
2023-01-12 11:28:03 +0000

    Reset exemptions in RenewalStartForm instead of RenewWithoutChangesForm

--- app/forms/waste_exemptions_engine/renew_without_changes_form.rb
+++ app/forms/waste_exemptions_engine/renew_without_changes_form.rb
@@ -3,14 +3,6 @@
 module WasteExemptionsEngine
   class RenewWithoutChangesForm < BaseForm
     def submit(_params)
-      # When a user first renews with changes and deselects some or all
-      # exemptions to renew and subsequently navigates back to renew without
-      # changes we run into an edge case where exemptions do not match the
-      # referring registration.  We re-assign these exemptions here to
-      # correctly renew the registration.
-      transient_registration.exemptions =
-        transient_registration.registration_exemptions_to_copy
-
       super({})
     end
   end
--- app/forms/waste_exemptions_engine/renewal_start_form.rb
+++ app/forms/waste_exemptions_engine/renewal_start_form.rb
@@ -14,6 +14,18 @@ module WasteExemptionsEngine
                 }
               }
 
+    def initialize(transient_registration)
+      # When a user first renews with changes and deselects some or all
+      # exemptions to renew and subsequently navigates back to renew without
+      # changes we run into an edge case where exemptions do not match the
+      # referring registration.  We re-assign these exemptions here to
+      # correctly reset the transient registration with the correct exemptions.
+      transient_registration.exemptions =
+        transient_registration.registration_exemptions_to_copy
+
+      super(transient_registration)
+    end
+
     private
 
     def data_overview_presenter
