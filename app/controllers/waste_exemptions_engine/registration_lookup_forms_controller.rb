f9bb289292dfb1d85ef285ce14e51e29ed3b8a4f
Leonid Brujev <brujev@gmail.com>
2023-08-01 11:59:17 +0100

    RUBY-2566: refactoring

--- app/controllers/waste_exemptions_engine/registration_lookup_forms_controller.rb
+++ app/controllers/waste_exemptions_engine/registration_lookup_forms_controller.rb
@@ -9,30 +9,13 @@ module WasteExemptionsEngine
     def create
       return false unless set_up_form(RegistrationLookupForm, "registration_lookup_form", params[:token])
 
-      submit_form(@registration_lookup_form, transient_registration_attributes)
+      super(RegistrationLookupForm, "registration_lookup_form")
     end
 
     private
 
     def transient_registration_attributes
       params.fetch(:registration_lookup_form, {}).permit(:reference)
-    end
-
-    def submit_form(form, params)
-      respond_to do |format|
-        if form.submit(params)
-          format.html { redirect_to_registration_lookup_email_journey }
-          true
-        else
-          format.html { render :new }
-          false
-        end
-      end
-    end
-
-    def redirect_to_registration_lookup_email_journey
-      @transient_registration.next_state!
-      redirect_to_correct_form
     end
   end
 end
