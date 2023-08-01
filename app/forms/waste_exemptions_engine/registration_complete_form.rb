ef908d35a3ab3a8faf3f0b491acf13c281fce56d
Leonid Brujev <brujev@gmail.com>
2023-08-01 16:42:14 +0100

    RUBY-2566: removing unnecessary code

--- app/forms/waste_exemptions_engine/registration_complete_form.rb
+++ app/forms/waste_exemptions_engine/registration_complete_form.rb
@@ -4,11 +4,6 @@ module WasteExemptionsEngine
   class RegistrationCompleteForm < BaseForm
     delegate :exemptions, :reference, :applicant_email, :contact_email, to: :transient_registration
 
-    # Override BaseForm method as users shouldn't be able to submit this form
-    def submit(_params)
-      raise UnsubmittableForm
-    end
-
     def self.can_navigate_flexibly?
       false
     end
