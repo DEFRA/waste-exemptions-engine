6eaf90584e988d0ce513d49d82d8e2491b0643a2
Leonid Brujev <brujev@gmail.com>
2023-08-03 15:10:23 +0100

    RUBY-2566: restoring accidentally deleted method

--- app/forms/waste_exemptions_engine/registration_complete_form.rb
+++ app/forms/waste_exemptions_engine/registration_complete_form.rb
@@ -4,6 +4,11 @@ module WasteExemptionsEngine
   class RegistrationCompleteForm < BaseForm
     delegate :exemptions, :reference, :applicant_email, :contact_email, to: :transient_registration
 
+    # Override BaseForm method as users shouldn't be able to submit this form
+    def submit(_params)
+      raise UnsubmittableForm
+    end
+
     def self.can_navigate_flexibly?
       false
     end
