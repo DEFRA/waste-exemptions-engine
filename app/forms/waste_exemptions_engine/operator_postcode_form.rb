5d3fe3c7c92aebdc89560d66a4e6683950a76ca7
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-01-09 16:48:51 +0000

    Use the new defra_ruby_validators postcode validator

--- app/forms/waste_exemptions_engine/operator_postcode_form.rb
+++ app/forms/waste_exemptions_engine/operator_postcode_form.rb
@@ -4,7 +4,8 @@ module WasteExemptionsEngine
   class OperatorPostcodeForm < BasePostcodeForm
     delegate :business_type, :temp_operator_postcode, to: :transient_registration
 
-    validates :temp_operator_postcode, "waste_exemptions_engine/postcode": true
+    validates :temp_operator_postcode, "defra_ruby/validators/postcode": true
+    validates :temp_operator_postcode, "waste_exemptions_engine/address_lookup": true
 
     def submit(params)
       params[:temp_operator_postcode] = format_postcode(params[:temp_operator_postcode])
--- app/validators/concerns/waste_exemptions_engine/can_add_validation_errors.rb
+++ app/validators/concerns/waste_exemptions_engine/can_add_validation_errors.rb
@@ -5,9 +5,11 @@ module WasteExemptionsEngine
     private
 
     def add_validation_error(record, attribute, error)
-      record.errors.add(attribute,
-                        error,
-                        message: error_message(record, attribute, error))
+      # Move on if an error of this type with the same message has already been added
+      message = error_message(record, attribute, error)
+      return if record.errors.find { |e| e.type == error && e.message == message }
+
+      record.errors.add(attribute, error, message:)
     end
 
     def error_message(record, attribute, error)
