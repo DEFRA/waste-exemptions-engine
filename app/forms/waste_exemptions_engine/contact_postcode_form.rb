3645aabefa6d28ba740ab4f44ccc1e521a2c9237
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-01-10 13:00:59 +0000

    Fix/ruby 2805 postcode validation (#913)
    https://eaflood.atlassian.net/browse/RUBY-2805

--- app/forms/waste_exemptions_engine/contact_postcode_form.rb
+++ app/forms/waste_exemptions_engine/contact_postcode_form.rb
@@ -4,7 +4,8 @@ module WasteExemptionsEngine
   class ContactPostcodeForm < BasePostcodeForm
     delegate :business_type, :temp_contact_postcode, to: :transient_registration
 
-    validates :temp_contact_postcode, "waste_exemptions_engine/postcode": true
+    validates :temp_contact_postcode, "defra_ruby/validators/postcode": true
+    validates :temp_contact_postcode, "waste_exemptions_engine/address_lookup": true
 
     def submit(params)
       params[:temp_contact_postcode] = format_postcode(params[:temp_contact_postcode])
--- app/forms/waste_exemptions_engine/site_postcode_form.rb
+++ app/forms/waste_exemptions_engine/site_postcode_form.rb
@@ -4,7 +4,8 @@ module WasteExemptionsEngine
   class SitePostcodeForm < BasePostcodeForm
     delegate :business_type, :temp_site_postcode, to: :transient_registration
 
-    validates :temp_site_postcode, "waste_exemptions_engine/postcode": true
+    validates :temp_site_postcode, "defra_ruby/validators/postcode": true
+    validates :temp_site_postcode, "waste_exemptions_engine/address_lookup": true
 
     def submit(params)
       params[:temp_site_postcode] = format_postcode(params[:temp_site_postcode])
