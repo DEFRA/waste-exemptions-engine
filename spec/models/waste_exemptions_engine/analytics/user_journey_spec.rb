09a99115eee4d673396a1b02c039887457b20010
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-03-01 16:03:04 +0000

    [RUBY-2885] Refactor UserJourney model with new scopes and methods; add specs for UserJourney
    - Add START_CUTOFF_PAGE constant
    - Implement new scopes for filtering by registration type, start route, completion status, and date range
    - Add `passed_start_cutoff_page` scope with subqueries for filtering UserJourneys
    - Create `minimum_created_at` and `average_duration` class methods
    - Update `complete_journey` method to handle journey completion logic
    - Write comprehensive tests

---
+++ spec/models/waste_exemptions_engine/analytics/user_journey_spec.rb
@@ -0,0 +1,175 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  module Analytics
+
+    RSpec.describe UserJourney do
+
+      # rubocop:disable RSpec/IndexedLet
+      let(:count_1) { Faker::Number.between(from: 1, to: 5) }
+      let(:count_2) { Faker::Number.between(from: 1, to: 5) }
+      let(:count_3) { Faker::Number.between(from: 1, to: 5) }
+      # rubocop:enable RSpec/IndexedLet
+
+      describe "journey type scopes" do
+        before do
+          create_list(:user_journey, count_1, :registration)
+          create_list(:user_journey, count_2, :renewal)
+          create_list(:user_journey, count_3, journey_type: "Foo")
+        end
+
+        it { expect(described_class.registrations.length).to eq count_1 }
+        it { expect(described_class.renewals.length).to eq count_2 }
+        it { expect(described_class.only_types(%w[NewRegistration]).length).to eq count_1 }
+        it { expect(described_class.only_types(%w[RenewingRegistration]).length).to eq count_2 }
+        it { expect(described_class.only_types(%w[NewRegistration RenewingRegistration]).length).to eq count_1 + count_2 }
+      end
+
+      describe "start route scopes" do
+        before do
+          create_list(:user_journey, count_1, :started_digital)
+          create_list(:user_journey, count_2, :started_assisted_digital)
+        end
+
+        it { expect(described_class.started_digital.length).to eq count_1 }
+        it { expect(described_class.started_assisted_digital.length).to eq count_2 }
+      end
+
+      describe "passed_start_cutoff_page" do
+        let!(:journey_initial_page_only) { create(:user_journey, visited_pages: %w[start_form]) }
+        let!(:journey_to_location_page) { create(:user_journey, visited_pages: %w[start_form location_form]) }
+        let!(:journey_past_location_page) { create(:user_journey, visited_pages: %w[start_form location_form business_type_form]) }
+
+        it { expect(described_class.passed_start_cutoff_page).not_to include(journey_initial_page_only) }
+        it { expect(described_class.passed_start_cutoff_page).not_to include(journey_to_location_page) }
+        it { expect(described_class.passed_start_cutoff_page).to include(journey_past_location_page) }
+      end
+
+      describe "completion scopes" do
+        before do
+          create_list(:user_journey, count_1, :completed_digital)
+          create_list(:user_journey, count_2, :completed_assisted_digital)
+          create_list(:user_journey, count_3, completed_at: nil)
+        end
+
+        it { expect(described_class.completed_digital.length).to eq count_1 }
+        it { expect(described_class.completed_assisted_digital.length).to eq count_2 }
+        it { expect(described_class.completed.length).to eq count_1 + count_2 }
+        it { expect(described_class.incomplete.length).to eq count_3 }
+      end
+
+      describe ".date_range" do
+        subject(:date_range_query_results) { described_class.date_range(start_date, end_date) }
+
+        let(:start_date) { 10.days.ago.midnight }
+        let(:end_date) { Date.today.midnight }
+        let(:journey_started_before_range) { Timecop.freeze(start_date - 2.days) { create(:user_journey, completed_at: start_date - 1.day) } }
+        let(:journey_started_at_range_start) { Timecop.freeze(start_date) { create(:user_journey, completed_at: start_date + 1.day) } }
+        let(:journey_started_before_range_end) { Timecop.freeze(end_date - 1.day) { create(:user_journey, completed_at: end_date) } }
+        let(:journey_started_after_range) { Timecop.freeze(end_date + 1.day) { create(:user_journey) } }
+        let(:journey_completed_after_range) { Timecop.freeze(start_date - 2.days) { create(:user_journey, completed_at: end_date + 1.day) } }
+        let(:journey_started_before_and_ended_within_range) { Timecop.freeze(start_date - 1.day) { create(:user_journey, completed_at: end_date) } }
+        let(:ongoing_journey_started_in_range) { Timecop.freeze(start_date + 1.day) { create(:user_journey, completed_at: nil) } }
+        let(:ongoing_journey_started_before_range) { Timecop.freeze(start_date - 3.days) { create(:user_journey, completed_at: nil) } }
+
+        before do
+          journey_started_before_range
+          journey_started_at_range_start
+          journey_started_before_range_end
+          journey_started_after_range
+          journey_completed_after_range
+          journey_started_before_and_ended_within_range
+          ongoing_journey_started_in_range
+          ongoing_journey_started_before_range
+        end
+
+        it { expect(date_range_query_results).not_to include(journey_started_before_range) }
+        it { expect(date_range_query_results).to include(journey_started_at_range_start) }
+        it { expect(date_range_query_results).to include(journey_started_before_range_end) }
+        it { expect(date_range_query_results).not_to include(journey_started_after_range) }
+        it { expect(date_range_query_results).not_to include(journey_completed_after_range) }
+        it { expect(date_range_query_results).not_to include(journey_started_before_and_ended_within_range) }
+        it { expect(date_range_query_results).to include(ongoing_journey_started_in_range) }
+        it { expect(date_range_query_results).not_to include(ongoing_journey_started_before_range) }
+      end
+
+      describe "#complete_journey" do
+        let(:transient_registration) { create(:new_registration) }
+        let(:journey) { Timecop.freeze(1.hour.ago) { create(:user_journey, token: transient_registration.token) } }
+        let(:completion_time) { Time.zone.now }
+
+        before { allow(WasteExemptionsEngine.configuration).to receive(:host_is_back_office?).and_return(false) }
+
+        it "updates user journey attributes on completion" do
+          Timecop.freeze(completion_time) { journey.complete_journey(transient_registration) }
+
+          expect(journey.completed_at).to be_within(1.second).of(completion_time)
+          expect(journey.completed_route).to eq "DIGITAL"
+          expect(journey.registration_data).to include(
+            transient_registration.attributes.slice(
+            "business_type",
+            "type"
+            )
+          )
+        end
+      end
+
+      describe ".average_session_duration" do
+        let(:transient_registration_a) { create(:new_registration) }
+        let(:transient_registration_b) { create(:new_registration) }
+        let(:transient_registration_c) { create(:renewing_registration) }
+
+        let(:journey_a) { Timecop.freeze(6.hours.ago) { create(:user_journey, token: transient_registration_a.token) } }
+        let(:journey_b) { Timecop.freeze(4.hours.ago) { create(:user_journey, token: transient_registration_b.token) } }
+        let(:journey_c) { Timecop.freeze(1.hour.ago) { create(:user_journey, token: transient_registration_c.token) } }
+
+        let(:completion_time) { Time.zone.now }
+
+        let(:journey_a_duration) { journey_a.completed_at.to_time - journey_a.created_at.to_time }
+        let(:journey_b_duration) { journey_b.completed_at.to_time - journey_b.created_at.to_time }
+        let(:journey_c_duration) { journey_c.updated_at.to_time - journey_c.created_at.to_time }
+
+        before do
+          Timecop.freeze(completion_time) do
+            journey_a.complete_journey(transient_registration_a)
+            journey_b.complete_journey(transient_registration_b)
+            # simulate a non-completion update:
+            journey_c.touch
+          end
+        end
+
+        it "returns the average duration across all user journeys" do
+          total_duration = journey_a_duration + journey_b_duration + journey_c_duration
+
+          expect(described_class.average_duration(described_class.all)).to eq total_duration / 3
+        end
+
+        context "with completed registrations only" do
+          it "returns the average duration across completed journeys only" do
+            expect(described_class.average_duration(described_class.completed)).to eq (journey_a_duration + journey_b_duration) / 2
+          end
+        end
+
+        context "with incomplete registrations only" do
+          it "returns the average duration across incomplete journeys only" do
+            expect(described_class.average_duration(described_class.incomplete)).to eq journey_c_duration
+          end
+        end
+      end
+
+      describe ".minimum_created_at" do
+        before do
+          @earliest_created_journey = create(:user_journey, created_at: 5.days.ago)
+          @latest_created_journey = create(:user_journey, created_at: 1.day.ago)
+        end
+
+        it "returns the earliest created user journey" do
+          expect(described_class.minimum_created_at).to be_within(1.second).of(@earliest_created_journey.created_at)
+        end
+      end
+    end
+  end
+end
+
--- spec/support/factories/user_journeys.rb
+++ spec/support/factories/user_journeys.rb
@@ -2,12 +2,45 @@
 
 FactoryBot.define do
   factory :user_journey, class: "WasteExemptionsEngine::Analytics::UserJourney" do
-    journey_type { "MyString" }
-    completed_at { "2024-02-22 14:59:23" }
-    token { "MyString" }
-    user { "MyString" }
+    journey_type { "NewRegistration" }
+    token { SecureRandom.hex(20) }
     started_route { "DIGITAL" }
-    completed_route { "MyString" }
     registration_data { {} }
+
+    transient do
+      visited_pages { [] }
+    end
+
+    after :create do |user_journey, options|
+      options.visited_pages.each do |page|
+        user_journey.page_views.create(page:, time: Time.zone.now, route: "DIGITAL")
+      end
+    end
+
+    trait :registration do
+      journey_type { "NewRegistration" }
+    end
+
+    trait :renewal do
+      journey_type { "RenewingRegistration" }
+    end
+
+    trait :started_digital do
+      started_route { "DIGITAL" }
+    end
+
+    trait :started_assisted_digital do
+      started_route { "ASSISTED_DIGITAL" }
+    end
+
+    trait :completed_digital do
+      completed_route { "DIGITAL" }
+      completed_at { Time.zone.now }
+    end
+
+    trait :completed_assisted_digital do
+      completed_route { "ASSISTED_DIGITAL" }
+      completed_at { Time.zone.now }
+    end
   end
 end
