5976f8c67dab59a6f9911ea5288421735667317b
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-03-08 11:04:41 +0000

    RUBY 2885 wex implement analytics metrics (#943)
    * [RUBY-2974] Add analytics models, migrations, and factories for user journeys and page views
    
    * [RUBY-2974] Add analytics tracking for user journeys in WasteExemptionsEngine
    
    * [RUBY-2974] Handle NotImplementedError in user journey analytics recording
    
    * [RUBY-2885] Refactor UserJourney model with new scopes and methods; add specs for UserJourney
    
    - Add START_CUTOFF_PAGE constant
    - Implement new scopes for filtering by registration type, start route, completion status, and date range
    - Add `passed_start_cutoff_page` scope with subqueries for filtering UserJourneys
    - Create `minimum_created_at` and `average_duration` class methods
    - Update `complete_journey` method to handle journey completion logic
    - Write comprehensive tests
    
    * [RUBY-2885] Rubocop Fixes
    
    * [RUBY-2885] Rename factory files for consistency

---
+++ spec/support/factories/user_journey.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user_journey, class: "WasteExemptionsEngine::Analytics::UserJourney" do
+    journey_type { "NewRegistration" }
+    token { SecureRandom.hex(20) }
+    started_route { "DIGITAL" }
+    registration_data { {} }
+
+    transient do
+      visited_pages { [] }
+    end
+
+    after :create do |user_journey, options|
+      options.visited_pages.each do |page|
+        user_journey.page_views.create(page:, time: Time.zone.now, route: "DIGITAL")
+      end
+    end
+
+    trait :registration do
+      journey_type { "NewRegistration" }
+    end
+
+    trait :renewal do
+      journey_type { "RenewingRegistration" }
+    end
+
+    trait :started_digital do
+      started_route { "DIGITAL" }
+    end
+
+    trait :started_assisted_digital do
+      started_route { "ASSISTED_DIGITAL" }
+    end
+
+    trait :completed_digital do
+      completed_route { "DIGITAL" }
+      completed_at { Time.zone.now }
+    end
+
+    trait :completed_assisted_digital do
+      completed_route { "ASSISTED_DIGITAL" }
+      completed_at { Time.zone.now }
+    end
+  end
+end
