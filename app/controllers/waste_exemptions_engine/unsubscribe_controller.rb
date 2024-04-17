45bf7634bbb632cc7937afc8d6c9014926995a84
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-04-17 11:51:08 +0100

    [RUBY-2976] Add communication log entry and page titles for unsubscribe feature

--- app/controllers/waste_exemptions_engine/unsubscribe_controller.rb
+++ app/controllers/waste_exemptions_engine/unsubscribe_controller.rb
@@ -6,6 +6,7 @@ module WasteExemptionsEngine
       registration = Registration.find_by(unsubscribe_token: params[:unsubscribe_token])
       if registration
         registration.update(reminder_opt_in: false)
+        create_communication_log(registration:)
         redirect_to unsubscribe_registration_successful_path
       else
         redirect_to unsubscribe_registration_failed_path
@@ -18,6 +19,17 @@ module WasteExemptionsEngine
 
     def unsubscribe_failed
       # This method is empty to display the view
+    end
+
+    private
+
+    def create_communication_log(registration:)
+      registration.communication_logs.create(
+        message_type: "email",
+        template_id: nil,
+        template_label: "User unsubscribed from renewal reminders",
+        sent_to: registration.contact_email
+      )
     end
   end
 end
--- spec/requests/waste_exemptions_engine/unsubscribe_spec.rb
+++ spec/requests/waste_exemptions_engine/unsubscribe_spec.rb
@@ -21,6 +21,36 @@ module WasteExemptionsEngine
           get unsubscribe_registration_path(unsubscribe_token:)
           expect(response).to redirect_to(unsubscribe_registration_successful_path)
         end
+
+        it "creates a communication log entry" do
+          expect do
+            get unsubscribe_registration_path(unsubscribe_token:)
+          end.to change { registration.communication_logs.count }.by(1)
+        end
+
+        describe "the created communication log entry" do
+          before do
+            get unsubscribe_registration_path(unsubscribe_token:)
+          end
+
+          let(:communication_log) { registration.communication_logs.last }
+
+          it "has the correct message_type" do
+            expect(communication_log.message_type).to eq("email")
+          end
+
+          it "has a nil template_id" do
+            expect(communication_log.template_id).to be_nil
+          end
+
+          it "has the correct template_label" do
+            expect(communication_log.template_label).to eq("User unsubscribed from renewal reminders")
+          end
+
+          it "has the correct sent_to value" do
+            expect(communication_log.sent_to).to eq(registration.contact_email)
+          end
+        end
       end
 
       context "when the unsubscribe token is invalid" do
