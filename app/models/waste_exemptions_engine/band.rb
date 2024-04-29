28eef7d60aafe6fffaf8e5a5ab28c74c2a363ee8
Leonid Brujev <brujev@gmail.com>
2024-04-29 15:58:46 +0100

    [RUBY-3050] Adding validations for Band model

--- app/models/waste_exemptions_engine/band.rb
+++ app/models/waste_exemptions_engine/band.rb
@@ -7,5 +7,11 @@ module WasteExemptionsEngine
     has_paper_trail
 
     has_many :exemptions
+
+    validates :name, presence: true
+    validates :sequence, uniqueness: true, numericality: { only_integer: true }, allow_nil: true
+    validates :registration_fee, numericality: { only_integer: true }, allow_nil: true
+    validates :initial_compliance_charge, numericality: { only_integer: true }, allow_nil: true
+    validates :additional_compliance_charge, numericality: { only_integer: true }, allow_nil: true
   end
 end
