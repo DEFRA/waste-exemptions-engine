0a0ad67429587ff0648004bd121a99f446418492
Leonid Brujev <brujev@gmail.com>
2024-04-30 12:12:41 +0100

    [RUBY-3050] Ensuring that name and sequence are unique in Band and Bucket factories

--- spec/support/factories/bucket.rb
+++ spec/support/factories/bucket.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :bucket, class: "WasteExemptionsEngine::Bucket" do
-    name { "Bucket #{Faker::Lorem.word}" }
+    name { "Bucket #{Faker::Lorem.unique.word}" }
     charge_amount { Faker::Number.between(from: 10_000, to: 99_900) }
 
     trait :farmer_exemptions do
