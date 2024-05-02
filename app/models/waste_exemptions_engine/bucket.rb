4ba65803a5c9b048ae01e689f4a7163a0f084e43
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-05-02 15:22:53 +0100

    [RUBY-3066] Add association between exemptions and buckets through bucket_exemptions in WasteExemptionsEngine

--- app/models/waste_exemptions_engine/bucket.rb
+++ app/models/waste_exemptions_engine/bucket.rb
@@ -6,6 +6,7 @@ module WasteExemptionsEngine
 
     has_paper_trail
 
+    has_many :bucket_exemptions
     has_many :exemptions, through: :bucket_exemptions
 
     has_one :initial_compliance_charge, lambda {
