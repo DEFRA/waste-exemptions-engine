7def920850d55a516179e7abc9d3d678fd440832
Leonid Brujev <brujev@gmail.com>
2024-05-03 12:22:04 +0100

    [RUBY-3113] improving test coverage

--- spec/models/waste_exemptions_engine/concerns/can_convert_pence_to_pounds_spec.rb
+++ spec/models/waste_exemptions_engine/concerns/can_convert_pence_to_pounds_spec.rb
@@ -12,4 +12,10 @@ RSpec.shared_examples_for "can_convert_pence_to_pounds" do |factory_name, field|
     object = build(factory_name.to_s.underscore.to_sym, field => 100)
     expect(object.send(:"#{field}_in_pounds")).to eq(format("%.2f", object.send(field).to_f / 100))
   end
+
+  it "can set {field}_in_pounds" do
+    object = build(factory_name.to_s.underscore.to_sym, field => 100)
+    object.send(:"#{field}_in_pounds=", "1.23")
+    expect(object.send(field)).to eq(123)
+  end
 end
--- spec/support/factories/band.rb
+++ spec/support/factories/band.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :band, class: "WasteExemptionsEngine::Band" do
     name { "Band #{Faker::Lorem.unique.word}" }
-    sequence(:sequence) { Faker::Number.unique.between(from: 1, to: 10) } # reserved word
+    sequence(:sequence) { Faker::Number.unique.between(from: 1, to: 99) } # reserved word
     registration_charge { Faker::Number.between(from: 1000, to: 9900) }
     initial_compliance_charge { Faker::Number.between(from: 10_000, to: 99_900) }
     additional_compliance_charge { Faker::Number.between(from: 10_000, to: 99_900) }
