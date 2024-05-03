3f3aaaba8a0d79be15b6ced67afe167a6ae0b7ba
Leonid Brujev <brujev@gmail.com>
2024-05-04 00:39:00 +0100

    improving test coverage

--- spec/models/waste_exemptions_engine/band_spec.rb
+++ spec/models/waste_exemptions_engine/band_spec.rb
@@ -12,6 +12,40 @@ module WasteExemptionsEngine
           expect(band).to respond_to(property)
         end
       end
+
+      context "when initial_compliance_charge set" do
+        let(:charge) { build(:charge, :initial_compliance_charge, charge_amount: 100) }
+        let(:band) { build(:band, initial_compliance_charge: charge) }
+
+        it "responds to initial_compliance_charge" do
+          expect(band.initial_compliance_charge).to be_present
+        end
+
+        it "has charge_type set to initial_compliance_charge" do
+          expect(band.initial_compliance_charge.charge_type).to eq("initial_compliance_charge")
+        end
+
+        it "has charge_amount set to 100" do
+          expect(band.initial_compliance_charge.charge_amount).to eq(100)
+        end
+      end
+
+      context "when additional_compliance_charge set" do
+        let(:charge) { build(:charge, :additional_compliance_charge, charge_amount: 100) }
+        let(:band) { build(:band, additional_compliance_charge: charge) }
+
+        it "responds to additional_compliance_charge" do
+          expect(band.additional_compliance_charge).to be_present
+        end
+
+        it "has charge_type set to additional_compliance_charge" do
+          expect(band.additional_compliance_charge.charge_type).to eq("additional_compliance_charge")
+        end
+
+        it "has charge_amount set to 100" do
+          expect(band.additional_compliance_charge.charge_amount).to eq(100)
+        end
+      end
     end
   end
 end
--- spec/support/factories/band.rb
+++ spec/support/factories/band.rb
@@ -5,7 +5,9 @@ FactoryBot.define do
     name { "Band #{Faker::Lorem.unique.word}" }
     sequence(:sequence) { Faker::Number.unique.between(from: 1, to: 99) } # reserved word
 
-    initial_compliance_charge { association :charge, :initial_compliance_charge }
-    additional_compliance_charge { association :charge, :additional_compliance_charge }
+    trait :with_charges do
+      initial_compliance_charge { association :charge, :initial_compliance_charge }
+      additional_compliance_charge { association :charge, :additional_compliance_charge }
+    end
   end
 end
