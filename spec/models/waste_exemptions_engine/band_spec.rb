44427e5c9cd79a35b5bc37ceff681fa0b3dfe311
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-05-10 17:11:02 +0100

    [RUBY-3065] Add callback to prevent destroying an exemption with exemption and add spec

--- spec/models/waste_exemptions_engine/band_spec.rb
+++ spec/models/waste_exemptions_engine/band_spec.rb
@@ -13,6 +13,35 @@ module WasteExemptionsEngine
         end
       end
 
+      describe "callbacks" do
+        context "when destroying a band" do
+          context "when it has no exemptions" do
+            it "destroys the band" do
+              band = create(:band, :no_charges)
+              band.destroy
+              expect(described_class.exists?(band.id)).to be(false)
+            end
+          end
+
+          context "when it has exemptions" do
+            let(:band) { create(:band, :no_charges) }
+
+            before do
+              create(:exemption, band: band)
+              band.destroy
+            end
+
+            it "does not destroy the band" do
+              expect(described_class.exists?(band.id)).to be(true)
+            end
+
+            it "adds an error to the band" do
+              expect(band.errors[:base]).to include("Cannot delete band while it has exemptions associated")
+            end
+          end
+        end
+      end
+
       context "when initial_compliance_charge set" do
         let(:band) { create(:band, :no_charges) }
         let(:charge) { create(:charge, :initial_compliance_charge, charge_amount: 100, chargeable: band) }
@@ -50,6 +79,23 @@ module WasteExemptionsEngine
         it "has charge_amount set to 100" do
           charge
           expect(band.reload.additional_compliance_charge.charge_amount).to eq(100)
+        end
+      end
+
+      describe "#can_be_destroyed" do
+        context "when band has no exemptions" do
+          it "returns true" do
+            band = create(:band, :no_charges)
+            expect(band.can_be_destroyed?).to be(true)
+          end
+        end
+
+        context "when band has exemptions" do
+          it "returns false" do
+            band = create(:band, :no_charges)
+            create(:exemption, band: band)
+            expect(band.can_be_destroyed?).to be(false)
+          end
         end
       end
     end
