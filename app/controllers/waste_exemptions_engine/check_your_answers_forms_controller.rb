ec19962edeb3cf1842d12ee32b79c191f78fbd51
Leonid Brujev <brujev@gmail.com>
2024-05-20 12:25:59 +0100

    [RUBY-3115] Edit contact name functionality for Check Your Answers form

--- app/controllers/waste_exemptions_engine/check_your_answers_forms_controller.rb
+++ app/controllers/waste_exemptions_engine/check_your_answers_forms_controller.rb
@@ -9,5 +9,23 @@ module WasteExemptionsEngine
     def create
       super(CheckYourAnswersForm, "check_your_answers_form")
     end
+
+    def edit_contact_name
+      transition_to_edit("edit_contact_name")
+    end
+
+    private
+
+    def transition_to_edit(transition)
+      find_or_initialize_registration(params[:token])
+
+      @transient_registration.update(
+        temp_check_your_answers_flow: true,
+        workflow_history: @transient_registration.workflow_history << "check_your_answers_form"
+      )
+
+      @transient_registration.send(:"#{transition}!")
+      redirect_to_correct_form
+    end
   end
 end
