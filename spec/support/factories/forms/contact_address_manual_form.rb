95551b4d2f979ea16784fdb09a82fd59ba5ced11
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-05-23 16:25:55 +0100

    [RUBY-3117] Add edit contact address feature to check your answers page and update workflow transitions (#994)

--- spec/support/factories/forms/contact_address_manual_form.rb
+++ spec/support/factories/forms/contact_address_manual_form.rb
@@ -6,4 +6,10 @@ FactoryBot.define do
       new(create(:new_registration, workflow_state: "contact_address_manual_form"))
     end
   end
+
+  factory :check_your_answers_contact_address_manual_form, class: "WasteExemptionsEngine::ContactAddressManualForm" do
+    initialize_with do
+      new(create(:new_registration, workflow_state: "contact_address_manual_form", temp_check_your_answers_flow: true))
+    end
+  end
 end
