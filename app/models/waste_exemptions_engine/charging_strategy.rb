cff32baa8b4c26d34852bde29a423054e8a0160d
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-06-06 22:04:24 +0100

    ChargingStrategy base class

---
+++ app/models/waste_exemptions_engine/charging_strategy.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  class ChargingStrategy
+    attr_reader :order, :highest_band
+
+    def initialize(order)
+      @order = order
+      @highest_band = order.highest_band
+    end
+
+    def registration_charge_amount
+      order.exemptions.empty? ? 0 : Charge.find_by(charge_type: "registration_charge").charge_amount
+    end
+
+    def charge_details
+      ChargeDetail.new(
+        registration_charge_amount:,
+        band_charge_details:
+      )
+    end
+
+    def total_compliance_charge_amount
+      @total_compliance_charge_amount ||=
+        (order.bucket&.initial_compliance_charge&.charge_amount || 0) +
+        band_charge_details.sum do |bc|
+          bc.initial_compliance_charge_amount + bc.additional_compliance_charge_amount
+        end
+    end
+
+    def total_charge_amount
+      @total_charge_amount ||= registration_charge_amount + total_compliance_charge_amount
+    end
+
+    def band_charge_details
+      @band_charge_details ||= populate_band_charge_details
+    end
+
+    private
+
+    def populate_band_charge_details
+      bands = Band.where(id: order.exemptions.map(&:band_id).uniq)
+      bands.map do |band|
+        initial_compliance_charge_amount = initial_compliance_charge(band)
+        additional_compliance_charge_amount = additional_compliance_charge(
+          band,
+          initial_compliance_charge_amount.positive?
+        )
+        BandChargeDetail.new(
+          band_id: band.id,
+          initial_compliance_charge_amount:,
+          additional_compliance_charge_amount:
+        )
+      end
+    end
+
+    def initial_compliance_charge(_band)
+      raise NotImplementedError
+    end
+
+    def additional_compliance_charge(_band, _initial_compliance_charge_applied)
+      raise NotImplementedError
+    end
+  end
+end
---
+++ spec/services/waste_exemptions_engine/charging_strategy_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+
+  RSpec.describe ChargingStrategy do
+    include_context "with bands and charges"
+    include_context "with an order with exemptions"
+
+    # A subclass of described_class with the virtual methods implemented for test purposes:
+    let(:strategy_test_class) do
+      Class.new(described_class) do
+        attr_reader :band_1, :band_2, :band_3
+
+        def initialize(order)
+          @band_1, @band_2, @band_3 = Band.all.limit(3).sort_by(&:sequence).to_a
+
+          super(order)
+        end
+
+        def initial_compliance_charge(band)
+          case band
+          when band_1, band_2
+            0
+          else
+            10
+          end
+        end
+
+        def additional_compliance_charge(band, _initial_compliance_charge_applied)
+          case band
+          when band_1
+            4
+          when band_2
+            5
+          else
+            7
+          end
+        end
+      end
+    end
+
+    describe "#registration_charge" do
+      subject(:strategy_registration_charge_amount) { strategy_test_class.new(order).registration_charge_amount }
+
+      shared_examples "constant registration charge" do
+        it { expect(strategy_registration_charge_amount).to eq registration_charge_amount }
+      end
+
+      context "with an empty order" do
+        let(:exemptions) { [] }
+
+        it { expect(strategy_registration_charge_amount).to be_zero }
+      end
+
+      context "with an order with a single exemption" do
+        let(:exemptions) { single_band_single_exemption }
+
+        it_behaves_like "constant registration charge"
+      end
+
+      context "with an order with multiple exemptions in a single band" do
+        let(:exemptions) { single_band_multiple_exemptions }
+
+        it_behaves_like "constant registration charge"
+      end
+
+      context "with an order with multiple exemptions in multiple bands" do
+        let(:exemptions) { multiple_bands_multiple_exemptions }
+
+        it_behaves_like "constant registration charge"
+      end
+    end
+
+    describe "#charge_details" do
+      subject(:charge_details) { strategy_test_class.new(order).charge_details }
+
+      let(:exemptions) { multiple_bands_multiple_exemptions }
+
+      it { expect(charge_details).to be_a(ChargeDetail) }
+      it { expect(charge_details.registration_charge_amount).to eq registration_charge_amount }
+      it { expect(charge_details.band_charge_details.length).to eq 3 }
+      it { expect(charge_details.bucket_charge_amount).to be_zero }
+    end
+
+    describe "#total_charge_amount" do
+      subject(:strategy) { strategy_test_class.new(order) }
+
+      let(:exemptions) { multiple_bands_multiple_exemptions }
+
+      it do
+        expect(strategy.total_charge_amount).to eq(
+          strategy.registration_charge_amount +
+          strategy.charge_details.band_charge_details.sum do |bc|
+            bc.initial_compliance_charge_amount + bc.additional_compliance_charge_amount
+          end
+        )
+      end
+    end
+  end
+end
