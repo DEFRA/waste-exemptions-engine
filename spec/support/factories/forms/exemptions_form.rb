b61ce50fdc7ee189e9fc164b67d261adba1e2a48
Leonid Brujev <brujev@gmail.com>
2024-06-12 15:37:37 +0100

    [RUBY-3120] chore: Refactor exemptions form factory code for readability

--- spec/support/factories/forms/exemptions_form.rb
+++ spec/support/factories/forms/exemptions_form.rb
@@ -7,7 +7,8 @@ FactoryBot.define do
 
   factory :check_your_answers_edit_exemptions_form, class: "WasteExemptionsEngine::ExemptionsForm" do
     initialize_with do
-      new(create(:new_registration, workflow_state: "exemptions_form", exemption_ids: WasteExemptionsEngine::Exemption.limit(5).pluck(:id),
+      new(create(:new_registration, workflow_state: "exemptions_form",
+                                    exemption_ids: WasteExemptionsEngine::Exemption.limit(5).pluck(:id),
                                     temp_check_your_answers_flow: true))
     end
   end
