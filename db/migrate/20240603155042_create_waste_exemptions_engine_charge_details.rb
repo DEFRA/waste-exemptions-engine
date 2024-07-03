654a453f7cebf562be99f27b15cdc9b466b2977c
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-07-03 18:55:45 +0100

    Change default bucket charge amount from nil to zero

--- db/migrate/20240603155042_create_waste_exemptions_engine_charge_details.rb
+++ db/migrate/20240603155042_create_waste_exemptions_engine_charge_details.rb
@@ -4,7 +4,7 @@ class CreateWasteExemptionsEngineChargeDetails < ActiveRecord::Migration[7.1]
   def change
     create_table :charge_details do |t|
       t.integer :registration_charge_amount
-      t.integer :bucket_charge_amount, default: nil
+      t.integer :bucket_charge_amount, default: 0
 
       t.timestamps
     end
