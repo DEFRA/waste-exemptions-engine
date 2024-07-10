82fdd0b9848d48aecc4945bbf2b1ec43c4f6fde0
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-07-10 13:38:57 +0100

    [RUBY-3218] Fix bug with removed workflow transition, add spec for expected transitions, and rename shared example for clarity
    - Added new transition from `is_a_farmer_form` to `site_postcode_form` in `can_use_renewing_registration_workflow`.
    - Updated error handling in `transient_registration` to include detailed error messages.
    - Renamed shared test examples to "simple progressing transition" and "simple returning transition" for clarity.
    - Updated various test descriptions to reflect the new shared example names.

--- spec/models/waste_exemptions_engine/new_registration_workflow_state/contact_position_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/contact_position_form_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe NewRegistration do
     describe "#workflow_state" do
-      it_behaves_like "a simple bidirectional transition",
+      it_behaves_like "a simple progressing transition",
                       current_state: :contact_position_form,
                       next_state: :check_contact_phone_form,
                       factory: :new_registration
--- spec/models/waste_exemptions_engine/new_registration_workflow_state/registration_number_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/registration_number_form_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe NewRegistration do
     describe "#workflow_state" do
-      it_behaves_like "a simple bidirectional transition",
+      it_behaves_like "a simple progressing transition",
                       current_state: :registration_number_form,
                       next_state: :check_registered_name_and_address_form,
                       factory: :new_registration
