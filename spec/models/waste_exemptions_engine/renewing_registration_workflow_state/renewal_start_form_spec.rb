7eee94163944e75ac2162c70e740088dcdd046e1
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-08-14 13:19:01 +0100

    rename renew_without_changes_form to confirm_renewal_form (#1066)
    https://eaflood.atlassian.net/browse/RUBY-3291

--- spec/models/waste_exemptions_engine/renewing_registration_workflow_state/renewal_start_form_spec.rb
+++ spec/models/waste_exemptions_engine/renewing_registration_workflow_state/renewal_start_form_spec.rb
@@ -7,16 +7,16 @@ module WasteExemptionsEngine
     describe "#workflow_state" do
       subject(:renewing_registration) { create(:renewing_registration, workflow_state: :renewal_start_form) }
 
-      context "when subject.should_renew_without_changes? is true" do
-        next_state = :renew_without_changes_form
+      context "when subject.should_confirm_renewal? is true" do
+        next_state = :confirm_renewal_form
 
-        it "can only transition to :renew_without_changes_form" do
+        it "can only transition to :confirm_renewal_form" do
           permitted_states = Helpers::WorkflowStates.permitted_states(renewing_registration)
           expect(permitted_states).to contain_exactly(next_state)
         end
 
-        it "changes to :renew_without_changes_form after the 'next' event" do
-          expect(renewing_registration).to transition_from(:renewal_start_form).to(:renew_without_changes_form).on_event(:next)
+        it "changes to :confirm_renewal_form after the 'next' event" do
+          expect(renewing_registration).to transition_from(:renewal_start_form).to(:confirm_renewal_form).on_event(:next)
         end
       end
     end
