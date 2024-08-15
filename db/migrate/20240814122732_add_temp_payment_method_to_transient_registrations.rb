b26defb1721cdc9a3fb3e03f38ffa04bf8874fb5
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-08-15 11:40:10 +0100

    [RUBY-3173] Add `# frozen_string_literal: true` comment to migration file

--- db/migrate/20240814122732_add_temp_payment_method_to_transient_registrations.rb
+++ db/migrate/20240814122732_add_temp_payment_method_to_transient_registrations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTempPaymentMethodToTransientRegistrations < ActiveRecord::Migration[7.1]
   def change
     add_column :transient_registrations, :temp_payment_method, :string
