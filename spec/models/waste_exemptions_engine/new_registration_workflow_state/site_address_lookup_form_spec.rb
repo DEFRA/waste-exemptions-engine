8eac317e6929c4a75dd4cbedcc5ad4b32f476fec
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-08-21 14:43:12 +0100

    Fix transient_registration class types in existing specs

--- spec/models/waste_exemptions_engine/new_registration_workflow_state/site_address_lookup_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_registration_workflow_state/site_address_lookup_form_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe NewRegistration do
     describe "#workflow_state" do
-      it_behaves_like "site_address_lookup_form"
+      it_behaves_like "site_address_lookup_form", :new_registration, :check_your_answers_form
     end
   end
 end
--- spec/support/factories/forms/edit_exemptions_form.rb
+++ spec/support/factories/forms/edit_exemptions_form.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :edit_exemptions_form, class: "WasteExemptionsEngine::EditExemptionsForm" do
     initialize_with do
-      new(create(:renewing_registration, workflow_state: "edit_exemptions_form"))
+      new(create(:front_office_edit_registration, workflow_state: "edit_exemptions_form"))
     end
   end
 end
