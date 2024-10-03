bc04b392f1170b8cedd3a34218173cac41f5e944
Leonid Brujev <brujev@gmail.com>
2024-10-03 11:29:38 +0100

    [RUBY-3343] Refactor certificate email confirmation tests

--- spec/requests/waste_exemptions_engine/certificates_spec.rb
+++ spec/requests/waste_exemptions_engine/certificates_spec.rb
@@ -15,31 +15,36 @@ module WasteExemptionsEngine
     let(:base_path) { "/waste_exemptions_engine/#{registration.reference}/certificate" }
     let(:valid_email_uppercase) { valid_email.upcase }
     let(:valid_email_mixed_case) { "ConTact@ExAmple.CoM" }
+    let(:valid_email_params) { { confirm_email_form: { email: valid_email } } }
+    let(:invalid_email_params) { { confirm_email_form: { email: invalid_email } } }
+    let(:form) { build(:confirm_email_form) }
 
     describe "POST process_email" do
       context "with valid email" do
         it "sets the valid email in session and redirects to the certificate page" do
-          post process_email_path, params: { email: valid_email }
+          post process_email_path, params: { confirm_email_form: { email: valid_email } }
           expect(response).to redirect_to("#{base_path}?token=#{token}")
         end
 
         it "redirects to the certificate page with uppercase email" do
-          post process_email_path, params: { email: valid_email_uppercase }
+          post process_email_path, params: { confirm_email_form: { email: valid_email_uppercase } }
           expect(response).to redirect_to("#{base_path}?token=#{token}")
         end
 
         it "redirects to the certificate page with mixed case email" do
-          post process_email_path, params: { email: valid_email_mixed_case }
+          post process_email_path, params: { confirm_email_form: { email: valid_email_mixed_case } }
           expect(response).to redirect_to("#{base_path}?token=#{token}")
         end
       end
 
       context "with invalid email" do
         it "does not set the email in session and renders the confirmation page with an error" do
-          post process_email_path, params: { email: invalid_email }
+          allow(WasteExemptionsEngine::ConfirmEmailForm).to receive(:new).and_return(form)
+
+          post process_email_path, params: invalid_email_params
 
           aggregate_failures do
-            expect(flash[:error]).to be_present
+            expect(form.errors[:email].count).to eq(1)
             expect(response).to render_template(:confirm_email)
           end
         end
@@ -50,7 +55,7 @@ module WasteExemptionsEngine
       context "with valid email in session and valid token" do
         subject(:get_certificate) { get "#{base_path}?token=#{token}" }
 
-        before { post process_email_path, params: { email: valid_email } }
+        before { post process_email_path, params: valid_email_params }
 
         it "renders the HTML certificate" do
           get_certificate
@@ -63,7 +68,7 @@ module WasteExemptionsEngine
       end
 
       context "with valid email in session but invalid token" do
-        before { post process_email_path, params: { email: valid_email } }
+        before { post process_email_path, params: valid_email_params }
 
         it "redirects due to invalid token" do
           get "#{base_path}?token=invalidtoken"
@@ -76,7 +81,7 @@ module WasteExemptionsEngine
       end
 
       context "without valid email in session or token" do
-        before { post process_email_path, params: { email: invalid_email } }
+        before { post process_email_path, params: invalid_email_params }
 
         it "redirects to the email confirmation page" do
           get base_path
@@ -89,7 +94,7 @@ module WasteExemptionsEngine
       end
 
       context "without valid email in session but with valid token" do
-        before { post process_email_path, params: { email: invalid_email } }
+        before { post process_email_path, params: invalid_email_params }
 
         it "redirects to the email confirmation page" do
           get "#{base_path}?token=#{token}"
@@ -108,7 +113,7 @@ module WasteExemptionsEngine
       let(:base_path) { "/waste_exemptions_engine/#{registration.reference}/pdf_certificate" }
 
       context "with valid email in session and valid token" do
-        before { post process_email_path, params: { email: valid_email } }
+        before { post process_email_path, params: valid_email_params }
 
         it "renders the page" do
           get_certificate
@@ -118,7 +123,7 @@ module WasteExemptionsEngine
       end
 
       context "with valid email in session but invalid token" do
-        before { post process_email_path, params: { email: valid_email } }
+        before { post process_email_path, params: valid_email_params }
 
         it "redirects due to invalid token" do
           get "#{base_path}?token=invalidtoken"
@@ -131,7 +136,7 @@ module WasteExemptionsEngine
       end
 
       context "without valid email in session" do
-        before { post process_email_path, params: { email: invalid_email } }
+        before { post process_email_path, params: invalid_email_params }
 
         it "redirects to the email confirmation page" do
           get "#{base_path}.pdf"
@@ -153,7 +158,7 @@ module WasteExemptionsEngine
       end
 
       context "with an invalid token" do
-        before { post process_email_path, params: { email: valid_email } }
+        before { post process_email_path, params: valid_email_params }
 
         it "redirects due to invalid token" do
           get certificate_confirm_email_path(registration.reference, token: "invalidtoken")
---
+++ spec/support/factories/forms/confirm_email_form.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :confirm_email_form, class: "WasteExemptionsEngine::ConfirmEmailForm"
+end
