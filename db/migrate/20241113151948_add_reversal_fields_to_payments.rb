4506862635544fa3f7cd256c3af043dc13218c59
Jerome Pratt <jerome.pratt@hey.com>
2024-11-20 12:50:38 +0000

    [RUBY-2713] Refactor reversal fields in payments
    Updated migration to replace `reversed_by`, `reversed_at`, and `reversal_id` fields with `created_by` and `associated_payment_id`. Adjusted indexes accordingly in the schema.

--- db/migrate/20241113151948_add_reversal_fields_to_payments.rb
+++ db/migrate/20241113151948_add_reversal_fields_to_payments.rb
@@ -2,9 +2,8 @@
 
 class AddReversalFieldsToPayments < ActiveRecord::Migration[7.1]
   def change
-    add_column :payments, :reversed_by, :string
-    add_column :payments, :reversed_at, :datetime
-    add_column :payments, :reversal_id, :integer
-    add_index :payments, :reversal_id
+    add_column :payments, :created_by, :string
+    add_column :payments, :associated_payment_id, :integer
+    add_index :payments, :associated_payment_id
   end
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -199,12 +199,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_13_154617) do
     t.bigint "account_id"
     t.string "reference"
     t.string "comments", limit: 500
-    t.string "reversed_by"
-    t.datetime "reversed_at"
-    t.integer "reversal_id"
+    t.string "created_by"
+    t.integer "associated_payment_id"
     t.index ["account_id"], name: "index_payments_on_account_id"
+    t.index ["associated_payment_id"], name: "index_payments_on_associated_payment_id"
     t.index ["order_id"], name: "index_payments_on_order_id"
-    t.index ["reversal_id"], name: "index_payments_on_reversal_id"
   end
 
   create_table "people", id: :serial, force: :cascade do |t|
