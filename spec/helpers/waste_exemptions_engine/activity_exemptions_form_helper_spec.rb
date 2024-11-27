5895856647b7cef4fd0ec1791386b893ec02348f
Leonid Brujev <brujev@gmail.com>
2024-11-27 17:00:36 +0000

    [RUBY-3055] Refactor failing spec

--- spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
@@ -8,18 +8,18 @@ module WasteExemptionsEngine
       let(:activity_one) { create(:waste_activity) }
       let(:activity_two) { create(:waste_activity) }
       let(:activity_three) { create(:waste_activity) }
-      let(:exemption_one) { create(:exemption, waste_activity_id: activity_one.id) }
-      let(:exemption_two) { create(:exemption, waste_activity_id: activity_two.id) }
-      let(:exemption_three) { create(:exemption, waste_activity_id: activity_one.id) }
-      let(:exemption_four) { create(:exemption, waste_activity_id: activity_three.id) }
 
       it "returns a list of selected exemptions ordered by activity id and exemption id" do
         aggregate_failures "sorting waste activities" do
-          exemptions = helper.selected_activity_exemptions([exemption_one.id, exemption_two.id, exemption_three.id, exemption_four.id])
-          expect(exemptions.first).to eq(exemption_one)
-          expect(exemptions.second).to eq(exemption_three)
-          expect(exemptions.third).to eq(exemption_two)
-          expect(exemptions.fourth).to eq(exemption_four)
+          activity_ids = [activity_one.id, activity_two.id, activity_three.id]
+          create_list(:exemption, 4, waste_activity_id: activity_ids.sample)
+
+          exemptions = helper.selected_activity_exemptions(activity_ids)
+          exemption_acivity_ids = exemptions.map(&:waste_activity_id)
+          expect(exemptions.first.waste_activity_id).to eq(exemption_acivity_ids.min)
+          expect(exemptions.second.waste_activity_id).to be >= exemption_acivity_ids.min
+          expect(exemptions.third.waste_activity_id).to be <= exemption_acivity_ids.max
+          expect(exemptions.last.waste_activity_id).to eq(exemption_acivity_ids.max)
         end
       end
     end
