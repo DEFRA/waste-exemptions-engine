c457bbbdf4104e4baf60616a21015ea9b6ca2a44
Leonid Brujev <brujev@gmail.com>
2024-11-27 16:15:08 +0000

    [RUBY-3055] Refactoring spec

--- spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
@@ -5,21 +5,19 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe ActivityExemptionsFormsHelper do
     describe "#selected_activity_exemptions" do
-      before do
-        create_list(:waste_activity, 3)
-      end
+      let(:activity_one) { create(:waste_activity) }
+      let(:activity_two) { create(:waste_activity) }
+      let(:activity_three) { create(:waste_activity) }
+      let(:exemption_one) { create(:exemption, waste_activity_id: activity_one.id) }
+      let(:exemption_two) { create(:exemption, waste_activity_id: activity_two.id) }
+      let(:exemption_three) { create(:exemption, waste_activity_id: activity_one.id) }
+      let(:exemption_four) { create(:exemption, waste_activity_id: activity_three.id) }
 
       it "returns a list of selected exemptions ordered by activity id and exemption id" do
-        activities = WasteExemptionsEngine::WasteActivity.all
-        exemption_one = create(:exemption, waste_activity_id: activities.second.id)
-        exemption_two = create(:exemption, waste_activity_id: activities.second.id)
-        exemption_three = create(:exemption, waste_activity_id: activities.first.id)
-        exemption_four = create(:exemption, waste_activity_id: activities.third.id)
-
         aggregate_failures "sorting waste activities" do
           exemptions = helper.selected_activity_exemptions([exemption_one.id, exemption_two.id, exemption_three.id, exemption_four.id])
-          expect(exemptions.first).to eq(exemption_three)
-          expect(exemptions.second).to eq(exemption_one)
+          expect(exemptions.first).to eq(exemption_one)
+          expect(exemptions.second).to eq(exemption_three)
           expect(exemptions.third).to eq(exemption_two)
           expect(exemptions.fourth).to eq(exemption_four)
         end
