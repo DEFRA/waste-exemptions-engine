54a455395ae15d4fc7d51ab40f01e03375fbb342
Leonid Brujev <brujev@gmail.com>
2024-11-28 19:22:09 +0000

    [RUBY-3055] refactoring helper sorting and specs

--- app/models/waste_exemptions_engine/waste_activity.rb
+++ app/models/waste_exemptions_engine/waste_activity.rb
@@ -4,7 +4,9 @@ module WasteExemptionsEngine
   class WasteActivity < ApplicationRecord
     self.table_name = "waste_activities"
 
+    ACTIVITY_CATEGORIES = { using_waste: 0, treating_waste: 1, disposing_of_waste: 2, storing_waste: 3 }.freeze
+
     # reusing categories defined in Exemption model
-    enum category: { using_waste: 0, treating_waste: 1, disposing_of_waste: 2, storing_waste: 3 }
+    enum category: ACTIVITY_CATEGORIES
   end
 end
--- spec/support/factories/waste_activity.rb
+++ spec/support/factories/waste_activity.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :waste_activity, class: "WasteExemptionsEngine::WasteActivity" do
     name { Faker::Lorem.words(number: 3) }
-    category { %w[storing_waste treating_waste disposing_of_waste using_waste].sample }
+    category { WasteExemptionsEngine::WasteActivity::ACTIVITY_CATEGORIES.values.sample }
   end
 end
