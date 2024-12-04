708e1f36a600cfcdb26a5c7fccf7f9911ad78eba
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-12-04 18:39:50 +0000

    Remove any pre-exisintg transient registration before starting renewal journey

--- app/services/waste_exemptions_engine/renewal_start_service.rb
+++ app/services/waste_exemptions_engine/renewal_start_service.rb
@@ -3,7 +3,17 @@
 module WasteExemptionsEngine
   class RenewalStartService < BaseService
     def run(registration:)
-      RenewingRegistration.find_or_create_by(reference: registration.reference)
+      # Check for edge cases at renewal time where an edit registration was previously
+      # created and has not yet been cleaned up.
+      transient_registration = TransientRegistration.where(reference: registration.reference).first
+
+      if transient_registration.present?
+        Rails.logger.info "Removing existing transient registration of type #{transient_registration.class} " \
+                          "before starting renewal of #{registration.reference}"
+        transient_registration.destroy
+      end
+
+      RenewingRegistration.create!(reference: registration.reference)
     end
   end
 end
--- spec/services/waste_exemptions_engine/renewal_start_service_spec.rb
+++ spec/services/waste_exemptions_engine/renewal_start_service_spec.rb
@@ -5,10 +5,27 @@ require "rails_helper"
 module WasteExemptionsEngine
   RSpec.describe RenewalStartService do
     describe "run" do
-      let(:service) { described_class.run(registration: create(:registration, :complete)) }
+      subject(:run_service) { described_class.run(registration:) }
 
-      it "creates a new RenewingRegistration" do
-        expect { service }.to change(RenewingRegistration, :count).by(1)
+      let(:registration) { create(:registration, :complete) }
+
+      context "when no transient registration already exists for this registration" do
+        it "creates a new RenewingRegistration" do
+          expect { run_service }.to change(RenewingRegistration, :count).by(1)
+        end
+      end
+
+      context "when a transient registration already exists for the registration" do
+        let!(:existing_transient_registration) { create(:front_office_edit_registration, reference: registration.reference) }
+        let(:existing_transient_registration_id) { existing_transient_registration.id }
+
+        it "deletes the preexisting registration" do
+          expect { run_service }.to change { TransientRegistration.where(id: existing_transient_registration_id).count }.to(0)
+        end
+
+        it "creates a new RenewingRegistration" do
+          expect { run_service }.to change(RenewingRegistration, :count).by(1)
+        end
       end
     end
   end
