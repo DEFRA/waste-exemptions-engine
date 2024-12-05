6175a2241b32354d9ecfaec793423cca6f3d1a28
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-12-05 10:00:53 +0000

    [RUBY-2704] Refactor payment scopes in `Payment` model (#1135)
    * [RUBY-2704] Move payment back office scopes to back office app
    
    * [RUBY-2704] Move delegations of scopes which are used in back office to
    back office
    
    * [RUBY-2704] Remove `payment_spec.rb` tests from `waste_exemptions_engine`
    
    This commit deletes the `payment_spec.rb` file, which contained tests for payment-related scopes such as `refunds_and_reversals`, `excluding_refunds_and_reversals`, `refundable`, and `reverseable`.

--- app/models/waste_exemptions_engine/payment.rb
+++ app/models/waste_exemptions_engine/payment.rb
@@ -66,17 +66,6 @@ module WasteExemptionsEngine
     validates :payment_type, presence: true
     validates :payment_status, presence: true
 
-    scope :not_cancelled, -> { where.not(payment_status: PAYMENT_STATUS_CANCELLED) }
-    scope :refunds_and_reversals, -> { where(payment_type: [PAYMENT_TYPE_REFUND, PAYMENT_TYPE_REVERSAL]) }
-    scope :excluding_refunds_and_reversals, -> { where.not(payment_type: [PAYMENT_TYPE_REFUND, PAYMENT_TYPE_REVERSAL]) }
-    scope :refundable, -> { where(payment_type: REFUNDABLE_PAYMENT_TYPES) }
-    scope :reverseable, lambda {
-      excluding_refunds_and_reversals
-        .success
-        .where.not(payment_type: PAYMENT_TYPE_GOVPAY)
-        .where.not(id: Payment.where.not(associated_payment_id: nil).select(:associated_payment_id))
-    }
-
     def success?
       payment_status == PAYMENT_STATUS_SUCCESS
     end
