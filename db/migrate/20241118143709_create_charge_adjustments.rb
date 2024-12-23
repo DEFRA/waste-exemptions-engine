fe89d8a3876407df1b01d67d45b20e58fae90ea9
Jerome Pratt <jerome.pratt@hey.com>
2024-12-23 16:50:19 +0000

    [RUBY-3406] Add check for existing `charge_adjustments` table in migration
    This change ensures that the `charge_adjustments` table is only created if it does not already exist, preventing potential errors during migration.

--- db/migrate/20241118143709_create_charge_adjustments.rb
+++ db/migrate/20241118143709_create_charge_adjustments.rb
@@ -2,12 +2,14 @@
 
 class CreateChargeAdjustments < ActiveRecord::Migration[7.1]
   def change
-    create_table :charge_adjustments do |t|
-      t.references :account, null: false, foreign_key: true
-      t.integer :amount, null: false
-      t.string :adjustment_type, null: false
-      t.string :reason, null: false
-      t.timestamps
+    unless table_exists?(:charge_adjustments)
+      create_table :charge_adjustments do |t|
+        t.references :account, null: false, foreign_key: true
+        t.integer :amount, null: false
+        t.string :adjustment_type, null: false
+        t.string :reason, null: false
+        t.timestamps
+      end
     end
   end
 end
