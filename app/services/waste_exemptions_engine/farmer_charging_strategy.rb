0ad8a75a63c03d3b341c7c65fe2fe85389ed4d36
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-01-20 16:53:25 +0000

    Revert "Apply lowest-charge rule to farmer exemptions"
    This reverts commit c018a64086b1ed33310774ed39b02a4df241d10f.

--- app/services/waste_exemptions_engine/farmer_charging_strategy.rb
+++ app/services/waste_exemptions_engine/farmer_charging_strategy.rb
@@ -10,20 +10,6 @@ module WasteExemptionsEngine
       super
     end
 
-    # If the only selected exemptions are in the farmer bucket and the total compliance
-    # charge (if non-farmer-bucket) would have been less than the standard registration
-    # charge, registration_charge_amount should return the lower value.
-    def registration_charge_amount
-      default_registration_charge_amount = super
-
-      # Any non-bucket exemptions in the order?
-      return default_registration_charge_amount unless (order.exemptions - bucket.exemptions).empty?
-
-      non_bucket_compliance_charge = order.exemptions.sum { |ex| ex.band.initial_compliance_charge.charge_amount }
-
-      [non_bucket_compliance_charge, default_registration_charge_amount].min
-    end
-
     def charge_detail
       unless order.charge_detail.present?
         order.charge_detail = ChargeDetail.new(
