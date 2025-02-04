9c50b7aa6d3398f0377e42c0ddc20101a6644abe
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-02-04 12:37:44 +0000

    RUBY 3537 wex charging farmer journey farming exemptions and non farming exemptions route (#1182)
    Journey of user from farm exemptions journey to also add non farming exemptions
    
    
    * [RUBY-3537] Enhance exemptions handling in activity exemptions flow
    
    - Updated `ActivityExemptionsForm` to handle `temp_confirm_exemptions` and conditionally combine exemptions.
    - Modified `selected_activity_exemptions` helper to exclude farmer bucket exemptions when adding non-farming exemptions.
    - Refactored workflow transitions and method names in `can_use_new_charged_registration_workflow` to clarify handling of additional non-farm exemptions.
    
    * [RUBY-3537] Refactor `bucket_charge_amount` calculation in `FarmerChargingStrategy`
    
    - Simplified the logic in `bucket_charge_amount` to directly calculate the charge amount based on bucket exemptions present in the order.
    - Updated the test suite to reflect the new calculation logic, ensuring expected bucket charge amounts are correctly validated in various scenarios.
    - Introduced `expected_bucket_charge_amount` and `expected_farmers_bucket_charge_amount` in tests to encapsulate the logic for determining the lesser charge between the sum of
    
    * [RUBY-3537] Refactor exemption selection logic and update tests
    
    - Modified `selected_activity_exemptions` method to use a `transient_registration` object instead of `waste_activity_ids`.
    - Added a new scope `for_waste_activities` in the `Exemption` model for querying exemptions by waste activities.
    - Improved `bucket_charge_amount` method in `FarmerChargingStrategy` for better readability.
    - Updated view logic in `activity_exemptions_forms/new.html.erb` to use the
    
    * [RUBY-3537] Fix handling of `temp_exemptions` in `activity_exemptions_form.rb`
    
    Ensure `temp_exemptions` and `params[:temp_exemptions]` are combined as arrays to prevent errors when concatenating.
    
    * [RUBY-3537] Refactor and improve waste exemptions handling
    
    - Updated `for_waste_activities` scope in `Exemption` model to use a lambda for better readability.
    - Refactored `order_bucket_compliance_charge` calculation in `FarmerChargingStrategy` to use a multiline block for clarity.
    - Fixed typo in `activity_exemptions_form_helper_spec.rb` by adding a space in the frozen string literal comment.
    - Added `:aggregate_failures` to several tests in `activity_exempt
    
    * [RUBY-3537] Update transition in confirm_farm_exemptions_form_spec
    
    Changed the expected transition from `:farm_exemptions_form` to `:waste_activities_form` in the test for the `next` event within the `confirm_farm_exemptions_form_spec`.

--- app/models/waste_exemptions_engine/exemption.rb
+++ app/models/waste_exemptions_engine/exemption.rb
@@ -14,6 +14,9 @@ module WasteExemptionsEngine
     has_many :buckets, through: :bucket_exemptions
 
     scope :visible, -> { where(hidden: false) }
+    scope :for_waste_activities, lambda { |waste_activity_ids|
+      where(waste_activity_id: waste_activity_ids).order(:waste_activity_id, :id)
+    }
 
     enum category: { using_waste: 0, treating_waste: 1, disposing_of_waste: 2, storing_waste: 3 }
   end
