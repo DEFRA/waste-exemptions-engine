513bb2f2be28472300d69da4419e15ea981d5efb
Jerome Pratt <jerome.pratt@hey.com>
2025-02-04 16:06:17 +0000

    [RUBY-3537] Refactor exemption selection logic and update tests
    - Updated `selected_activity_exemptions` method to refine logic for excluding farming exemptions based on `temp_confirm_exemptions` value.
    - Adjusted conditions to handle `temp_confirm_exemptions` being `false` more explicitly.
    - Revised test contexts in `activity_exemptions_form_helper_spec.rb` to align with updated logic, including a new context for when `temp_confirm_exemptions` is `nil`.
    - Ensured tests cover

--- app/helpers/waste_exemptions_engine/activity_exemptions_forms_helper.rb
+++ app/helpers/waste_exemptions_engine/activity_exemptions_forms_helper.rb
@@ -3,11 +3,12 @@
 module WasteExemptionsEngine
   module ActivityExemptionsFormsHelper
     def selected_activity_exemptions(transient_registration)
+      # First get all exemptions for selected waste activities
       exemptions = WasteExemptionsEngine::Exemption.for_waste_activities(transient_registration.temp_waste_activities)
 
-      # If user wants to add non-farming exemptions,
+      # If user doesn't want farming exemptions and there is a farmer bucket,
       # exclude exemptions that are in the farmer bucket
-      if !transient_registration.temp_confirm_exemptions && WasteExemptionsEngine::Bucket.farmer_bucket.present?
+      if transient_registration.temp_confirm_exemptions == false && WasteExemptionsEngine::Bucket.farmer_bucket.present?
         farmer_bucket = WasteExemptionsEngine::Bucket.farmer_bucket
         exemptions = exemptions.where.not(id: farmer_bucket.exemption_ids)
       end
--- spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
+++ spec/helpers/waste_exemptions_engine/activity_exemptions_form_helper_spec.rb
@@ -16,7 +16,6 @@ module WasteExemptionsEngine
       let(:non_farming_exemptions) { create_list(:exemption, 2, waste_activity: activity_two) }
 
       before do
-        allow(transient_registration).to receive(:temp_confirm_exemptions).and_return(temp_confirm_exemptions)
         allow(WasteExemptionsEngine::Bucket).to receive(:farmer_bucket).and_return(farmer_bucket)
         allow(farmer_bucket).to receive(:exemption_ids).and_return(farming_exemptions.map(&:id))
 
@@ -25,7 +24,7 @@ module WasteExemptionsEngine
         non_farming_exemptions
       end
 
-      context "when user confirms farming exemptions" do
+      context "when temp_confirm_exemptions is true" do
         let(:temp_confirm_exemptions) { true }
 
         it "returns all exemptions for selected waste activities", :aggregate_failures do
@@ -35,7 +34,7 @@ module WasteExemptionsEngine
         end
       end
 
-      context "when user wants to add non-farming exemptions" do
+      context "when temp_confirm_exemptions is false" do
         let(:temp_confirm_exemptions) { false }
 
         it "excludes farming bucket exemptions", :aggregate_failures do
@@ -45,15 +44,28 @@ module WasteExemptionsEngine
         end
       end
 
-      context "when there is no farmer bucket" do
-        before do
-          allow(WasteExemptionsEngine::Bucket).to receive(:farmer_bucket).and_return(nil)
-        end
+      context "when temp_confirm_exemptions is nil" do
+        let(:temp_confirm_exemptions) { nil }
 
         it "returns all exemptions", :aggregate_failures do
           exemptions = helper.selected_activity_exemptions(transient_registration)
           expect(exemptions).to include(*farming_exemptions)
           expect(exemptions).to include(*non_farming_exemptions)
+        end
+      end
+
+      context "when there is no farmer bucket" do
+        before do
+          allow(WasteExemptionsEngine::Bucket).to receive(:farmer_bucket).and_return(nil)
+        end
+
+        it "returns all exemptions regardless of temp_confirm_exemptions value", :aggregate_failures do
+          [true, false, nil].each do |confirm_value|
+            transient_registration.temp_confirm_exemptions = confirm_value
+            exemptions = helper.selected_activity_exemptions(transient_registration)
+            expect(exemptions).to include(*farming_exemptions)
+            expect(exemptions).to include(*non_farming_exemptions)
+          end
         end
       end
     end
