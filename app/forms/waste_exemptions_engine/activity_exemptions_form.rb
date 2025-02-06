21a7ba4575d99df00584841e402dffbfbac8bf32
Jerome Pratt <jerome.pratt@hey.com>
2025-02-06 16:38:10 +0000

    [RUBY-3537] Refactor exemption handling logic in forms
    - Updated `ActivityExemptionsForm` to refine logic for combining exemptions, ensuring unique exemptions are stored.
    - Enhanced `FarmExemptionsForm` to similarly adjust exemption combination, leveraging `temp_exemptions` for improved clarity and maintenance.
    - Both forms now check `transient_registration.farm_affiliated?` to determine the appropriate exemption combination strategy.

--- app/forms/waste_exemptions_engine/activity_exemptions_form.rb
+++ app/forms/waste_exemptions_engine/activity_exemptions_form.rb
@@ -7,17 +7,18 @@ module WasteExemptionsEngine
     validates :temp_exemptions, "waste_exemptions_engine/exemptions": true
 
     def submit(params)
-      # If temp_confirm_exemptions is true, we're confirming the selection
-      # Otherwise, we're adding to it
-      new_activity_exemptions = if temp_confirm_exemptions
-                                 Array(params[:temp_exemptions])
-                               else
-                                 Array(temp_activity_exemptions) + Array(params[:temp_exemptions])
-                               end
+      new_activity_exemptions = Array(params[:temp_exemptions])
+      total_exemptions = if temp_confirm_exemptions == false && transient_registration.farm_affiliated?
+                           new_activity_exemptions + Array(temp_farm_exemptions)
+                         else
+                           new_activity_exemptions
+                         end
+
+
 
       attributes = {
         temp_activity_exemptions: new_activity_exemptions,
-        temp_exemptions: Array(temp_farm_exemptions) + new_activity_exemptions
+        temp_exemptions: total_exemptions.uniq
       }
 
       super(attributes)
--- app/forms/waste_exemptions_engine/farm_exemptions_form.rb
+++ app/forms/waste_exemptions_engine/farm_exemptions_form.rb
@@ -2,13 +2,20 @@
 
 module WasteExemptionsEngine
   class FarmExemptionsForm < BaseForm
-    delegate :temp_farm_exemptions, :temp_activity_exemptions, to: :transient_registration
+    delegate :temp_exemptions, :temp_farm_exemptions, :temp_activity_exemptions, to: :transient_registration
 
     def submit(params)
       # Set farm exemptions and combine with activity exemptions for temp_exemptions
+      new_farm_exemptions = Array(params[:temp_exemptions])
+      total_exemptions = if temp_confirm_exemptions == false && transient_registration.farm_affiliated?
+                           new_farm_exemptions + Array(temp_activity_exemptions)
+                         else
+                           new_farm_exemptions
+                         end
+
       attributes = {
         temp_farm_exemptions: params[:temp_exemptions],
-        temp_exemptions: Array(params[:temp_exemptions]) + Array(temp_activity_exemptions)
+        temp_exemptions: total_exemptions.uniq
       }
 
       super(attributes)
