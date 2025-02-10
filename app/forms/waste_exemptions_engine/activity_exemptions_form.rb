8ea4dc1e03b2520a77ed1bdb6caf9820a02f97f5
Jerome Pratt <jerome.pratt@hey.com>
2025-02-10 20:25:53 +0000

    [RUBY-3537] Refactor exemption handling in `ExemptionParamsService`
    - Simplified logic for combining exemptions by introducing `can_combine` variable.
    - Ensured that combined exemptions are unique and sorted.
    - Improved readability by aligning variable assignments and conditions.

--- app/forms/waste_exemptions_engine/activity_exemptions_form.rb
+++ app/forms/waste_exemptions_engine/activity_exemptions_form.rb
@@ -7,7 +7,6 @@ module WasteExemptionsEngine
     validates :temp_exemptions, "waste_exemptions_engine/exemptions": true
 
     def submit(params)
-
       attributes = ExemptionParamsService.run(
         registration: transient_registration,
         exemption_type: :activity,
--- app/services/waste_exemptions_engine/exemption_params_service.rb
+++ app/services/waste_exemptions_engine/exemption_params_service.rb
@@ -3,22 +3,27 @@
 module WasteExemptionsEngine
   class ExemptionParamsService < BaseService
     def run(registration:, exemption_type:, new_exemptions:)
-      new_exemptions      = Array(new_exemptions)
+      new_exemptions = Array(new_exemptions)
       activity_exemptions = Array(registration.temp_activity_exemptions)
-      farm_exemptions     = Array(registration.temp_farm_exemptions)
-      combine_exemptions             = registration.farm_affiliated? &&
-                            registration.temp_add_additional_non_farm_exemptions == true
+      farm_exemptions = Array(registration.temp_farm_exemptions)
+      can_combine = registration.farm_affiliated? && registration.temp_add_additional_non_farm_exemptions
 
       total_exemptions = case exemption_type
                          when :farm
-                           combine_exemptions ? (activity_exemptions + new_exemptions) : activity_exemptions
+                           if can_combine
+                             (activity_exemptions + new_exemptions).uniq.sort
+                           else
+                             new_exemptions.sort
+                           end
                          when :activity
-                           combine_exemptions ? (new_exemptions + farm_exemptions) : new_exemptions
-                         else
-                           new_exemptions
+                           if can_combine
+                             (new_exemptions + farm_exemptions).uniq.sort
+                           else
+                             new_exemptions.sort
+                           end
                          end
 
-      { "temp_#{exemption_type}_exemptions": new_exemptions, temp_exemptions: total_exemptions.uniq }
+      { "temp_#{exemption_type}_exemptions": new_exemptions, temp_exemptions: total_exemptions }
     end
   end
 end
