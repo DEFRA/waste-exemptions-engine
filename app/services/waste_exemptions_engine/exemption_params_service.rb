bba4da03904359cd6d338e0f8c6f15f185370229
Jerome Pratt <jerome.pratt@hey.com>
2025-02-10 17:43:28 +0000

    [RUBY-3537] Refactor `ExemptionParamsService` to simplify exemption logic
    - Streamlined the logic for determining `total_exemptions` by using a boolean `is_farm` to check conditions for farm-related exemptions.
    - Removed redundant code and comments to enhance readability.
    - Ensured the calculation of `total_exemptions` is concise and clear.

--- app/services/waste_exemptions_engine/exemption_params_service.rb
+++ app/services/waste_exemptions_engine/exemption_params_service.rb
@@ -1,32 +1,19 @@
-# frozen_string_literal: true
-
 module WasteExemptionsEngine
   class ExemptionParamsService < BaseService
     def run(registration:, exemption_type:, new_exemptions:)
-      new_exemptions = Array(new_exemptions)
-
+      new_exemptions      = Array(new_exemptions)
       activity_exemptions = Array(registration.temp_activity_exemptions)
-      farm_exemptions = Array(registration.temp_farm_exemptions)
+      farm_exemptions     = Array(registration.temp_farm_exemptions)
+      is_farm             = registration.farm_affiliated? && registration.temp_add_additional_non_farm_exemptions == true
 
-      # Determine total exemptions based on type and settings
       total_exemptions = case exemption_type
                          when :farm
-                           # Always include activity exemptions, add farm exemptions if allowed
-                           if new_exemptions.empty?
-                             activity_exemptions
-                           elsif registration.temp_add_additional_non_farm_exemptions && registration.farm_affiliated?
-                             activity_exemptions + new_exemptions
-                           else
-                             activity_exemptions
-                           end
+                           is_farm ? (activity_exemptions + new_exemptions) : activity_exemptions
                          when :activity
-                           # For activity exemptions, combine with farm exemptions if allowed
-                           if registration.temp_add_additional_non_farm_exemptions && registration.farm_affiliated?
-                             new_exemptions + farm_exemptions
-                           else
-                             new_exemptions
-                           end
-                         end.uniq
+                           is_farm ? (new_exemptions + farm_exemptions) : new_exemptions
+                         else
+                           new_exemptions
+                         end
 
       { "temp_#{exemption_type}_exemptions": new_exemptions, temp_exemptions: total_exemptions.uniq }
     end
