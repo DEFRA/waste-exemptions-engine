d470217d8829a569cc7dcc57bd93f00aadd40d7f
Jerome Pratt <jerome.pratt@hey.com>
2025-02-11 10:37:29 +0000

    [RUBY-3537] Remove unnecessary delegate for `temp_add_additional_non_farm_exemptions` in `ConfirmActivityExemptionsForm`.

--- app/forms/waste_exemptions_engine/confirm_activity_exemptions_form.rb
+++ app/forms/waste_exemptions_engine/confirm_activity_exemptions_form.rb
@@ -4,7 +4,6 @@ module WasteExemptionsEngine
   class ConfirmActivityExemptionsForm < BaseForm
     delegate :exemption_ids, :temp_exemptions, :temp_farm_exemptions, :temp_confirm_exemptions, :farm_affiliated?,
              to: :transient_registration
-    delegate :temp_add_additional_non_farm_exemptions, to: :transient_registration
 
     validates :temp_confirm_exemptions, "defra_ruby/validators/true_false": true
 
