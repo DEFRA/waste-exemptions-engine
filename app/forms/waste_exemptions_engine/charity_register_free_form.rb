477322965d0ea9e94e779a4663239fc896c3c585
Jerome Pratt <jerome.pratt@hey.com>
2025-03-04 14:16:18 +0000

    [RUBY-3677] Add navigation restriction and tests for CharityRegisterFreeForm
    - Implement `can_navigate_flexibly?` method in `CharityRegisterFreeForm` to restrict flexible navigation.
    - Add VCR cassette for `GET-charity_register_free_form` to validate HTML content.
    - Update request specs for `CharityRegisterFreeForm` to include new tests for template rendering, HTTP status, and HTML validation.
    - Introduce `charged` trait in `business_type_form` factory to denote charged registrations

--- app/forms/waste_exemptions_engine/charity_register_free_form.rb
+++ app/forms/waste_exemptions_engine/charity_register_free_form.rb
@@ -2,6 +2,11 @@
 
 module WasteExemptionsEngine
   class CharityRegisterFreeForm < BaseForm
+    # This is a final state, so we don't want users to be able to navigate to it flexibly
+    def self.can_navigate_flexibly?
+      false
+    end
+
     # Override BaseForm method as users shouldn't be able to submit this form
     def submit(_params)
       raise UnsubmittableForm
