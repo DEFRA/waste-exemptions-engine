ffc9c1aa56c01e990f59f2606432e5a4e93ccbb3
Jerome Pratt <jerome.pratt@hey.com>
2025-03-04 12:28:45 +0000

    [RUBY-3677] Update factory to use `new_charged_registration` for charity form tests

--- spec/support/factories/forms/charity_register_free_form.rb
+++ spec/support/factories/forms/charity_register_free_form.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :charity_register_free_form, class: "WasteExemptionsEngine::CharityRegisterFreeForm" do
     initialize_with do
-      new(create(:new_registration, workflow_state: "charity_register_free_form"))
+      new(create(:new_charged_registration, workflow_state: "charity_register_free_form"))
     end
   end
 end
