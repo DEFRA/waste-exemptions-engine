c0cdcd452ac2110433e87a86e56acc3bf7e11d06
Jerome Pratt <jerome.pratt@hey.com>
2025-03-05 16:01:55 +0000

    [RUBY-3677] Remove unused `:charged` trait from `business_type_form` factory
    The `:charged` trait was redundant as the `business_type_form` is only used for charged registrations, and the factory is already initialized with a charged registration by default.

--- spec/support/factories/forms/business_type_form.rb
+++ spec/support/factories/forms/business_type_form.rb
@@ -2,12 +2,6 @@
 
 FactoryBot.define do
   factory :business_type_form, class: "WasteExemptionsEngine::BusinessTypeForm" do
-    trait :charged do
-      # currently business_type_form is available for charged registrations only,
-      # so factory gets initialised with charged registration by default
-      # no need to do anything here.
-    end
-
     initialize_with do
       new(create(:new_registration, workflow_state: "business_type_form"))
     end
