6c4ab4a764e728e7834d49e91d6d7c3b7a171476
Leonid Brujev <brujev@gmail.com>
2025-04-02 12:48:02 +0100

    [RUBY-3745] Refactor SendRegistrationEditEmailJob to set edit_link_requested_by attribute

--- app/jobs/send_registration_edit_email_job.rb
+++ app/jobs/send_registration_edit_email_job.rb
@@ -2,6 +2,7 @@
 
 class SendRegistrationEditEmailJob < ApplicationJob
   def perform(reference:, email:)
+    @email = email
     ActiveRecord::Base.transaction do
       @registration = WasteExemptionsEngine::RegistrationEmailMatchService.run(reference: reference, email: email)
       return if @registration.blank?
@@ -11,19 +12,17 @@ class SendRegistrationEditEmailJob < ApplicationJob
 
       if @registration.contact_email.present?
         WasteExemptionsEngine::RegistrationEditLinkEmailService.run(
-          registration: @registration,
-          magic_link_token:,
-          recipient: @registration.contact_email
+          registration: @registration, magic_link_token:, recipient: @registration.contact_email
         )
       end
 
       if @registration.applicant_email.present? && @registration.applicant_email != @registration.contact_email
         WasteExemptionsEngine::RegistrationEditLinkEmailService.run(
-          registration: @registration,
-          magic_link_token:,
-          recipient: @registration.applicant_email
+          registration: @registration, magic_link_token:, recipient: @registration.applicant_email
         )
       end
+
+      set_edit_link_requested_by
     end
   end
 
@@ -32,5 +31,9 @@ class SendRegistrationEditEmailJob < ApplicationJob
   def generate_magic_link_token
     @registration.regenerate_and_timestamp_edit_token
     @registration.edit_token
+  end
+
+  def set_edit_link_requested_by
+    @registration.update(edit_link_requested_by: @email)
   end
 end
