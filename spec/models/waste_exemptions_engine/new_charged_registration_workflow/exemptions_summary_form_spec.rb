cadc27dd3edf88caceae7b4d3ccbf1ca7e0c53df
Jerome Pratt <jerome.pratt@hey.com>
2025-04-03 09:45:15 +0100

    [RUBY-3751] Refactor exemptions summary flow and update redirect paths
    - Removed conditional logic for `check_your_answers_flow` in `exemptions_summary_form_spec.rb` to simplify the transition logic, now always transitioning to `site_grid_reference_form`.
    - Updated redirection paths in `confirm_activity_exemptions_forms_spec.rb` and `confirm_farm_exemptions_forms_spec.rb` to point to `exemptions_summary_forms_path` instead of `site_grid_reference_forms_path`.

--- spec/models/waste_exemptions_engine/new_charged_registration_workflow/exemptions_summary_form_spec.rb
+++ spec/models/waste_exemptions_engine/new_charged_registration_workflow/exemptions_summary_form_spec.rb
@@ -10,30 +10,11 @@ module WasteExemptionsEngine
                workflow_state: :exemptions_summary_form)
       end
 
-      context "when not in check_your_answers_flow" do
-        before do
-          allow(new_registration).to receive(:check_your_answers_flow?).and_return(false)
-        end
-
-        it "transitions to site_grid_reference_form" do
-          expect(new_registration)
-            .to transition_from(:exemptions_summary_form)
-            .to(:site_grid_reference_form)
-            .on_event(:next)
-        end
-      end
-
-      context "when in check_your_answers_flow" do
-        before do
-          allow(new_registration).to receive(:check_your_answers_flow?).and_return(true)
-        end
-
-        it "transitions to declaration_form" do
-          expect(new_registration)
-            .to transition_from(:exemptions_summary_form)
-            .to(:declaration_form)
-            .on_event(:next)
-        end
+      it "transitions to site_grid_reference_form" do
+        expect(new_registration)
+          .to transition_from(:exemptions_summary_form)
+          .to(:site_grid_reference_form)
+          .on_event(:next)
       end
     end
   end
