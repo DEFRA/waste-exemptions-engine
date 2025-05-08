29ac9d24699e81521f077f893a982e042a7be682
Leonid Brujev <brujev@gmail.com>
2025-05-08 16:45:11 +0100

    [RUBY-3149] Add DetailedLogger class with logging methods based on feature toggle

---
+++ lib/waste_exemptions_engine/detailed_logger.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module WasteExemptionsEngine
+  class DetailedLogger
+    # Do not log anything unless detailed_logging is enabled.
+    def self.fatal(*)
+      Rails.logger.fatal(*) if detail_enabled?
+    end
+
+    def self.error(*)
+      Rails.logger.error(*) if detail_enabled?
+    end
+
+    def self.warn(*)
+      Rails.logger.warn(*) if detail_enabled?
+    end
+
+    def self.info(*)
+      Rails.logger.info(*) if detail_enabled?
+    end
+
+    def self.debug(*)
+      Rails.logger.debug(*) if detail_enabled?
+    end
+
+    def self.unknown(*)
+      Rails.logger.unknown(*) if detail_enabled?
+    end
+
+    def self.detail_enabled?
+      FeatureToggle.active?(:detailed_logging)
+    end
+  end
+end
