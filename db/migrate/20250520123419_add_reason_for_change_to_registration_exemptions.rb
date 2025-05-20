244330cbef43111f2efe42a35aaef5e9e53da6c4
Leonid Brujev <brujev@gmail.com>
2025-05-20 15:45:06 +0100

    [RUBY-3772] Add frozen_string_literal comment to migration

--- db/migrate/20250520123419_add_reason_for_change_to_registration_exemptions.rb
+++ db/migrate/20250520123419_add_reason_for_change_to_registration_exemptions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddReasonForChangeToRegistrationExemptions < ActiveRecord::Migration[7.2]
   def change
     add_column :registration_exemptions, :reason_for_change, :string, limit: 500
