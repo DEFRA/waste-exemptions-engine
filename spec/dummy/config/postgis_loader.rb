8928bb73a2f6d552ec301ef306ddc321bcf151ca
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-05-21 19:17:31 +0100

    [RUBY-3764] Remove deprecated postgis_loader.rb due to activerecord-postgis-adapter bug workaround no longer needed

--- spec/dummy/config/postgis_loader.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# This is required due to a bug in the activerecord-postgis-adapter gem due to
-# its reliance on rgeo-activerecord version 8.0.0 which is broken
-require "rgeo/active_record"
-require "activerecord-postgis-adapter"
