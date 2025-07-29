1f3f2623bbe767bdd0ab46b532882d3160ee7f22
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-07-29 18:32:13 +0100

    Remove farm bucket if farm answers changed to No

--- spec/support/factories/forms/on_a_farm_form.rb
+++ spec/support/factories/forms/on_a_farm_form.rb
@@ -3,14 +3,14 @@
 FactoryBot.define do
   factory :on_a_farm_form, class: "WasteExemptionsEngine::OnAFarmForm" do
     initialize_with do
-      new(create(:new_registration, workflow_state: "on_a_farm_form"))
+      new(create(:new_charged_registration, workflow_state: "on_a_farm_form"))
     end
   end
 
   factory :check_your_answers_edit_on_a_farm_form, class: "WasteExemptionsEngine::OnAFarmForm" do
     initialize_with do
-      new(create(:new_registration, workflow_state: "on_a_farm_form", on_a_farm: true,
-                                    temp_check_your_answers_flow: true))
+      new(create(:new_charged_registration, workflow_state: "on_a_farm_form", on_a_farm: true,
+                                            temp_check_your_answers_flow: true))
     end
   end
 
