b62189d732c1223315ebeb868e0bb1d1e3efdb72
Paul Doyle <97455399+PaulDoyle-EA@users.noreply.github.com>
2025-07-30 15:21:36 +0100

    Remove farm bucket if farm answers changed to No (#1335)
    https://eaflood.atlassian.net/browse/RUBY-3910

--- app/forms/waste_exemptions_engine/is_a_farmer_form.rb
+++ app/forms/waste_exemptions_engine/is_a_farmer_form.rb
@@ -5,5 +5,15 @@ module WasteExemptionsEngine
     delegate :is_a_farmer, to: :transient_registration
 
     validates :is_a_farmer, "defra_ruby/validators/true_false": true
+
+    def submit(attributes)
+      # If the user is revisiting this form and previously said yes, there might
+      # be a farm bucket which should now be removed if they said no this time around.
+      if @transient_registration.respond_to?(:order) && attributes[:is_a_farmer] == "false"
+        @transient_registration.order&.update(bucket: nil)
+      end
+
+      super
+    end
   end
 end
