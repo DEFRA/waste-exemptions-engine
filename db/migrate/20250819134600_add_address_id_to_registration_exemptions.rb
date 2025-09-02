e9dd2721beaa2180a0dbb07433be69bfd1a79dcd
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-09-02 16:41:56 +0100

    [RUBY-3942] Separate migrations for indexes from those adding columns
    So that disabled ddl transaction migrations are safer

--- db/migrate/20250819134600_add_address_id_to_registration_exemptions.rb
+++ db/migrate/20250819134600_add_address_id_to_registration_exemptions.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class AddAddressIdToRegistrationExemptions < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
   def change
     add_reference :registration_exemptions, :address,
                   null: true,
-                  index: { algorithm: :concurrently, name: "index_registration_exemptions_on_address_id" },
+                  index: false,
                   foreign_key: { to_table: :addresses, validate: false }
   end
 end
--- db/migrate/20250829100932_add_transient_address_id_to_transient_registration_exemptions.rb
+++ db/migrate/20250829100932_add_transient_address_id_to_transient_registration_exemptions.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 class AddTransientAddressIdToTransientRegistrationExemptions < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
   def change
     add_reference :transient_registration_exemptions, :transient_address,
                   null: true,
-                  index: {
-                    algorithm: :concurrently,
-                    name: "index_transient_registration_exemptions_on_transient_address_id"
-                  },
+                  index: false,
                   foreign_key: { to_table: :transient_addresses, validate: false }
   end
 end
