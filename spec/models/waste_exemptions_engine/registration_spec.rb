105ee80a9a406dd0a92b61efe2d75f1c0d15a4b0
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-09-12 10:43:58 +0100

    [RUBY-3942] Remove tests for `#multisite?` method in registration spec as part of charging rules engine update

--- spec/models/waste_exemptions_engine/registration_spec.rb
+++ spec/models/waste_exemptions_engine/registration_spec.rb
@@ -40,21 +40,6 @@ module WasteExemptionsEngine
         end
       end
 
-      describe "#multisite?" do
-        context "when there is one or no site addresses" do
-          it "returns false for single site address" do
-            expect(registration.multisite?).to be false
-          end
-        end
-
-        context "when there are multiple site addresses" do
-          it "returns true" do
-            create(:address, address_type: 3, registration: registration)
-            expect(registration.multisite?).to be true
-          end
-        end
-      end
-
       describe "#site_count" do
         it "returns the count of site addresses" do
           create(:address, address_type: 3, registration: registration)
