536abe0541b703407a5ef2dbd09c291f63c496ff
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-09-17 19:01:08 +0100

    [RUBY-3943] Exclude Kaminari pagination partials from SonarQube HTML validation
    Added `sonar.exclusions` for Kaminari partial templates `_gap.html.erb` and `_page.html.erb` to avoid false positives in HTML validation, as these partials are rendered within proper `<ul>` containers.
    
    Remove unneeded comments

--- app/forms/waste_exemptions_engine/multisite_site_address_lookup_form.rb
+++ app/forms/waste_exemptions_engine/multisite_site_address_lookup_form.rb
@@ -14,16 +14,12 @@ module WasteExemptionsEngine
 
     def submit(params)
       site_address_params = params.fetch(:site_address, {})
-      # Set for validation (expects a hash with :uprn or :postcode)
       @site_address = site_address_params
 
-      # Find address details from lookup results
       address_attributes = get_address_data(site_address_params[:uprn], :site)
 
-      # Must be valid and have a matching address from the lookup
       return false unless valid? && address_attributes.present?
 
-      # Create the site address directly in the transient registration (multi-site)
       transient_registration.transient_addresses.create!(
         address_attributes.merge(
           address_type: "site",
