dcc11090cb573665625e2fc2f06a18219cbb2227
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-09-24 17:27:51 +0100

    [RUBY-3943] Simplify `OrderCalculatorService#strategy_type` logic and update tests
    Removed multisite-related charging strategies and related conditionals from `OrderCalculatorService`, simplifying the `strategy_type` method to only distinguish between `FarmerChargingStrategy` and `RegularChargingStrategy` based on the bucket type. Updated specs by removing tests for multisite registration scenarios and added an environment variable mock for `ENABLE_MULTISITE` in feature toggle specs.

--- spec/models/waste_exemptions_engine/feature_toggle_spec.rb
+++ spec/models/waste_exemptions_engine/feature_toggle_spec.rb
@@ -69,6 +69,7 @@ module WasteExemptionsEngine
             before do
               # Change the environment variable value for this test only
               allow(ENV).to receive(:fetch).with("ENV_VARIABLE_TEST_FEATURE", any_args).and_return("true")
+              allow(ENV).to receive(:fetch).with("ENABLE_MULTISITE", any_args).and_return("true")
               # Force reload of the toggle settings after setting the environment variable
               described_class.send("reload_feature_toggles")
             end
