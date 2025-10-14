515ce231111c34d90692bef5443b2454e0f69e51
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-10-14 20:51:53 +0100

    [RUBY-3985] Add ability to remove a site from multiple site registrations (#1365)
    * [RUBY-3985] Add ability to remove a site from multiple site registrations
    
    - Implement `remove_site` action in `SitesFormsController` to delete a site address by `site_id`.
    - Update sites form view to include a remove button with confirmation and accessible site details.
    - Add localization for remove site details.
    - Add request specs to cover removing a site, including edge cases for invalid site, different registration, and invalid token.
    
    * [RUBY-3985] Add HasDisplayableAddress concern for address formatting and site identification
    
    - Introduced `HasDisplayableAddress` concern to provide `site_identifier` and `displayable_address_lines` methods.
    - Included the concern in `Address` and `TransientAddress` models.
    - Updated site removal view to use `site_identifier` for displaying site details.
    - Added comprehensive specs for the new methods in both `Address` and `TransientAddress` models.

--- app/controllers/waste_exemptions_engine/sites_forms_controller.rb
+++ app/controllers/waste_exemptions_engine/sites_forms_controller.rb
@@ -11,6 +11,14 @@ module WasteExemptionsEngine
       super(SitesForm, "sites_form")
     end
 
+    def remove_site
+      return unless set_up_form(SitesForm, "sites_form", params[:token])
+
+      @transient_registration.addresses.find_by(address_type: "site", id: params[:site_id])&.destroy
+
+      redirect_to new_sites_form_path(@sites_form.token, page: params[:page])
+    end
+
     private
 
     def transient_registration_attributes
