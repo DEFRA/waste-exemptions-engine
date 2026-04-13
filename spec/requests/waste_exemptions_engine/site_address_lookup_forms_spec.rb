f4efde06605506bc5b8b958c4ef9c75ef7deb69b
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-04-13 10:47:15 +0200

    RUBY 4063 wex multiple site improve the ux on the site page (#1480)
    * [RUBY-4063] Implement redirection to the last sites page in various forms
    
    * [RUBY-4063] Refactor form_path method for better readability and structure

--- spec/requests/waste_exemptions_engine/site_address_lookup_forms_spec.rb
+++ spec/requests/waste_exemptions_engine/site_address_lookup_forms_spec.rb
@@ -7,10 +7,21 @@ module WasteExemptionsEngine
     before { VCR.insert_cassette("postcode_valid", allow_playback_repeats: true) }
     after { VCR.eject_cassette }
 
+    let(:form) { build(:site_address_lookup_form) }
+
     it_behaves_like "GET form", :site_address_lookup_form, "/site-address-lookup"
     it_behaves_like "POST form", :site_address_lookup_form, "/site-address-lookup" do
       let(:form_data) { { site_address: { uprn: "340116" } } }
       let(:invalid_form_data) { [{ site_address: { uprn: nil } }] }
+    end
+
+    it "redirects to the last sites page after adding a looked-up multisite address" do
+      form.transient_registration.update!(is_multisite_registration: true)
+
+      post site_address_lookup_forms_path(token: form.token),
+           params: { site_address_lookup_form: { site_address: { uprn: "340116" } } }
+
+      expect(response).to redirect_to(new_sites_form_path(token: form.token, page: "last"))
     end
   end
 end
