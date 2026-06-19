a9a96b9da3cbac53bb07eef13705b53b820305c6
Leonid Brujev <brujev@gmail.com>
2026-06-19 18:09:57 +0100

    [RUBY-4332] code styling fixes

--- spec/support/shared_examples/requests/skip_to_manual_address.rb
+++ spec/support/shared_examples/requests/skip_to_manual_address.rb
@@ -4,6 +4,7 @@ RSpec.shared_examples "skip to manual address" do |form_factory, address_type:|
   let(:form) { build(form_factory) }
   let(:manual_address_request_path) { send("skip_to_manual_address_#{form_factory}s_path", token: form.token) }
   let(:manual_address_form_path) { send("#{address_type}_address_manual_forms_path", token: form.token) }
+
   status_code = WasteExemptionsEngine::ApplicationController::SUCCESSFUL_REDIRECTION_CODE
 
   describe "GET #{form_factory} skip to manual address" do
