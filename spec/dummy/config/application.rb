840edcd0b6dcab3af07936f99ab98257ee76fbb4
Leonid Brujev <brujev@gmail.com>
2026-07-24 15:11:37 +0100

    Upgrade Rails to 8.1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -20,7 +20,7 @@ require "waste_exemptions_engine/detailed_logger"
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults 7.2
+    config.load_defaults 8.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
