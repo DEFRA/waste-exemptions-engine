9f55da681e157af0ff0060c89e858ce2c2f12d81
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-09-15 14:36:58 +0100

    [RUBY-4395] Use ticket proof of payment variables

--- app/presenters/waste_exemptions_engine/proof_of_payment_presenter.rb
+++ app/presenters/waste_exemptions_engine/proof_of_payment_presenter.rb
@@ -6,38 +6,18 @@ module WasteExemptionsEngine
 
     PUBLIC_REGISTER_LINK = "https://environment.data.gov.uk/public-register/view/search-waste-exemptions"
 
-    PAYMENT_METHOD_LABELS = {
-      Payment::PAYMENT_TYPE_GOVPAY => "Card",
-      Payment::PAYMENT_TYPE_MISSING_CARD_PAYMENT => "Card",
-      Payment::PAYMENT_TYPE_BANK_TRANSFER => "BACS",
-      Payment::PAYMENT_TYPE_OTHER => "Other"
-    }.freeze
-
     def initialize(registration:)
       @registration = registration
     end
 
     def personalisation
-      # Notify placeholder names are case-sensitive. The supplied templates currently
-      # use a mixture of canonical and legacy names, so provide both sets of values.
-      registration_date = registration.submitted_at.to_date.to_fs(:day_month_year)
-      paid_date = payment_date.to_date.to_fs(:day_month_year)
-      method = PAYMENT_METHOD_LABELS.fetch(latest_payment.payment_type, latest_payment.payment_type.humanize)
-
       {
         reg_identifier: registration.reference,
         first_name: registration.contact_first_name,
         last_name: registration.contact_last_name,
-        " last_name" => registration.contact_last_name,
-        date_registered: registration_date,
-        "Date" => registration_date,
-        date_paid: paid_date,
-        "Date_paid" => paid_date,
-        payment_method: method,
-        "Payment_Method" => method,
+        "Date" => registration.submitted_at.to_date.to_fs(:day_month_year),
+        date_paid: payment_date.to_date.to_fs(:day_month_year),
         payment_amount: formatted_payment_amount,
-        "payment_Amount" => "£#{formatted_payment_amount}",
-        "Exemption_Total" => formatted_payment_amount,
         exemption_breakdown: exemption_breakdown,
         public_register_link: PUBLIC_REGISTER_LINK
       }
